9fda266b61fe261a6dbc133c5dd138b5f3df6574
AlberT <albert@faktiva.com>
2016-12-16 16:33:14 +0100

    CO: Fixed CORS setting the header
    Traumflug: PS commit 50f809d81b940b596040795c8944fd6356a01b8e,
               related to pull request 7081 there.

--- classes/Tools.php
+++ classes/Tools.php
@@ -3370,7 +3370,7 @@ class ToolsCore
         fwrite(
             $write_fd, "<IfModule mod_headers.c>
 	<FilesMatch \"\.(ttf|ttc|otf|eot|woff|woff2|svg)$\">
-		Header add Access-Control-Allow-Origin \"*\"
+		Header set Access-Control-Allow-Origin \"*\"
 	</FilesMatch>
 </IfModule>\n\n"
         );
