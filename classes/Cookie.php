32a9ff7ed472c13a07092d07f7aaf8747fc44c5a
NemoPS <nemo@nemops.com>
2017-01-17 16:09:14 +0100

    Using Tools instead of the rew strtolower

--- classes/Cookie.php
+++ classes/Cookie.php
@@ -87,7 +87,7 @@ class CookieCore
         $this->_path = rawurlencode($this->_path);
         $this->_path = str_replace('%2F', '/', $this->_path);
         $this->_path = str_replace('%7E', '~', $this->_path);
-        $this->_path = strtolower($this->_path);
+        $this->_path = Tools::strtolower($this->_path);
         $this->_domain = $this->getDomain($sharedUrls);
         $this->_name = 'PrestaShop-'.md5(($this->_standalone ? '' : _PS_VERSION_).$name.$this->_domain);
         $this->_allow_writing = true;
