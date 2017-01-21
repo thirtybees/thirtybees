8f618bfcf20c05109a1cc38d97abee12de8258ad
Michael Dekker <git@michaeldekker.nl>
2017-01-22 00:42:20 +0100

    Fix SpecificPrice test

--- tests/unit/SpecificPriceTest.php
+++ tests/unit/SpecificPriceTest.php
@@ -19,7 +19,7 @@ class SpecificPriceTest extends \Codeception\Test\Unit
     public function testScoreQuery()
     {
         $this->assertEquals(
-            '( IF (`idGroup` = 1, 2, 0) +  IF (`idCountry` = 1, 4, 0) +  IF (`idCurrency` = 1, 8, 0) +  IF (`idShop` = 1, 16, 0) +  IF (`idCustomer` = 1, 32, 0)) AS `score`',
+            '( IF (`id_group` = 1, 2, 0) +  IF (`id_country` = 1, 4, 0) +  IF (`id_currency` = 1, 8, 0) +  IF (`id_shop` = 1, 16, 0) +  IF (`id_customer` = 1, 32, 0)) AS `score`',
             $this->tester->invokeStaticMethod('SpecificPrice', '_getScoreQuery', [1, 1, 1, 1, 1, 1])
         );
     }
