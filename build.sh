1a308f195e207f8427c28343040aa23ff1eaec96
Michael Dekker <git@michaeldekker.nl>
2017-02-08 14:46:57 +0100

    Add build script

---
+++ build.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+composer install --no-dev
+find -type d -name tests -exec rm -rf {} \;
+composer dump-autoload -o
+mv admin-dev admin
+mv install-dev install
+rm -rf .git
+rm architecture.md
+rm codeception.yml
+rm .coveralls.yml
+rm .gitignore
+rm .gitmodules
+rm .scrutinizer.yml
+rm .travis.yml
+rm build.sh
+
+exit 0
