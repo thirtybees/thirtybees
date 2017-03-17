9f99767913aac3d2acf0f52bd502aeca8facff5e
Michael Dekker <git@michaeldekker.nl>
2017-03-17 18:18:24 +0100

    Cleanup ChangeCurrencyController

--- controllers/front/ChangeCurrencyController.php
+++ controllers/front/ChangeCurrencyController.php
@@ -21,25 +21,31 @@
  * versions in the future. If you wish to customize PrestaShop for your
  * needs please refer to https://www.thirtybees.com for more information.
  *
- *  @author    Thirty Bees <contact@thirtybees.com>
- *  @author    PrestaShop SA <contact@prestashop.com>
- *  @copyright 2017 Thirty Bees
- *  @copyright 2007-2016 PrestaShop SA
- *  @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ * @author    Thirty Bees <contact@thirtybees.com>
+ * @author    PrestaShop SA <contact@prestashop.com>
+ * @copyright 2017 Thirty Bees
+ * @copyright 2007-2016 PrestaShop SA
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  *  PrestaShop is an internationally registered trademark & property of PrestaShop SA
  */
 
+/**
+ * Class ChangeCurrencyControllerCore
+ *
+ * @since 1.0.0
+ */
 class ChangeCurrencyControllerCore extends FrontController
 {
     /**
      * Assign template vars related to page content
+     *
      * @see FrontController::initContent()
      */
     public function initContent()
     {
-        $currency = new Currency((int)Tools::getValue('id_currency'));
+        $currency = new Currency((int) Tools::getValue('id_currency'));
         if (Validate::isLoadedObject($currency) && !$currency->deleted) {
-            $this->context->cookie->id_currency = (int)$currency->id;
+            $this->context->cookie->id_currency = (int) $currency->id;
             $this->ajaxDie('1');
         }
         $this->ajaxDie('0');
