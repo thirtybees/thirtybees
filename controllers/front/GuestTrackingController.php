785d02bc0166e963f0074dcf2f0a445ea2cd18d9
Michael Dekker <git@michaeldekker.nl>
2017-03-18 21:45:41 +0100

    Cleanup GuestTrackingController

--- controllers/front/GuestTrackingController.php
+++ controllers/front/GuestTrackingController.php
@@ -21,22 +21,34 @@
  * versions in the future. If you wish to customize PrestaShop for your
  * needs please refer to https://www.thirtybees.com for more information.
  *
- *  @author    Thirty Bees <contact@thirtybees.com>
- *  @author    PrestaShop SA <contact@prestashop.com>
- *  @copyright 2017 Thirty Bees
- *  @copyright 2007-2016 PrestaShop SA
- *  @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ * @author    Thirty Bees <contact@thirtybees.com>
+ * @author    PrestaShop SA <contact@prestashop.com>
+ * @copyright 2017 Thirty Bees
+ * @copyright 2007-2016 PrestaShop SA
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  *  PrestaShop is an internationally registered trademark & property of PrestaShop SA
  */
 
+/**
+ * Class GuestTrackingControllerCore
+ *
+ * @since 1.0.0
+ */
 class GuestTrackingControllerCore extends FrontController
 {
+    // @codingStandardsIgnoreStart
+    /** @var bool $ssl */
     public $ssl = true;
+    /** @var string $php_self */
     public $php_self = 'guest-tracking';
+    // @codingStandardsIgnoreEnd
 
     /**
-     * Initialize guest tracking controller
-     * @see FrontController::init()
+     * Initialize
+     *
+     * @return void
+     *
+     * @since 1.0.0
      */
     public function init()
     {
@@ -48,35 +60,38 @@ class GuestTrackingControllerCore extends FrontController
 
     /**
      * Start forms process
-     * @see FrontController::postProcess()
+     *
+     * @return void
+     *
+     * @since 1.0.0
      */
     public function postProcess()
     {
         if (Tools::isSubmit('submitGuestTracking') || Tools::isSubmit('submitTransformGuestToCustomer')) {
             // These lines are here for retrocompatibility with old theme
-            $id_order = Tools::getValue('id_order');
-            $order_collection = [];
-            if ($id_order) {
-                if (is_numeric($id_order)) {
-                    $order = new Order((int)$id_order);
+            $idOrder = Tools::getValue('id_order');
+            $orderCollection = [];
+            if ($idOrder) {
+                if (is_numeric($idOrder)) {
+                    $order = new Order((int) $idOrder);
                     if (Validate::isLoadedObject($order)) {
-                        $order_collection = Order::getByReference($order->reference);
+                        $orderCollection = Order::getByReference($order->reference);
                     }
                 } else {
-                    $order_collection = Order::getByReference($id_order);
+                    $orderCollection = Order::getByReference($idOrder);
                 }
             }
 
             // Get order reference, ignore package reference (after the #, on the order reference)
-            $order_reference = current(explode('#', Tools::getValue('order_reference')));
+            $orderReference = current(explode('#', Tools::getValue('order_reference')));
             // Ignore $result_number
-            if (!empty($order_reference)) {
-                $order_collection = Order::getByReference($order_reference);
+            if (!empty($orderReference)) {
+                $orderCollection = Order::getByReference($orderReference);
             }
 
             $email = Tools::getValue('email');
 
-            if (empty($order_reference) && empty($id_order)) {
+            if (empty($orderReference) && empty($idOrder)) {
                 $this->errors[] = Tools::displayError('Please provide your order\'s reference number.');
             } elseif (empty($email)) {
                 $this->errors[] = Tools::displayError('Please provide a valid email address.');
@@ -87,14 +102,14 @@ class GuestTrackingControllerCore extends FrontController
             } elseif (Customer::customerExists($email, false, true)) {
                 $this->errors[] = Tools::displayError('This page is for guest accounts only. Since your guest account has already been transformed into a customer account, you can no longer view your order here. Please log in to your customer account to view this order');
                 $this->context->smarty->assign('show_login_link', true);
-            } elseif (!count($order_collection)) {
+            } elseif (!count($orderCollection)) {
                 $this->errors[] = Tools::displayError('Invalid order reference');
-            } elseif (!$order_collection->getFirst()->isAssociatedAtGuest($email)) {
+            } elseif (!$orderCollection->getFirst()->isAssociatedAtGuest($email)) {
                 $this->errors[] = Tools::displayError('Invalid order reference');
             } else {
-                $this->assignOrderTracking($order_collection);
-                if (Tools::isSubmit('submitTransformGuestToCustomer')) {
-                    $customer = new Customer((int)$order->id_customer);
+                $this->assignOrderTracking($orderCollection);
+                if (isset($order) && Tools::isSubmit('submitTransformGuestToCustomer')) {
+                    $customer = new Customer((int) $order->id_customer);
                     if (!Validate::isLoadedObject($customer)) {
                         $this->errors[] = Tools::displayError('Invalid customer');
                     } elseif (!Tools::getValue('password')) {
@@ -111,8 +126,75 @@ class GuestTrackingControllerCore extends FrontController
     }
 
     /**
-     * Assign template vars related to page content
-     * @see FrontController::initContent()
+     * Assigns template vars related to order tracking information
+     *
+     * @param PrestaShopCollection $orderCollection
+     *
+     * @throws PrestaShopException
+     *
+     * @since 1.0.0
+     */
+    protected function assignOrderTracking($orderCollection)
+    {
+        $customer = new Customer((int) $orderCollection->getFirst()->id_customer);
+
+        $orderCollection = ($orderCollection->getAll());
+
+        $orderList = [];
+        foreach ($orderCollection as $order) {
+            $orderList[] = $order;
+        }
+
+        foreach ($orderList as &$order) {
+            /** @var Order $order */
+            $order->id_order_state = (int) $order->getCurrentState();
+            $order->invoice = (OrderState::invoiceAvailable((int) $order->id_order_state) && $order->invoice_number);
+            $order->order_history = $order->getHistory((int) $this->context->language->id, false, true);
+            $order->carrier = new Carrier((int) $order->id_carrier, (int) $order->id_lang);
+            $order->address_invoice = new Address((int) $order->id_address_invoice);
+            $order->address_delivery = new Address((int) $order->id_address_delivery);
+            $order->inv_adr_fields = AddressFormat::getOrderedAddressFields($order->address_invoice->id_country);
+            $order->dlv_adr_fields = AddressFormat::getOrderedAddressFields($order->address_delivery->id_country);
+            $order->invoiceAddressFormatedValues = AddressFormat::getFormattedAddressFieldsValues($order->address_invoice, $order->inv_adr_fields);
+            $order->deliveryAddressFormatedValues = AddressFormat::getFormattedAddressFieldsValues($order->address_delivery, $order->dlv_adr_fields);
+            $order->currency = new Currency($order->id_currency);
+            $order->discounts = $order->getCartRules();
+            $order->invoiceState = (Validate::isLoadedObject($order->address_invoice) && $order->address_invoice->id_state) ? new State((int) $order->address_invoice->id_state) : false;
+            $order->deliveryState = (Validate::isLoadedObject($order->address_delivery) && $order->address_delivery->id_state) ? new State((int) $order->address_delivery->id_state) : false;
+            $order->products = $order->getProducts();
+            $order->customizedDatas = Product::getAllCustomizedDatas((int) $order->id_cart);
+            Product::addCustomizationPrice($order->products, $order->customizedDatas);
+            $order->total_old = $order->total_discounts > 0 ? (float) $order->total_paid - (float) $order->total_discounts : false;
+
+            if ($order->carrier->url && $order->shipping_number) {
+                $order->followup = str_replace('@', $order->shipping_number, $order->carrier->url);
+            }
+            $order->hook_orderdetaildisplayed = Hook::exec('displayOrderDetail', ['order' => $order]);
+
+            Hook::exec('actionOrderDetail', ['carrier' => $order->carrier, 'order' => $order]);
+        }
+
+        $this->context->smarty->assign(
+            [
+                'shop_name'           => Configuration::get('PS_SHOP_NAME'),
+                'order_collection'    => $orderList,
+                'return_allowed'      => false,
+                'invoiceAllowed'      => (int) Configuration::get('PS_INVOICE'),
+                'is_guest'            => true,
+                'group_use_tax'       => (Group::getPriceDisplayMethod($customer->id_default_group) == PS_TAX_INC),
+                'CUSTOMIZE_FILE'      => Product::CUSTOMIZE_FILE,
+                'CUSTOMIZE_TEXTFIELD' => Product::CUSTOMIZE_TEXTFIELD,
+                'use_tax'             => Configuration::get('PS_TAX'),
+            ]
+        );
+    }
+
+    /**
+     * Initialize content
+     *
+     * @return void
+     *
+     * @since 1.0.0
      */
     public function initContent()
     {
@@ -125,75 +207,20 @@ class GuestTrackingControllerCore extends FrontController
 
         $this->context->smarty->assign(
             [
-            'action' => $this->context->link->getPageLink('guest-tracking.php', true),
-            'errors' => $this->errors,
+                'action' => $this->context->link->getPageLink('guest-tracking.php', true),
+                'errors' => $this->errors,
             ]
         );
         $this->setTemplate(_PS_THEME_DIR_.'guest-tracking.tpl');
     }
 
     /**
-     * Assigns template vars related to order tracking information
+     * Set media
      *
-     * @param PrestaShopCollection $order_collection
+     * @return void
      *
-     * @throws PrestaShopException
+     * @since 1.0.0
      */
-    protected function assignOrderTracking($order_collection)
-    {
-        $customer = new Customer((int)$order_collection->getFirst()->id_customer);
-
-        $order_collection = ($order_collection->getAll());
-
-        $order_list = [];
-        foreach ($order_collection as $order) {
-            $order_list[] = $order;
-        }
-
-        foreach ($order_list as &$order) {
-            /** @var Order $order */
-            $order->id_order_state = (int)$order->getCurrentState();
-            $order->invoice = (OrderState::invoiceAvailable((int)$order->id_order_state) && $order->invoice_number);
-            $order->order_history = $order->getHistory((int)$this->context->language->id, false, true);
-            $order->carrier = new Carrier((int)$order->id_carrier, (int)$order->id_lang);
-            $order->address_invoice = new Address((int)$order->id_address_invoice);
-            $order->address_delivery = new Address((int)$order->id_address_delivery);
-            $order->inv_adr_fields = AddressFormat::getOrderedAddressFields($order->address_invoice->id_country);
-            $order->dlv_adr_fields = AddressFormat::getOrderedAddressFields($order->address_delivery->id_country);
-            $order->invoiceAddressFormatedValues = AddressFormat::getFormattedAddressFieldsValues($order->address_invoice, $order->inv_adr_fields);
-            $order->deliveryAddressFormatedValues = AddressFormat::getFormattedAddressFieldsValues($order->address_delivery, $order->dlv_adr_fields);
-            $order->currency = new Currency($order->id_currency);
-            $order->discounts = $order->getCartRules();
-            $order->invoiceState = (Validate::isLoadedObject($order->address_invoice) && $order->address_invoice->id_state) ? new State((int)$order->address_invoice->id_state) : false;
-            $order->deliveryState = (Validate::isLoadedObject($order->address_delivery) && $order->address_delivery->id_state) ? new State((int)$order->address_delivery->id_state) : false;
-            $order->products = $order->getProducts();
-            $order->customizedDatas = Product::getAllCustomizedDatas((int)$order->id_cart);
-            Product::addCustomizationPrice($order->products, $order->customizedDatas);
-            $order->total_old = $order->total_discounts > 0 ? (float)$order->total_paid - (float)$order->total_discounts : false;
-
-            if ($order->carrier->url && $order->shipping_number) {
-                $order->followup = str_replace('@', $order->shipping_number, $order->carrier->url);
-            }
-            $order->hook_orderdetaildisplayed = Hook::exec('displayOrderDetail', ['order' => $order]);
-
-            Hook::exec('actionOrderDetail', ['carrier' => $order->carrier, 'order' => $order]);
-        }
-
-        $this->context->smarty->assign(
-            [
-            'shop_name' => Configuration::get('PS_SHOP_NAME'),
-            'order_collection' => $order_list,
-            'return_allowed' => false,
-            'invoiceAllowed' => (int)Configuration::get('PS_INVOICE'),
-            'is_guest' => true,
-            'group_use_tax' => (Group::getPriceDisplayMethod($customer->id_default_group) == PS_TAX_INC),
-            'CUSTOMIZE_FILE' => Product::CUSTOMIZE_FILE,
-            'CUSTOMIZE_TEXTFIELD' => Product::CUSTOMIZE_TEXTFIELD,
-            'use_tax' => Configuration::get('PS_TAX'),
-            ]
-        );
-    }
-
     public function setMedia()
     {
         parent::setMedia();
@@ -204,10 +231,12 @@ class GuestTrackingControllerCore extends FrontController
 
     protected function processAddressFormat(Address $delivery, Address $invoice)
     {
-        $inv_adr_fields = AddressFormat::getOrderedAddressFields($invoice->id_country, false, true);
-        $dlv_adr_fields = AddressFormat::getOrderedAddressFields($delivery->id_country, false, true);
+        $invAdrFields = AddressFormat::getOrderedAddressFields($invoice->id_country, false, true);
+        $dlvAdrFields = AddressFormat::getOrderedAddressFields($delivery->id_country, false, true);
 
-        $this->context->smarty->assign('inv_adr_fields', $inv_adr_fields);
-        $this->context->smarty->assign('dlv_adr_fields', $dlv_adr_fields);
+        $this->context->smarty->assign([
+            'inv_adr_fields' => $invAdrFields,
+            'dlv_adr_fields' => $dlvAdrFields,
+        ]);
     }
 }
