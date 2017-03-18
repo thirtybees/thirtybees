1cc2663677420032d057aee894c31ec4ff993ed0
Michael Dekker <git@michaeldekker.nl>
2017-03-18 21:50:30 +0100

    Cleanup StoresController

--- controllers/front/StoresController.php
+++ controllers/front/StoresController.php
@@ -21,21 +21,34 @@
  * versions in the future. If you wish to customize PrestaShop for your
  * needs please refer to https://www.thirtybees.com for more information.
  *
- *  @author    Thirty Bees <contact@thirtybees.com>
- *  @author    PrestaShop SA <contact@prestashop.com>
- *  @copyright 2017 Thirty Bees
- *  @copyright 2007-2016 PrestaShop SA
- *  @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ * @author    Thirty Bees <contact@thirtybees.com>
+ * @author    PrestaShop SA <contact@prestashop.com>
+ * @copyright 2017 Thirty Bees
+ * @copyright 2007-2016 PrestaShop SA
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  *  PrestaShop is an internationally registered trademark & property of PrestaShop SA
  */
 
+/**
+ * Class StoresControllerCore
+ *
+ * @since 1.0.0
+ */
 class StoresControllerCore extends FrontController
 {
+    // @codingStandardsIgnoreStart
+    /** @var string $php_self */
     public $php_self = 'stores';
+    // @codingStandardsIgnoreEnd
 
     /**
      * Initialize stores controller
-     * @see FrontController::init()
+     *
+     * @see   FrontController::init()
+     *
+     * @return void
+     *
+     * @since 1.0.0
      */
     public function init()
     {
@@ -48,60 +61,57 @@ class StoresControllerCore extends FrontController
     }
 
     /**
-     * Get formatted string address
+     * Assign template vars related to page content
      *
-     * @param array $store
+     * @see FrontController::initContent()
      *
-     * @return string
+     * @return void
+     *
+     * @since 1.0.0
      */
-    protected function processStoreAddress($store)
+    public function initContent()
     {
-        $ignore_field = [
-            'firstname',
-            'lastname'
-        ];
+        parent::initContent();
 
-        $out_datas = [];
-
-        $address_datas = AddressFormat::getOrderedAddressFields($store['id_country'], false, true);
-        $state = (isset($store['id_state'])) ? new State($store['id_state']) : null;
-
-        foreach ($address_datas as $data_line) {
-            $data_fields = explode(' ', $data_line);
-            $addr_out = [];
-
-            $data_fields_mod = false;
-            foreach ($data_fields as $field_item) {
-                $field_item = trim($field_item);
-                if (!in_array($field_item, $ignore_field) && !empty($store[$field_item])) {
-                    $addr_out[] = ($field_item == 'city' && $state && isset($state->iso_code) && strlen($state->iso_code)) ?
-                        $store[$field_item].', '.$state->iso_code : $store[$field_item];
-                    $data_fields_mod = true;
-                }
-            }
-            if ($data_fields_mod) {
-                $out_datas[] = implode(' ', $addr_out);
-            }
+        if (Configuration::get('PS_STORES_SIMPLIFIED')) {
+            $this->assignStoresSimplified();
+        } else {
+            $this->assignStores();
         }
 
-        $out = implode('<br />', $out_datas);
-        return $out;
+        $this->context->smarty->assign(
+            [
+                'mediumSize'  => Image::getSize(ImageType::getFormatedName('medium')),
+                'defaultLat'  => (float) Configuration::get('PS_STORES_CENTER_LAT'),
+                'defaultLong' => (float) Configuration::get('PS_STORES_CENTER_LONG'),
+                'searchUrl'   => $this->context->link->getPageLink('stores'),
+                'logo_store'  => Configuration::get('PS_STORES_ICON'),
+            ]
+        );
+
+        $this->setTemplate(_PS_THEME_DIR_.'stores.tpl');
     }
 
     /**
      * Assign template vars for simplified stores
+     *
+     * @return void
+     *
+     * @since 1.0.0
      */
     protected function assignStoresSimplified()
     {
-        $stores = Db::getInstance()->executeS('
+        $stores = Db::getInstance()->executeS(
+            '
 		SELECT s.*, cl.name country, st.iso_code state
 		FROM '._DB_PREFIX_.'store s
 		'.Shop::addSqlAssociation('store', 's').'
 		LEFT JOIN '._DB_PREFIX_.'country_lang cl ON (cl.id_country = s.id_country)
 		LEFT JOIN '._DB_PREFIX_.'state st ON (st.id_state = s.id_state)
-		WHERE s.active = 1 AND cl.id_lang = '.(int)$this->context->language->id);
+		WHERE s.active = 1 AND cl.id_lang = '.(int) $this->context->language->id
+        );
 
-        $addresses_formated = [];
+        $addressesFormatted = [];
 
         foreach ($stores as &$store) {
             $address = new Address();
@@ -111,128 +121,73 @@ class StoresControllerCore extends FrontController
             $address->postcode = $store['postcode'];
             $address->city = $store['city'];
 
-            $addresses_formated[$store['id_store']] = AddressFormat::getFormattedLayoutData($address);
+            $addressesFormatted[$store['id_store']] = AddressFormat::getFormattedLayoutData($address);
 
-            $store['has_picture'] = file_exists(_PS_STORE_IMG_DIR_.(int)$store['id_store'].'.jpg');
-            if ($working_hours = $this->renderStoreWorkingHours($store)) {
-                $store['working_hours'] = $working_hours;
+            $store['has_picture'] = file_exists(_PS_STORE_IMG_DIR_.(int) $store['id_store'].'.jpg');
+            if ($workingHours = $this->renderStoreWorkingHours($store)) {
+                $store['working_hours'] = $workingHours;
             }
         }
 
         $this->context->smarty->assign(
             [
-            'simplifiedStoresDiplay' => true,
-            'stores' => $stores,
-            'addresses_formated' => $addresses_formated,
+                'simplifiedStoresDiplay' => true,
+                'stores'                 => $stores,
+                'addresses_formated'     => $addressesFormatted,
             ]
         );
     }
 
-    public function renderStoreWorkingHours($store)
-    {
-        global $smarty;
-
-        $days[1] = 'Monday';
-        $days[2] = 'Tuesday';
-        $days[3] = 'Wednesday';
-        $days[4] = 'Thursday';
-        $days[5] = 'Friday';
-        $days[6] = 'Saturday';
-        $days[7] = 'Sunday';
-
-        $days_datas = [];
-        $hours = [];
-
-        if ($store['hours']) {
-            $hours = Tools::unSerialize($store['hours']);
-            if (is_array($hours)) {
-                $hours = array_filter($hours);
-            }
-        }
-
-        if (!empty($hours)) {
-            for ($i = 1; $i < 8; $i++) {
-                if (isset($hours[(int)$i - 1])) {
-                    $hours_datas = [];
-                    $hours_datas['hours'] = $hours[(int)$i - 1];
-                    $hours_datas['day'] = $days[$i];
-                    $days_datas[] = $hours_datas;
-                }
-            }
-            $smarty->assign('days_datas', $days_datas);
-            $smarty->assign('id_country', $store['id_country']);
-            return $this->context->smarty->fetch(_PS_THEME_DIR_.'store_infos.tpl');
-        }
-        return false;
-    }
-
-    public function getStores()
-    {
-        $distance_unit = Configuration::get('PS_DISTANCE_UNIT');
-        if (!in_array($distance_unit, ['km', 'mi'])) {
-            $distance_unit = 'km';
-        }
-
-        if (Tools::getValue('all') == 1) {
-            $stores = Db::getInstance()->executeS('
-			SELECT s.*, cl.name country, st.iso_code state
-			FROM '._DB_PREFIX_.'store s
-			'.Shop::addSqlAssociation('store', 's').'
-			LEFT JOIN '._DB_PREFIX_.'country_lang cl ON (cl.id_country = s.id_country)
-			LEFT JOIN '._DB_PREFIX_.'state st ON (st.id_state = s.id_state)
-			WHERE s.active = 1 AND cl.id_lang = '.(int)$this->context->language->id);
-        } else {
-            $distance = (int)Tools::getValue('radius', 100);
-            $multiplicator = ($distance_unit == 'km' ? 6371 : 3959);
-
-            $stores = Db::getInstance()->executeS('
-			SELECT s.*, cl.name country, st.iso_code state,
-			('.(int)$multiplicator.'
-				* acos(
-					cos(radians('.(float)Tools::getValue('latitude').'))
-					* cos(radians(latitude))
-					* cos(radians(longitude) - radians('.(float)Tools::getValue('longitude').'))
-					+ sin(radians('.(float)Tools::getValue('latitude').'))
-					* sin(radians(latitude))
-				)
-			) distance,
-			cl.id_country id_country
-			FROM '._DB_PREFIX_.'store s
-			'.Shop::addSqlAssociation('store', 's').'
-			LEFT JOIN '._DB_PREFIX_.'country_lang cl ON (cl.id_country = s.id_country)
-			LEFT JOIN '._DB_PREFIX_.'state st ON (st.id_state = s.id_state)
-			WHERE s.active = 1 AND cl.id_lang = '.(int)$this->context->language->id.'
-			HAVING distance < '.(int)$distance.'
-			ORDER BY distance ASC
-			LIMIT 0,20');
-        }
-
-        return $stores;
-    }
-
     /**
      * Assign template vars for classical stores
+     *
+     * @return void
+     *
+     * @since 1.0.0
      */
     protected function assignStores()
     {
         $this->context->smarty->assign('hasStoreIcon', file_exists(_PS_IMG_DIR_.Configuration::get('PS_STORES_ICON')));
 
-        $distance_unit = Configuration::get('PS_DISTANCE_UNIT');
-        if (!in_array($distance_unit, ['km', 'mi'])) {
-            $distance_unit = 'km';
+        $distanceUnit = Configuration::get('PS_DISTANCE_UNIT');
+        if (!in_array($distanceUnit, ['km', 'mi'])) {
+            $distanceUnit = 'km';
         }
 
         $this->context->smarty->assign(
             [
-            'distance_unit' => $distance_unit,
-            'simplifiedStoresDiplay' => false,
-            'stores' => $this->getStores(),
+                'distance_unit'          => $distanceUnit,
+                'simplifiedStoresDiplay' => false,
+                'stores'                 => $this->getStores(),
             ]
         );
     }
 
     /**
+     * Set media
+     *
+     * @return void
+     *
+     * @since 1.0.0
+     */
+    public function setMedia()
+    {
+        parent::setMedia();
+        $this->addCSS(_THEME_CSS_DIR_.'stores.css');
+
+        if (!Configuration::get('PS_STORES_SIMPLIFIED')) {
+            $defaultCountry = new Country((int) Tools::getCountry());
+            $this->addJS('http'.((Configuration::get('PS_SSL_ENABLED') && Configuration::get('PS_SSL_ENABLED_EVERYWHERE')) ? 's' : '').'://maps.google.com/maps/api/js?region='.substr($defaultCountry->iso_code, 0, 2));
+            $this->addJS(_THEME_JS_DIR_.'stores.js');
+        }
+    }
+
+    /**
      * Display the Xml for showing the nodes in the google map
+     *
+     * @return void
+     *
+     * @since 1.0.0
      */
     protected function displayAjax()
     {
@@ -250,12 +205,12 @@ class StoresControllerCore extends FrontController
             $newnode->addAttribute('address', $address);
             $newnode->addAttribute('other', $other);
             $newnode->addAttribute('phone', $store['phone']);
-            $newnode->addAttribute('id_store', (int)$store['id_store']);
-            $newnode->addAttribute('has_store_picture', file_exists(_PS_STORE_IMG_DIR_.(int)$store['id_store'].'.jpg'));
-            $newnode->addAttribute('lat', (float)$store['latitude']);
-            $newnode->addAttribute('lng', (float)$store['longitude']);
+            $newnode->addAttribute('id_store', (int) $store['id_store']);
+            $newnode->addAttribute('has_store_picture', file_exists(_PS_STORE_IMG_DIR_.(int) $store['id_store'].'.jpg'));
+            $newnode->addAttribute('lat', (float) $store['latitude']);
+            $newnode->addAttribute('lng', (float) $store['longitude']);
             if (isset($store['distance'])) {
-                $newnode->addAttribute('distance', (int)$store['distance']);
+                $newnode->addAttribute('distance', (int) $store['distance']);
             }
         }
 
@@ -264,41 +219,150 @@ class StoresControllerCore extends FrontController
     }
 
     /**
-     * Assign template vars related to page content
-     * @see FrontController::initContent()
+     * Get Stores
+     *
+     * @return array|false|mysqli_result|null|PDOStatement|resource
+     *
+     * @since 1.0.0
      */
-    public function initContent()
+    public function getStores()
     {
-        parent::initContent();
-
-        if (Configuration::get('PS_STORES_SIMPLIFIED')) {
-            $this->assignStoresSimplified();
-        } else {
-            $this->assignStores();
+        $distanceUnit = Configuration::get('PS_DISTANCE_UNIT');
+        if (!in_array($distanceUnit, ['km', 'mi'])) {
+            $distanceUnit = 'km';
         }
 
-        $this->context->smarty->assign(
-            [
-            'mediumSize' => Image::getSize(ImageType::getFormatedName('medium')),
-            'defaultLat' => (float)Configuration::get('PS_STORES_CENTER_LAT'),
-            'defaultLong' => (float)Configuration::get('PS_STORES_CENTER_LONG'),
-            'searchUrl' => $this->context->link->getPageLink('stores'),
-            'logo_store' => Configuration::get('PS_STORES_ICON')
-            ]
-        );
+        if (Tools::getValue('all') == 1) {
+            $stores = Db::getInstance()->executeS(
+                '
+			SELECT s.*, cl.name country, st.iso_code state
+			FROM '._DB_PREFIX_.'store s
+			'.Shop::addSqlAssociation('store', 's').'
+			LEFT JOIN '._DB_PREFIX_.'country_lang cl ON (cl.id_country = s.id_country)
+			LEFT JOIN '._DB_PREFIX_.'state st ON (st.id_state = s.id_state)
+			WHERE s.active = 1 AND cl.id_lang = '.(int) $this->context->language->id
+            );
+        } else {
+            $distance = (int) Tools::getValue('radius', 100);
+            $multiplicator = ($distanceUnit == 'km' ? 6371 : 3959);
 
-        $this->setTemplate(_PS_THEME_DIR_.'stores.tpl');
+            $stores = Db::getInstance()->executeS(
+                '
+			SELECT s.*, cl.name country, st.iso_code state,
+			('.(int) $multiplicator.'
+				* acos(
+					cos(radians('.(float) Tools::getValue('latitude').'))
+					* cos(radians(latitude))
+					* cos(radians(longitude) - radians('.(float) Tools::getValue('longitude').'))
+					+ sin(radians('.(float) Tools::getValue('latitude').'))
+					* sin(radians(latitude))
+				)
+			) distance,
+			cl.id_country id_country
+			FROM '._DB_PREFIX_.'store s
+			'.Shop::addSqlAssociation('store', 's').'
+			LEFT JOIN '._DB_PREFIX_.'country_lang cl ON (cl.id_country = s.id_country)
+			LEFT JOIN '._DB_PREFIX_.'state st ON (st.id_state = s.id_state)
+			WHERE s.active = 1 AND cl.id_lang = '.(int) $this->context->language->id.'
+			HAVING distance < '.(int) $distance.'
+			ORDER BY distance ASC
+			LIMIT 0,20'
+            );
+        }
+
+        return $stores;
     }
 
-    public function setMedia()
+    /**
+     * Get formatted string address
+     *
+     * @param array $store
+     *
+     * @return string
+     *
+     * @since 1.0.0
+     */
+    protected function processStoreAddress($store)
     {
-        parent::setMedia();
-        $this->addCSS(_THEME_CSS_DIR_.'stores.css');
+        $ignoreField = [
+            'firstname',
+            'lastname',
+        ];
 
-        if (!Configuration::get('PS_STORES_SIMPLIFIED')) {
-            $default_country = new Country((int)Tools::getCountry());
-            $this->addJS('http'.((Configuration::get('PS_SSL_ENABLED') && Configuration::get('PS_SSL_ENABLED_EVERYWHERE')) ? 's' : '').'://maps.google.com/maps/api/js?region='.substr($default_country->iso_code, 0, 2));
-            $this->addJS(_THEME_JS_DIR_.'stores.js');
+        $outDatas = [];
+
+        $addressDatas = AddressFormat::getOrderedAddressFields($store['id_country'], false, true);
+        $state = (isset($store['id_state'])) ? new State($store['id_state']) : null;
+
+        foreach ($addressDatas as $dataLine) {
+            $dataFields = explode(' ', $dataLine);
+            $addrOut = [];
+
+            $dataFieldsMod = false;
+            foreach ($dataFields as $fieldItem) {
+                $fieldItem = trim($fieldItem);
+                if (!in_array($fieldItem, $ignoreField) && !empty($store[$fieldItem])) {
+                    $addrOut[] = ($fieldItem == 'city' && $state && isset($state->iso_code) && strlen($state->iso_code)) ?
+                        $store[$fieldItem].', '.$state->iso_code : $store[$fieldItem];
+                    $dataFieldsMod = true;
+                }
+            }
+            if ($dataFieldsMod) {
+                $outDatas[] = implode(' ', $addrOut);
+            }
         }
+
+        $out = implode('<br />', $outDatas);
+
+        return $out;
+    }
+
+    /**
+     * Render opening hours
+     *
+     * @param $store
+     *
+     * @return bool|string
+     *
+     * @since 1.0.0
+     */
+    public function renderStoreWorkingHours($store)
+    {
+        global $smarty;
+
+        $days[1] = 'Monday';
+        $days[2] = 'Tuesday';
+        $days[3] = 'Wednesday';
+        $days[4] = 'Thursday';
+        $days[5] = 'Friday';
+        $days[6] = 'Saturday';
+        $days[7] = 'Sunday';
+
+        $daysDatas = [];
+        $hours = [];
+
+        if ($store['hours']) {
+            $hours = Tools::unSerialize($store['hours']);
+            if (is_array($hours)) {
+                $hours = array_filter($hours);
+            }
+        }
+
+        if (!empty($hours)) {
+            for ($i = 1; $i < 8; $i++) {
+                if (isset($hours[(int) $i - 1])) {
+                    $hoursDatas = [];
+                    $hoursDatas['hours'] = $hours[(int) $i - 1];
+                    $hoursDatas['day'] = $days[$i];
+                    $daysDatas[] = $hoursDatas;
+                }
+            }
+            $smarty->assign('days_datas', $daysDatas);
+            $smarty->assign('id_country', $store['id_country']);
+
+            return $this->context->smarty->fetch(_PS_THEME_DIR_.'store_infos.tpl');
+        }
+
+        return false;
     }
 }
