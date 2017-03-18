57e8d3c1c5cd12e3f3b2ffd7c3317563bc496718
Michael Dekker <git@michaeldekker.nl>
2017-03-18 21:48:09 +0100

    Cleanup OrderSlipController

--- controllers/front/OrderSlipController.php
+++ controllers/front/OrderSlipController.php
@@ -21,21 +21,39 @@
  * versions in the future. If you wish to customize PrestaShop for your
  * needs please refer to https://www.thirtybees.com for more information.
  *
- *  @author    Thirty Bees <contact@thirtybees.com>
- *  @author    PrestaShop SA <contact@prestashop.com>
- *  @copyright 2017 Thirty Bees
- *  @copyright 2007-2016 PrestaShop SA
- *  @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ * @author    Thirty Bees <contact@thirtybees.com>
+ * @author    PrestaShop SA <contact@prestashop.com>
+ * @copyright 2017 Thirty Bees
+ * @copyright 2007-2016 PrestaShop SA
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  *  PrestaShop is an internationally registered trademark & property of PrestaShop SA
  */
 
+/**
+ * Class OrderSlipControllerCore
+ *
+ * @since 1.0.0
+ */
 class OrderSlipControllerCore extends FrontController
 {
+    // @codingStandardsIgnoreStart
+    /** @var bool $auth */
     public $auth = true;
+    /** @var string $php_self */
     public $php_self = 'order-slip';
+    /** @var string $authRedirection */
     public $authRedirection = 'order-slip';
+    /** @var bool $ssl */
     public $ssl = true;
+    // @codingStandardsIgnoreEnd
 
+    /**
+     * Set media
+     *
+     * @return void
+     *
+     * @since 1.0.0
+     */
     public function setMedia()
     {
         parent::setMedia();
@@ -43,21 +61,26 @@ class OrderSlipControllerCore extends FrontController
         $this->addJqueryPlugin(['scrollTo', 'footable', 'footable-sort']);
         $this->addJS(
             [
-            _THEME_JS_DIR_.'history.js',
-            _THEME_JS_DIR_.'tools.js'
+                _THEME_JS_DIR_.'history.js',
+                _THEME_JS_DIR_.'tools.js',
             ] // retro compat themes 1.5
         );
     }
 
     /**
      * Assign template vars related to page content
+     *
      * @see FrontController::initContent()
+     *
+     * @return void
+     *
+     * @since 1.0.0
      */
     public function initContent()
     {
         parent::initContent();
 
-        $this->context->smarty->assign('ordersSlip', OrderSlip::getOrdersSlip((int)$this->context->cookie->id_customer));
+        $this->context->smarty->assign('ordersSlip', OrderSlip::getOrdersSlip((int) $this->context->cookie->id_customer));
         $this->setTemplate(_PS_THEME_DIR_.'order-slip.tpl');
     }
 }
