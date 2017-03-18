5a5d355db802f272e657aa91e4da88efe50d1645
Michael Dekker <git@michaeldekker.nl>
2017-03-18 21:45:19 +0100

    Cleanup DiscountController

--- controllers/front/DiscountController.php
+++ controllers/front/DiscountController.php
@@ -21,59 +21,72 @@
  * versions in the future. If you wish to customize PrestaShop for your
  * needs please refer to https://www.thirtybees.com for more information.
  *
- *  @author    Thirty Bees <contact@thirtybees.com>
- *  @author    PrestaShop SA <contact@prestashop.com>
- *  @copyright 2017 Thirty Bees
- *  @copyright 2007-2016 PrestaShop SA
- *  @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ * @author    Thirty Bees <contact@thirtybees.com>
+ * @author    PrestaShop SA <contact@prestashop.com>
+ * @copyright 2017 Thirty Bees
+ * @copyright 2007-2016 PrestaShop SA
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  *  PrestaShop is an internationally registered trademark & property of PrestaShop SA
  */
 
+/**
+ * Class DiscountControllerCore
+ *
+ * @since 1.0.0
+ */
 class DiscountControllerCore extends FrontController
 {
+    // @codingStandardsIgnoreStart
+    /** @var bool $auth */
     public $auth = true;
+    /** @var string $php_self */
     public $php_self = 'discount';
+    /** @var string $authRedirection */
     public $authRedirection = 'discount';
+    /** @var bool $ssl */
     public $ssl = true;
+    // @codingStandardsIgnoreEnd
 
     /**
-     * Assign template vars related to page content
-     * @see FrontController::initContent()
+     * Initialize content
+     *
+     * @return void
+     *
+     * @since 1.0.0
      */
     public function initContent()
     {
         parent::initContent();
 
-        $cart_rules = CartRule::getCustomerCartRules($this->context->language->id, $this->context->customer->id, true, false, true);
-        $nb_cart_rules = count($cart_rules);
+        $cartRules = CartRule::getCustomerCartRules($this->context->language->id, $this->context->customer->id, true, false, true);
+        $nbCartRules = count($cartRules);
 
-        foreach ($cart_rules as $key => &$discount ) {
+        foreach ($cartRules as $key => &$discount) {
             if ($discount['quantity_for_user'] === 0) {
-                unset($cart_rules[$key]);
+                unset($cartRules[$key]);
             }
 
-
             $discount['value'] = Tools::convertPriceFull(
-                                            $discount['value'],
-                                            new Currency((int)$discount['reduction_currency']),
-                                            new Currency((int)$this->context->cart->id_currency)
-                                        );
+                $discount['value'],
+                new Currency((int) $discount['reduction_currency']),
+                new Currency((int) $this->context->cart->id_currency)
+            );
             if ($discount['gift_product'] !== 0) {
                 $product = new Product((int) $discount['gift_product']);
                 if (isset($product->name)) {
-                    $discount['gift_product_name'] = current($product->name);
+                    $discount['gift_product_name'] = $product->name;
                 }
             }
         }
 
         $this->context->smarty->assign(
             [
-                                            'nb_cart_rules' => (int)$nb_cart_rules,
-                                            'cart_rules' => $cart_rules,
-                                            'discount' => $cart_rules,
-                                            'nbDiscounts' => (int)$nb_cart_rules
+                'nb_cart_rules' => (int) $nbCartRules,
+                'cart_rules'    => $cartRules,
+                'discount'      => $cartRules,
+                'nbDiscounts'   => (int) $nbCartRules,
             ]
-                                        );
+        );
         $this->setTemplate(_PS_THEME_DIR_.'discount.tpl');
     }
 }
