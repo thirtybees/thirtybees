a54b84a066424359fd368f40b429a22e3e0df721
Michael Dekker <git@michaeldekker.nl>
2017-03-19 00:07:38 +0100

    Cleanup AdminRangeWeightController

--- controllers/admin/AdminRangeWeightController.php
+++ controllers/admin/AdminRangeWeightController.php
@@ -21,19 +21,26 @@
  * versions in the future. If you wish to customize PrestaShop for your
  * needs please refer to https://www.thirtybees.com for more information.
  *
- *  @author    Thirty Bees <contact@thirtybees.com>
- *  @author    PrestaShop SA <contact@prestashop.com>
- *  @copyright 2017 Thirty Bees
- *  @copyright 2007-2016 PrestaShop SA
- *  @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ * @author    Thirty Bees <contact@thirtybees.com>
+ * @author    PrestaShop SA <contact@prestashop.com>
+ * @copyright 2017 Thirty Bees
+ * @copyright 2007-2016 PrestaShop SA
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  *  PrestaShop is an internationally registered trademark & property of PrestaShop SA
  */
 
 /**
- * @property RangeWeight $object
+ * Class AdminRangeWeightControllerCore
+ *
+ * @since 1.0.0
  */
 class AdminRangeWeightControllerCore extends AdminController
 {
+    /**
+     * AdminRangeWeightControllerCore constructor.
+     *
+     * @since 1.0.0
+     */
     public function __construct()
     {
         $this->bootstrap = true;
@@ -47,9 +54,9 @@ class AdminRangeWeightControllerCore extends AdminController
 
         $this->fields_list = [
             'id_range_weight' => ['title' => $this->l('ID'), 'align' => 'center', 'width' => 25],
-            'carrier_name' => ['title' => $this->l('Carrier'), 'align' => 'left', 'width' => 'auto', 'filter_key' => 'ca!name'],
-            'delimiter1' => ['title' => $this->l('From'), 'width' => 86, 'type' => 'float', 'suffix' => Configuration::get('PS_WEIGHT_UNIT'), 'align' => 'right'],
-            'delimiter2' => ['title' => $this->l('To'), 'width' => 86, 'type' => 'float', 'suffix' => Configuration::get('PS_WEIGHT_UNIT'), 'align' => 'right']
+            'carrier_name'    => ['title' => $this->l('Carrier'), 'align' => 'left', 'width' => 'auto', 'filter_key' => 'ca!name'],
+            'delimiter1'      => ['title' => $this->l('From'), 'width' => 86, 'type' => 'float', 'suffix' => Configuration::get('PS_WEIGHT_UNIT'), 'align' => 'right'],
+            'delimiter2'      => ['title' => $this->l('To'), 'width' => 86, 'type' => 'float', 'suffix' => Configuration::get('PS_WEIGHT_UNIT'), 'align' => 'right'],
         ];
 
         $this->_join = 'LEFT JOIN '._DB_PREFIX_.'carrier ca ON (ca.`id_carrier` = a.`id_carrier`)';
@@ -60,18 +67,32 @@ class AdminRangeWeightControllerCore extends AdminController
         parent::__construct();
     }
 
+    /**
+     * Initialize page header toolbar
+     *
+     * @return void
+     *
+     * @since 1.0.0
+     */
     public function initPageHeaderToolbar()
     {
         $this->page_header_toolbar_title = $this->l('Weight ranges');
         $this->page_header_toolbar_btn['new_weight_range'] = [
             'href' => self::$currentIndex.'&addrange_weight&token='.$this->token,
             'desc' => $this->l('Add new weight range', null, null, false),
-            'icon' => 'process-icon-new'
+            'icon' => 'process-icon-new',
         ];
 
         parent::initPageHeaderToolbar();
     }
 
+    /**
+     * Render form
+     *
+     * @return string
+     *
+     * @since 1.0.0
+     */
     public function renderForm()
     {
         $carriers = Carrier::getCarriers($this->context->language->id, true, false, false, null, Carrier::PS_CARRIERS_AND_CARRIER_MODULES_NEED_RANGE);
@@ -84,48 +105,60 @@ class AdminRangeWeightControllerCore extends AdminController
         $this->fields_form = [
             'legend' => [
                 'title' => $this->l('Weight ranges'),
-                'icon' => 'icon-suitcase'
+                'icon'  => 'icon-suitcase',
             ],
-            'input' => [
+            'input'  => [
                 [
-                    'type' => 'select',
-                    'label' => $this->l('Carrier'),
-                    'name' => 'id_carrier',
-                    'required' => false,
-                    'hint' => $this->l('You can apply this range to a different carrier by selecting its name.'),
-                    'options' => [
+                    'type'          => 'select',
+                    'label'         => $this->l('Carrier'),
+                    'name'          => 'id_carrier',
+                    'required'      => false,
+                    'hint'          => $this->l('You can apply this range to a different carrier by selecting its name.'),
+                    'options'       => [
                         'query' => $carriers,
-                        'id' => 'id_carrier',
-                        'name' => 'name'
+                        'id'    => 'id_carrier',
+                        'name'  => 'name',
                     ],
-                    'empty_message' => '<p class="alert alert-block">'.$this->l('There is no carrier available for this weight range.').'</p>'
+                    'empty_message' => '<p class="alert alert-block">'.$this->l('There is no carrier available for this weight range.').'</p>',
                 ],
                 [
-                    'type' => 'text',
-                    'label' => $this->l('From'),
-                    'name' => 'delimiter1',
+                    'type'     => 'text',
+                    'label'    => $this->l('From'),
+                    'name'     => 'delimiter1',
                     'required' => true,
-                    'suffix' => Configuration::get('PS_WEIGHT_UNIT'),
-                    'hint' => $this->l('Start range (included).'),
+                    'suffix'   => Configuration::get('PS_WEIGHT_UNIT'),
+                    'hint'     => $this->l('Start range (included).'),
                 ],
                 [
-                    'type' => 'text',
-                    'label' => $this->l('To'),
-                    'name' => 'delimiter2',
+                    'type'     => 'text',
+                    'label'    => $this->l('To'),
+                    'name'     => 'delimiter2',
                     'required' => true,
-                    'suffix' => Configuration::get('PS_WEIGHT_UNIT'),
-                    'hint' => $this->l('End range (excluded).'),
+                    'suffix'   => Configuration::get('PS_WEIGHT_UNIT'),
+                    'hint'     => $this->l('End range (excluded).'),
                 ],
             ],
             'submit' => [
                 'title' => $this->l('   Save   '),
-                'class' => 'btn btn-default'
-            ]
+                'class' => 'btn btn-default',
+            ],
         ];
 
         return parent::renderForm();
     }
 
+    /**
+     * Get list
+     *
+     * @param int         $idLang
+     * @param string|null $orderBy
+     * @param string|null $orderWay
+     * @param int         $start
+     * @param int|null    $limit
+     * @param int|bool    $idLangShop
+     *
+     * @since 1.0.0
+     */
     public function getList($idLang, $orderBy = null, $orderWay = null, $start = 0, $limit = null, $idLangShop = false)
     {
         parent::getList($idLang, $orderBy, $orderWay, $start, $limit, $idLangShop);
@@ -138,16 +171,23 @@ class AdminRangeWeightControllerCore extends AdminController
         }
     }
 
+    /**
+     * Post processing
+     *
+     * @return void
+     *
+     * @since 1.0.0
+     */
     public function postProcess()
     {
-        $id = (int)Tools::getValue('id_'.$this->table);
-        
+        $id = (int) Tools::getValue('id_'.$this->table);
+
         if (Tools::getValue('submitAdd'.$this->table)) {
             if (Tools::getValue('delimiter1') >= Tools::getValue('delimiter2')) {
                 $this->errors[] = Tools::displayError('Invalid range');
-            } elseif (!$id && RangeWeight::rangeExist((int)Tools::getValue('id_carrier'), (float)Tools::getValue('delimiter1'), (float)Tools::getValue('delimiter2'))) {
+            } elseif (!$id && RangeWeight::rangeExist((int) Tools::getValue('id_carrier'), (float) Tools::getValue('delimiter1'), (float) Tools::getValue('delimiter2'))) {
                 $this->errors[] = Tools::displayError('The range already exists');
-            } elseif (RangeWeight::isOverlapping((int)Tools::getValue('id_carrier'), (float)Tools::getValue('delimiter1'), (float)Tools::getValue('delimiter2'), ($id ? (int)$id : null))) {
+            } elseif (RangeWeight::isOverlapping((int) Tools::getValue('id_carrier'), (float) Tools::getValue('delimiter1'), (float) Tools::getValue('delimiter2'), ($id ? (int) $id : null))) {
                 $this->errors[] = Tools::displayError('Error: Ranges are overlapping');
             } elseif (!count($this->errors)) {
                 parent::postProcess();
