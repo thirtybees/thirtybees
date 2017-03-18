e26359139181a5e82022f34f86e1043377fad63b
Michael Dekker <git@michaeldekker.nl>
2017-03-18 21:50:21 +0100

    Cleanup StatisticsController

--- controllers/front/StatisticsController.php
+++ controllers/front/StatisticsController.php
@@ -21,21 +21,37 @@
  * versions in the future. If you wish to customize PrestaShop for your
  * needs please refer to https://www.thirtybees.com for more information.
  *
- *  @author    Thirty Bees <contact@thirtybees.com>
- *  @author    PrestaShop SA <contact@prestashop.com>
- *  @copyright 2017 Thirty Bees
- *  @copyright 2007-2016 PrestaShop SA
- *  @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ * @author    Thirty Bees <contact@thirtybees.com>
+ * @author    PrestaShop SA <contact@prestashop.com>
+ * @copyright 2017 Thirty Bees
+ * @copyright 2007-2016 PrestaShop SA
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  *  PrestaShop is an internationally registered trademark & property of PrestaShop SA
  */
 
+/**
+ * Class StatisticsControllerCore
+ *
+ * @since 1.0.0
+ */
 class StatisticsControllerCore extends FrontController
 {
+    // @codingStandardsIgnoreStart
+    /** @var bool $display_header */
     public $display_header = false;
+    /** @var bool $display_footer */
     public $display_footer = false;
-
+    /** @var string $param_token */
     protected $param_token;
+    // @codingStandardsIgnoreEnd
 
+    /**
+     * Post processing
+     *
+     * @return void
+     *
+     * @since 1.0.0
+     */
     public function postProcess()
     {
         $this->param_token = Tools::getValue('token');
@@ -54,39 +70,47 @@ class StatisticsControllerCore extends FrontController
 
     /**
      * Log statistics on navigation (resolution, plugins, etc.)
+     *
+     * @return void
+     *
+     * @since 1.0.0
      */
     protected function processNavigationStats()
     {
-        $id_guest = (int)Tools::getValue('id_guest');
-        if (sha1($id_guest._COOKIE_KEY_) != $this->param_token) {
+        $idGuest = (int) Tools::getValue('id_guest');
+        if (sha1($idGuest._COOKIE_KEY_) != $this->param_token) {
             die;
         }
 
-        $guest = new Guest((int)substr($_POST['id_guest'], 0, 10));
+        $guest = new Guest((int) substr($_POST['id_guest'], 0, 10));
         $guest->javascript = true;
-        $guest->screen_resolution_x = (int)substr($_POST['screen_resolution_x'], 0, 5);
-        $guest->screen_resolution_y = (int)substr($_POST['screen_resolution_y'], 0, 5);
-        $guest->screen_color = (int)substr($_POST['screen_color'], 0, 3);
-        $guest->sun_java = (int)substr($_POST['sun_java'], 0, 1);
-        $guest->adobe_flash = (int)substr($_POST['adobe_flash'], 0, 1);
-        $guest->adobe_director = (int)substr($_POST['adobe_director'], 0, 1);
-        $guest->apple_quicktime = (int)substr($_POST['apple_quicktime'], 0, 1);
-        $guest->real_player = (int)substr($_POST['real_player'], 0, 1);
-        $guest->windows_media = (int)substr($_POST['windows_media'], 0, 1);
+        $guest->screen_resolution_x = (int) substr($_POST['screen_resolution_x'], 0, 5);
+        $guest->screen_resolution_y = (int) substr($_POST['screen_resolution_y'], 0, 5);
+        $guest->screen_color = (int) substr($_POST['screen_color'], 0, 3);
+        $guest->sun_java = (int) substr($_POST['sun_java'], 0, 1);
+        $guest->adobe_flash = (int) substr($_POST['adobe_flash'], 0, 1);
+        $guest->adobe_director = (int) substr($_POST['adobe_director'], 0, 1);
+        $guest->apple_quicktime = (int) substr($_POST['apple_quicktime'], 0, 1);
+        $guest->real_player = (int) substr($_POST['real_player'], 0, 1);
+        $guest->windows_media = (int) substr($_POST['windows_media'], 0, 1);
         $guest->update();
     }
 
     /**
      * Log statistics on time spend on pages
+     *
+     * @return void
+     *
+     * @since 1.0.0
      */
     protected function processPageTime()
     {
-        $id_connection = (int)Tools::getValue('id_connections');
-        $time = (int)Tools::getValue('time');
-        $time_start = Tools::getValue('time_start');
-        $id_page = (int)Tools::getValue('id_page');
+        $idConnection = (int) Tools::getValue('id_connections');
+        $time = (int) Tools::getValue('time');
+        $timeStart = Tools::getValue('time_start');
+        $idPage = (int) Tools::getValue('id_page');
 
-        if (sha1($id_connection.$id_page.$time_start._COOKIE_KEY_) != $this->param_token) {
+        if (sha1($idConnection.$idPage.$timeStart._COOKIE_KEY_) != $this->param_token) {
             die;
         }
 
@@ -94,6 +118,6 @@ class StatisticsControllerCore extends FrontController
             die;
         }
 
-        Connection::setPageTime($id_connection, $id_page, substr($time_start, 0, 19), $time);
+        Connection::setPageTime($idConnection, $idPage, substr($timeStart, 0, 19), $time);
     }
 }
