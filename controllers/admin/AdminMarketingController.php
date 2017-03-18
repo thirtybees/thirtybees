3ee8672e8a4d9fa7621d350d193ec65b56476aab
Michael Dekker <git@michaeldekker.nl>
2017-03-18 23:16:35 +0100

    Cleanup AdminMarketingController

--- controllers/admin/AdminMarketingController.php
+++ controllers/admin/AdminMarketingController.php
@@ -21,49 +21,94 @@
  * versions in the future. If you wish to customize PrestaShop for your
  * needs please refer to https://www.thirtybees.com for more information.
  *
- *  @author    Thirty Bees <contact@thirtybees.com>
- *  @author    PrestaShop SA <contact@prestashop.com>
- *  @copyright 2017 Thirty Bees
- *  @copyright 2007-2016 PrestaShop SA
- *  @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ * @author    Thirty Bees <contact@thirtybees.com>
+ * @author    PrestaShop SA <contact@prestashop.com>
+ * @copyright 2017 Thirty Bees
+ * @copyright 2007-2016 PrestaShop SA
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  *  PrestaShop is an internationally registered trademark & property of PrestaShop SA
  */
 
+/**
+ * Class AdminMarketingControllerCore
+ *
+ * @since 1.0.0
+ */
 class AdminMarketingControllerCore extends AdminController
 {
+    /**
+     * AdminMarketingControllerCore constructor.
+     *
+     * @since 1.0.0
+     */
     public function __construct()
     {
         $this->bootstrap = true;
         parent::__construct();
     }
 
+    /**
+     * Initialize content
+     *
+     * @return void
+     *
+     * @since 1.0.0
+     */
     public function initContent()
     {
         $this->display = 'view';
-        return parent::initContent();
+
+        parent::initContent();
     }
-    
+
+    /**
+     * Initialize page toolbar title
+     *
+     * @return void
+     *
+     * @since 1.0.0
+     */
     public function initToolbarTitle()
     {
         $this->toolbar_title = array_unique($this->breadcrumbs);
     }
 
+    /**
+     * Initialize page header toolbar
+     *
+     * @return void
+     *
+     * @since 1.0.0
+     */
     public function initPageHeaderToolbar()
     {
         parent::initPageHeaderToolbar();
         $this->page_header_toolbar_btn = [];
     }
-    
+
+    /**
+     * Initialize toolbar
+     *
+     * @return false
+     */
     public function initToolbar()
     {
         return false;
     }
-    
+
+    /**
+     * Render view
+     *
+     * @return string
+     *
+     * @since 1.0.0
+     */
     public function renderView()
     {
         $this->tpl_view_vars = [
             'modules_list' => $this->renderModulesList(),
         ];
+
         return parent::renderView();
     }
 }
