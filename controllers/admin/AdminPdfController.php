1e081b4360e77e8a2882a90306220b0bddecd774
Michael Dekker <git@michaeldekker.nl>
2017-03-18 23:18:27 +0100

    Cleanup AdminPdfController

--- controllers/admin/AdminPdfController.php
+++ controllers/admin/AdminPdfController.php
@@ -21,16 +21,28 @@
  * versions in the future. If you wish to customize PrestaShop for your
  * needs please refer to https://www.thirtybees.com for more information.
  *
- *  @author    Thirty Bees <contact@thirtybees.com>
- *  @author    PrestaShop SA <contact@prestashop.com>
- *  @copyright 2017 Thirty Bees
- *  @copyright 2007-2016 PrestaShop SA
- *  @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ * @author    Thirty Bees <contact@thirtybees.com>
+ * @author    PrestaShop SA <contact@prestashop.com>
+ * @copyright 2017 Thirty Bees
+ * @copyright 2007-2016 PrestaShop SA
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  *  PrestaShop is an internationally registered trademark & property of PrestaShop SA
  */
 
+/**
+ * Class AdminPdfControllerCore
+ *
+ * @since 1.0.0
+ */
 class AdminPdfControllerCore extends AdminController
 {
+    /**
+     * Post processing
+     *
+     * @return void
+     *
+     * @since 1.0.0
+     */
     public function postProcess()
     {
         parent::postProcess();
@@ -39,11 +51,18 @@ class AdminPdfControllerCore extends AdminController
         exit;
     }
 
+    /**
+     * Initialize processing
+     *
+     * @return void
+     *
+     * @since 1.0.0
+     */
     public function initProcess()
     {
         parent::initProcess();
         $this->checkCacheFolder();
-        $access = Profile::getProfileAccess($this->context->employee->id_profile, (int)Tab::getIdFromClassName('AdminOrders'));
+        $access = Profile::getProfileAccess($this->context->employee->id_profile, (int) Tab::getIdFromClassName('AdminOrders'));
         if ($access['view'] === '1' && ($action = Tools::getValue('submitAction'))) {
             $this->action = $action;
         } else {
@@ -51,6 +70,13 @@ class AdminPdfControllerCore extends AdminController
         }
     }
 
+    /**
+     * Check cache folder
+     *
+     * @return void
+     *
+     * @since 1.0.0
+     */
     public function checkCacheFolder()
     {
         if (!is_dir(_PS_CACHE_DIR_.'tcpdf/')) {
@@ -58,6 +84,13 @@ class AdminPdfControllerCore extends AdminController
         }
     }
 
+    /**
+     * Process generate invoice PDF
+     *
+     * @return void
+     *
+     * @since 1.0.0
+     */
     public function processGenerateInvoicePdf()
     {
         if (Tools::isSubmit('id_order')) {
@@ -69,149 +102,248 @@ class AdminPdfControllerCore extends AdminController
         }
     }
 
+    /**
+     * Generate PDF invoice by Order ID
+     *
+     * @param int $idOrder
+     *
+     * @return void
+     *
+     * @since 1.0.0
+     */
+    public function generateInvoicePDFByIdOrder($idOrder)
+    {
+        $order = new Order((int) $idOrder);
+        if (!Validate::isLoadedObject($order)) {
+            die(Tools::displayError('The order cannot be found within your database.'));
+        }
+
+        $orderInvoiceList = $order->getInvoicesCollection();
+        Hook::exec('actionPDFInvoiceRender', ['order_invoice_list' => $orderInvoiceList]);
+        $this->generatePDF($orderInvoiceList, PDF::TEMPLATE_INVOICE);
+    }
+
+    /**
+     * Generate PDF
+     *
+     * @param $object
+     * @param $template
+     *
+     * @return void
+     *
+     * @since 1.0.0
+     */
+    public function generatePDF($object, $template)
+    {
+        $pdf = new PDF($object, $template, Context::getContext()->smarty);
+        $pdf->render();
+    }
+
+    /**
+     * Generate PDF Invoice by OrderInvoice ID
+     *
+     * @param int $idOrderInvoice
+     *
+     * @return void
+     *
+     * @since 1.0.0
+     */
+    public function generateInvoicePDFByIdOrderInvoice($idOrderInvoice)
+    {
+        $orderInvoice = new OrderInvoice((int) $idOrderInvoice);
+        if (!Validate::isLoadedObject($orderInvoice)) {
+            die(Tools::displayError('The order invoice cannot be found within your database.'));
+        }
+
+        Hook::exec('actionPDFInvoiceRender', ['order_invoice_list' => [$orderInvoice]]);
+        $this->generatePDF($orderInvoice, PDF::TEMPLATE_INVOICE);
+    }
+
+    /**
+     * Generate Order Slip PDF
+     *
+     * @return void
+     *
+     * @since 1.0.0
+     */
     public function processGenerateOrderSlipPDF()
     {
-        $order_slip = new OrderSlip((int)Tools::getValue('id_order_slip'));
-        $order = new Order((int)$order_slip->id_order);
+        $orderSlip = new OrderSlip((int) Tools::getValue('id_order_slip'));
+        $order = new Order((int) $orderSlip->id_order);
 
         if (!Validate::isLoadedObject($order)) {
             die(Tools::displayError('The order cannot be found within your database.'));
         }
 
-        $order->products = OrderSlip::getOrdersSlipProducts($order_slip->id, $order);
-        $this->generatePDF($order_slip, PDF::TEMPLATE_ORDER_SLIP);
+        $order->products = OrderSlip::getOrdersSlipProducts($orderSlip->id, $order);
+        $this->generatePDF($orderSlip, PDF::TEMPLATE_ORDER_SLIP);
     }
 
+    /**
+     * Process generate Delivery Slip PDF
+     *
+     * @return void
+     *
+     * @since 1.0.0
+     */
     public function processGenerateDeliverySlipPDF()
     {
         if (Tools::isSubmit('id_order')) {
-            $this->generateDeliverySlipPDFByIdOrder((int)Tools::getValue('id_order'));
+            $this->generateDeliverySlipPDFByIdOrder((int) Tools::getValue('id_order'));
         } elseif (Tools::isSubmit('id_order_invoice')) {
-            $this->generateDeliverySlipPDFByIdOrderInvoice((int)Tools::getValue('id_order_invoice'));
+            $this->generateDeliverySlipPDFByIdOrderInvoice((int) Tools::getValue('id_order_invoice'));
         } elseif (Tools::isSubmit('id_delivery')) {
-            $order = Order::getByDelivery((int)Tools::getValue('id_delivery'));
-            $this->generateDeliverySlipPDFByIdOrder((int)$order->id);
+            $order = Order::getByDelivery((int) Tools::getValue('id_delivery'));
+            $this->generateDeliverySlipPDFByIdOrder((int) $order->id);
         } else {
             die(Tools::displayError('The order ID -- or the invoice order ID -- is missing.'));
         }
     }
 
+    /**
+     * Generate Delivery Slip PDF by Order ID
+     *
+     * @param int $idOrder
+     *
+     * @throws PrestaShopException
+     *
+     * @return void
+     *
+     * @since 1.0.0
+     */
+    public function generateDeliverySlipPDFByIdOrder($idOrder)
+    {
+        $order = new Order((int) $idOrder);
+        if (!Validate::isLoadedObject($order)) {
+            throw new PrestaShopException('Can\'t load Order object');
+        }
+
+        $orderInvoiceCollection = $order->getInvoicesCollection();
+        $this->generatePDF($orderInvoiceCollection, PDF::TEMPLATE_DELIVERY_SLIP);
+    }
+
+    /**
+     * Generate Delivery Slip PDF by OrderInvoice ID
+     *
+     * @param int $idOrderInvoice
+     *
+     * @throws PrestaShopException
+     *
+     * @return void
+     *
+     * @since 1.0.0
+     */
+    public function generateDeliverySlipPDFByIdOrderInvoice($idOrderInvoice)
+    {
+        $orderInvoice = new OrderInvoice((int) $idOrderInvoice);
+        if (!Validate::isLoadedObject($orderInvoice)) {
+            throw new PrestaShopException('Can\'t load Order Invoice object');
+        }
+
+        $this->generatePDF($orderInvoice, PDF::TEMPLATE_DELIVERY_SLIP);
+    }
+
+    /**
+     * Generate PDF invoices
+     *
+     * @return void
+     *
+     * @since 1.0.0
+     */
     public function processGenerateInvoicesPDF()
     {
-        $order_invoice_collection = OrderInvoice::getByDateInterval(Tools::getValue('date_from'), Tools::getValue('date_to'));
+        $orderInvoiceCollection = OrderInvoice::getByDateInterval(Tools::getValue('date_from'), Tools::getValue('date_to'));
 
-        if (!count($order_invoice_collection)) {
+        if (!count($orderInvoiceCollection)) {
             die(Tools::displayError('No invoice was found.'));
         }
 
-        $this->generatePDF($order_invoice_collection, PDF::TEMPLATE_INVOICE);
+        $this->generatePDF($orderInvoiceCollection, PDF::TEMPLATE_INVOICE);
     }
 
+    /**
+     * Generate PDF invoices 2
+     *
+     * @return void
+     *
+     * @since 1.0.0
+     */
     public function processGenerateInvoicesPDF2()
     {
-        $order_invoice_collection = [];
-        foreach (explode('-', Tools::getValue('id_order_state')) as $id_order_state) {
-            if (is_array($order_invoices = OrderInvoice::getByStatus((int)$id_order_state))) {
-                $order_invoice_collection = array_merge($order_invoices, $order_invoice_collection);
+        $orderInvoiceCollection = [];
+        foreach (explode('-', Tools::getValue('id_order_state')) as $idOrderState) {
+            if (is_array($orderInvoices = OrderInvoice::getByStatus((int) $idOrderState))) {
+                $orderInvoiceCollection = array_merge($orderInvoices, $orderInvoiceCollection);
             }
         }
 
-        if (!count($order_invoice_collection)) {
+        if (!count($orderInvoiceCollection)) {
             die(Tools::displayError('No invoice was found.'));
         }
 
-        $this->generatePDF($order_invoice_collection, PDF::TEMPLATE_INVOICE);
+        $this->generatePDF($orderInvoiceCollection, PDF::TEMPLATE_INVOICE);
     }
 
+    /**
+     * Generate Order Slip PDFs
+     *
+     * @return void
+     *
+     * @since 1.0.0
+     */
     public function processGenerateOrderSlipsPDF()
     {
-        $id_order_slips_list = OrderSlip::getSlipsIdByDate(Tools::getValue('date_from'), Tools::getValue('date_to'));
-        if (!count($id_order_slips_list)) {
+        $idOrderSlipsList = OrderSlip::getSlipsIdByDate(Tools::getValue('date_from'), Tools::getValue('date_to'));
+        if (!count($idOrderSlipsList)) {
             die(Tools::displayError('No order slips were found.'));
         }
 
-        $order_slips = [];
-        foreach ($id_order_slips_list as $id_order_slips) {
-            $order_slips[] = new OrderSlip((int)$id_order_slips);
+        $orderSlips = [];
+        foreach ($idOrderSlipsList as $idOrderSlips) {
+            $orderSlips[] = new OrderSlip((int) $idOrderSlips);
         }
 
-        $this->generatePDF($order_slips, PDF::TEMPLATE_ORDER_SLIP);
+        $this->generatePDF($orderSlips, PDF::TEMPLATE_ORDER_SLIP);
     }
 
+    /**
+     * Generate Delivery Slip PDFs
+     *
+     * @return void
+     *
+     * @since 1.0.0
+     */
     public function processGenerateDeliverySlipsPDF()
     {
-        $order_invoice_collection = OrderInvoice::getByDeliveryDateInterval(Tools::getValue('date_from'), Tools::getValue('date_to'));
+        $orderInvoiceCollection = OrderInvoice::getByDeliveryDateInterval(Tools::getValue('date_from'), Tools::getValue('date_to'));
 
-        if (!count($order_invoice_collection)) {
+        if (!count($orderInvoiceCollection)) {
             die(Tools::displayError('No invoice was found.'));
         }
 
-        $this->generatePDF($order_invoice_collection, PDF::TEMPLATE_DELIVERY_SLIP);
+        $this->generatePDF($orderInvoiceCollection, PDF::TEMPLATE_DELIVERY_SLIP);
     }
 
+    /**
+     * Generate Supply Order Form PDFs
+     *
+     * @return void
+     *
+     * @since 1.0.0
+     */
     public function processGenerateSupplyOrderFormPDF()
     {
         if (!Tools::isSubmit('id_supply_order')) {
             die(Tools::displayError('The supply order ID is missing.'));
         }
 
-        $id_supply_order = (int)Tools::getValue('id_supply_order');
-        $supply_order = new SupplyOrder($id_supply_order);
+        $idSupplyOrder = (int) Tools::getValue('id_supply_order');
+        $supplyOrder = new SupplyOrder($idSupplyOrder);
 
-        if (!Validate::isLoadedObject($supply_order)) {
+        if (!Validate::isLoadedObject($supplyOrder)) {
             die(Tools::displayError('The supply order cannot be found within your database.'));
         }
 
-        $this->generatePDF($supply_order, PDF::TEMPLATE_SUPPLY_ORDER_FORM);
-    }
-
-    public function generateDeliverySlipPDFByIdOrder($id_order)
-    {
-        $order = new Order((int)$id_order);
-        if (!Validate::isLoadedObject($order)) {
-            throw new PrestaShopException('Can\'t load Order object');
-        }
-
-        $order_invoice_collection = $order->getInvoicesCollection();
-        $this->generatePDF($order_invoice_collection, PDF::TEMPLATE_DELIVERY_SLIP);
-    }
-
-    public function generateDeliverySlipPDFByIdOrderInvoice($id_order_invoice)
-    {
-        $order_invoice = new OrderInvoice((int)$id_order_invoice);
-        if (!Validate::isLoadedObject($order_invoice)) {
-            throw new PrestaShopException('Can\'t load Order Invoice object');
-        }
-
-        $this->generatePDF($order_invoice, PDF::TEMPLATE_DELIVERY_SLIP);
-    }
-
-    public function generateInvoicePDFByIdOrder($id_order)
-    {
-        $order = new Order((int)$id_order);
-        if (!Validate::isLoadedObject($order)) {
-            die(Tools::displayError('The order cannot be found within your database.'));
-        }
-
-        $order_invoice_list = $order->getInvoicesCollection();
-        Hook::exec('actionPDFInvoiceRender', ['order_invoice_list' => $order_invoice_list]);
-        $this->generatePDF($order_invoice_list, PDF::TEMPLATE_INVOICE);
-    }
-
-    public function generateInvoicePDFByIdOrderInvoice($id_order_invoice)
-    {
-        $order_invoice = new OrderInvoice((int)$id_order_invoice);
-        if (!Validate::isLoadedObject($order_invoice)) {
-            die(Tools::displayError('The order invoice cannot be found within your database.'));
-        }
-
-        Hook::exec('actionPDFInvoiceRender', ['order_invoice_list' => [$order_invoice]]);
-        $this->generatePDF($order_invoice, PDF::TEMPLATE_INVOICE);
-    }
-
-    public function generatePDF($object, $template)
-    {
-        $pdf = new PDF($object, $template, Context::getContext()->smarty);
-        $pdf->render();
+        $this->generatePDF($supplyOrder, PDF::TEMPLATE_SUPPLY_ORDER_FORM);
     }
 }
