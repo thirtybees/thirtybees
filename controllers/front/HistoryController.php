59e61ca709b75743e78cb39d2dbb462f53597423
Michael Dekker <git@michaeldekker.nl>
2017-03-18 21:45:54 +0100

    Cleanup HistoryController

--- controllers/front/HistoryController.php
+++ controllers/front/HistoryController.php
@@ -21,42 +21,63 @@
  * versions in the future. If you wish to customize PrestaShop for your
  * needs please refer to https://www.thirtybees.com for more information.
  *
- *  @author    Thirty Bees <contact@thirtybees.com>
- *  @author    PrestaShop SA <contact@prestashop.com>
- *  @copyright 2017 Thirty Bees
- *  @copyright 2007-2016 PrestaShop SA
- *  @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ * @author    Thirty Bees <contact@thirtybees.com>
+ * @author    PrestaShop SA <contact@prestashop.com>
+ * @copyright 2017 Thirty Bees
+ * @copyright 2007-2016 PrestaShop SA
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  *  PrestaShop is an internationally registered trademark & property of PrestaShop SA
  */
 
+/**
+ * Class HistoryControllerCore
+ *
+ * @since 1.0.0
+ */
 class HistoryControllerCore extends FrontController
 {
+    // @codingStandardsIgnoreStart
+    /** @var bool $auth */
     public $auth = true;
+    /** @var string $php_self */
     public $php_self = 'history';
+    /** @var string $authRedirection */
     public $authRedirection = 'history';
+    /** @var bool $ssl */
     public $ssl = true;
+    // @codingStandardsIgnoreEnd
 
+    /**
+     * Set media
+     *
+     * @return void
+     *
+     * @since 1.0.0
+     */
     public function setMedia()
     {
         parent::setMedia();
         $this->addCSS(
             [
-            _THEME_CSS_DIR_.'history.css',
-            _THEME_CSS_DIR_.'addresses.css'
+                _THEME_CSS_DIR_.'history.css',
+                _THEME_CSS_DIR_.'addresses.css',
             ]
         );
         $this->addJS(
             [
-            _THEME_JS_DIR_.'history.js',
-            _THEME_JS_DIR_.'tools.js' // retro compat themes 1.5
+                _THEME_JS_DIR_.'history.js',
+                _THEME_JS_DIR_.'tools.js', // retro compat themes 1.5
             ]
         );
         $this->addJqueryPlugin(['scrollTo', 'footable', 'footable-sort']);
     }
 
     /**
-     * Assign template vars related to page content
-     * @see FrontController::initContent()
+     * Initialize content
+     *
+     * @return void
+     *
+     * @since 1.0.0
      */
     public function initContent()
     {
@@ -64,7 +85,7 @@ class HistoryControllerCore extends FrontController
 
         if ($orders = Order::getCustomerOrders($this->context->customer->id)) {
             foreach ($orders as &$order) {
-                $myOrder = new Order((int)$order['id_order']);
+                $myOrder = new Order((int) $order['id_order']);
                 if (Validate::isLoadedObject($myOrder)) {
                     $order['virtual'] = $myOrder->isVirtual(false);
                 }
@@ -72,10 +93,10 @@ class HistoryControllerCore extends FrontController
         }
         $this->context->smarty->assign(
             [
-            'orders' => $orders,
-            'invoiceAllowed' => (int)Configuration::get('PS_INVOICE'),
-            'reorderingAllowed' => !(bool)Configuration::get('PS_DISALLOW_HISTORY_REORDERING'),
-            'slowValidation' => Tools::isSubmit('slowvalidation')
+                'orders'            => $orders,
+                'invoiceAllowed'    => (int) Configuration::get('PS_INVOICE'),
+                'reorderingAllowed' => !(bool) Configuration::get('PS_DISALLOW_HISTORY_REORDERING'),
+                'slowValidation'    => Tools::isSubmit('slowvalidation'),
             ]
         );
 
