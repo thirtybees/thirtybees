c9ce3247b36dfe2e60064209c9506d3b3404c1cf
Michael Dekker <git@michaeldekker.nl>
2017-03-19 02:14:09 +0100

    Cleanup AdminWebserviceController

--- controllers/admin/AdminWebserviceController.php
+++ controllers/admin/AdminWebserviceController.php
@@ -21,23 +21,33 @@
  * versions in the future. If you wish to customize PrestaShop for your
  * needs please refer to https://www.thirtybees.com for more information.
  *
- *  @author    Thirty Bees <contact@thirtybees.com>
- *  @author    PrestaShop SA <contact@prestashop.com>
- *  @copyright 2017 Thirty Bees
- *  @copyright 2007-2016 PrestaShop SA
- *  @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ * @author    Thirty Bees <contact@thirtybees.com>
+ * @author    PrestaShop SA <contact@prestashop.com>
+ * @copyright 2017 Thirty Bees
+ * @copyright 2007-2016 PrestaShop SA
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  *  PrestaShop is an internationally registered trademark & property of PrestaShop SA
  */
 
 /**
- * @property WebserviceKey $object
+ * Class AdminWebserviceControllerCore
+ *
+ * @since 1.0.0
  */
 class AdminWebserviceControllerCore extends AdminController
 {
-    /** this will be filled later */
+    // @codingStandardsIgnoreStart
+    /** @var array $fields_form */
     public $fields_form = ['webservice form'];
+    /** @var bool $toolbar_scroll */
     protected $toolbar_scroll = false;
+    // @codingStandardsIgnoreEnd
 
+    /**
+     * AdminWebserviceControllerCore constructor.
+     *
+     * @since 1.0.0
+     */
     public function __construct()
     {
         $this->bootstrap = true;
@@ -50,145 +60,150 @@ class AdminWebserviceControllerCore extends AdminController
 
         $this->bulk_actions = [
             'delete' => [
-                'text' => $this->l('Delete selected'),
+                'text'    => $this->l('Delete selected'),
                 'confirm' => $this->l('Delete selected items?'),
-                'icon' => 'icon-trash'
-            ]
+                'icon'    => 'icon-trash',
+            ],
         ];
 
         $this->fields_list = [
-            'key' => [
+            'key'         => [
                 'title' => $this->l('Key'),
-                'class' => 'fixed-width-md'
+                'class' => 'fixed-width-md',
             ],
             'description' => [
-                'title' => $this->l('Key description'),
-                'align' => 'left',
-                'orderby' => false
-            ],
-            'active' => [
-                'title' => $this->l('Enabled'),
-                'align' => 'center',
-                'active' => 'status',
-                'type' => 'bool',
+                'title'   => $this->l('Key description'),
+                'align'   => 'left',
                 'orderby' => false,
-                'class' => 'fixed-width-xs'
-            ]
+            ],
+            'active'      => [
+                'title'   => $this->l('Enabled'),
+                'align'   => 'center',
+                'active'  => 'status',
+                'type'    => 'bool',
+                'orderby' => false,
+                'class'   => 'fixed-width-xs',
+            ],
         ];
 
         $this->fields_options = [
-                'general' => [
-                    'title' =>    $this->l('Configuration'),
-                    'fields' =>    [
-                        'PS_WEBSERVICE' => [
-                            'title' => $this->l('Enable thirty bees\' webservice'),
-                            'desc' => $this->l('Before activating the webservice, you must be sure to: ').
-                                                '<ol>
+            'general' => [
+                'title'  => $this->l('Configuration'),
+                'fields' => [
+                    'PS_WEBSERVICE' => [
+                        'title' => $this->l('Enable thirty bees\' webservice'),
+                        'desc'  => $this->l('Before activating the webservice, you must be sure to: ').
+                            '<ol>
 													<li>'.$this->l('Check that URL rewriting is available on this server.').'</li>
 													<li>'.$this->l('Check that the five methods GET, POST, PUT, DELETE and HEAD are supported by this server.').'</li>
 												</ol>',
-                            'cast' => 'intval',
-                            'type' => 'bool'
-                        ],
+                        'cast'  => 'intval',
+                        'type'  => 'bool',
                     ],
-                    'submit' => ['title' => $this->l('Save')]
                 ],
+                'submit' => ['title' => $this->l('Save')],
+            ],
         ];
 
         if (!defined('_PS_HOST_MODE_')) {
             $this->fields_options['general']['fields']['PS_WEBSERVICE_CGI_HOST'] = [
                 'title' => $this->l('Enable CGI mode for PHP'),
-                'desc' => $this->l('Before choosing "Yes", check that PHP is not configured as an Apache module on your server.'),
-                'cast' => 'intval',
-                'type' => 'bool'
+                'desc'  => $this->l('Before choosing "Yes", check that PHP is not configured as an Apache module on your server.'),
+                'cast'  => 'intval',
+                'type'  => 'bool',
             ];
         }
 
         parent::__construct();
     }
 
+    /**
+     * Initialize page header toolbar
+     *
+     * @return void
+     *
+     * @since 1.0.0
+     */
     public function initPageHeaderToolbar()
     {
         if (empty($this->display)) {
             $this->page_header_toolbar_btn['new_webservice'] = [
                 'href' => self::$currentIndex.'&addwebservice_account&token='.$this->token,
                 'desc' => $this->l('Add new webservice key', null, null, false),
-                'icon' => 'process-icon-new'
+                'icon' => 'process-icon-new',
             ];
         }
 
         parent::initPageHeaderToolbar();
     }
 
-
-    protected function processUpdateOptions()
-    {
-        parent::processUpdateOptions();
-        Tools::generateHtaccess();
-    }
-
+    /**
+     * @return string
+     *
+     * @since 1.0.0
+     */
     public function renderForm()
     {
         $this->fields_form = [
             'legend' => [
                 'title' => $this->l('Webservice Accounts'),
-                'icon' => 'icon-lock'
+                'icon'  => 'icon-lock',
             ],
-            'input' => [
+            'input'  => [
                 [
-                    'type' => 'textbutton',
-                    'label' => $this->l('Key'),
-                    'name' => 'key',
-                    'id' => 'code',
+                    'type'     => 'textbutton',
+                    'label'    => $this->l('Key'),
+                    'name'     => 'key',
+                    'id'       => 'code',
                     'required' => true,
-                    'hint' => $this->l('Webservice account key.'),
-                    'button' => [
-                        'label' => $this->l('Generate!'),
+                    'hint'     => $this->l('Webservice account key.'),
+                    'button'   => [
+                        'label'      => $this->l('Generate!'),
                         'attributes' => [
-                            'onclick' => 'gencode(32)'
-                        ]
-                    ]
+                            'onclick' => 'gencode(32)',
+                        ],
+                    ],
                 ],
                 [
-                    'type' => 'textarea',
+                    'type'  => 'textarea',
                     'label' => $this->l('Key description'),
-                    'name' => 'description',
-                    'rows' => 3,
-                    'cols' => 110,
-                    'hint' => $this->l('Quick description of the key: who it is for, what permissions it has, etc.'),
+                    'name'  => 'description',
+                    'rows'  => 3,
+                    'cols'  => 110,
+                    'hint'  => $this->l('Quick description of the key: who it is for, what permissions it has, etc.'),
                 ],
                 [
-                    'type' => 'switch',
-                    'label' => $this->l('Status'),
-                    'name' => 'active',
+                    'type'     => 'switch',
+                    'label'    => $this->l('Status'),
+                    'name'     => 'active',
                     'required' => false,
-                    'is_bool' => true,
-                    'values' => [
+                    'is_bool'  => true,
+                    'values'   => [
                         [
-                            'id' => 'active_on',
+                            'id'    => 'active_on',
                             'value' => 1,
-                            'label' => $this->l('Enabled')
+                            'label' => $this->l('Enabled'),
                         ],
                         [
-                            'id' => 'active_off',
+                            'id'    => 'active_off',
                             'value' => 0,
-                            'label' => $this->l('Disabled')
-                        ]
-                    ]
+                            'label' => $this->l('Disabled'),
+                        ],
+                    ],
                 ],
                 [
-                    'type' => 'resources',
+                    'type'  => 'resources',
                     'label' => $this->l('Permissions'),
-                    'name' => 'resources',
-                ]
-            ]
+                    'name'  => 'resources',
+                ],
+            ],
         ];
 
         if (Shop::isFeatureActive()) {
             $this->fields_form['input'][] = [
-                'type' => 'shop',
+                'type'  => 'shop',
                 'label' => $this->l('Shop association'),
-                'name' => 'checkBoxShopAsso',
+                'name'  => 'checkBoxShopAsso',
             ];
         }
 
@@ -197,20 +212,27 @@ class AdminWebserviceControllerCore extends AdminController
         ];
 
         if (!($obj = $this->loadObject(true))) {
-            return;
+            return '';
         }
 
         $ressources = WebserviceRequest::getResources();
         $permissions = WebserviceKey::getPermissionForAccount($obj->key);
 
         $this->tpl_form_vars = [
-            'ressources' => $ressources,
-            'permissions' => $permissions
+            'ressources'  => $ressources,
+            'permissions' => $permissions,
         ];
 
         return parent::renderForm();
     }
 
+    /**
+     * Initialize content
+     *
+     * @return void
+     *
+     * @since 1.0.0
+     */
     public function initContent()
     {
         if ($this->display != 'add' && $this->display != 'edit') {
@@ -221,70 +243,20 @@ class AdminWebserviceControllerCore extends AdminController
     }
 
     /**
-     * Function used to render the options for this controller
+     * @return void
+     *
+     * @since 1.0.0
      */
-    public function renderOptions()
-    {
-        if ($this->fields_options && is_array($this->fields_options)) {
-            $helper = new HelperOptions($this);
-            $this->setHelperDisplay($helper);
-            $helper->toolbar_scroll = true;
-            $helper->toolbar_btn = [
-                'save' => [
-                                'href' => '#',
-                                'desc' => $this->l('Save')
-                ]
-            ];
-            $helper->id = $this->id;
-            $helper->tpl_vars = $this->tpl_option_vars;
-            $options = $helper->generateOptions($this->fields_options);
-
-            return $options;
-        }
-    }
-
-    public function initProcess()
-    {
-        parent::initProcess();
-        // This is a composite page, we don't want the "options" display mode
-        if ($this->display == 'options') {
-            $this->display = '';
-        }
-    }
-
-    public function postProcess()
-    {
-        if (Tools::getValue('key') && strlen(Tools::getValue('key')) < 32) {
-            $this->errors[] = Tools::displayError('Key length must be 32 character long.');
-        }
-        if (WebserviceKey::keyExists(Tools::getValue('key')) && !Tools::getValue('id_webservice_account')) {
-            $this->errors[] = Tools::displayError('This key already exists.');
-        }
-        return parent::postProcess();
-    }
-
-    protected function afterAdd($object)
-    {
-        Tools::generateHtaccess();
-        WebserviceKey::setPermissionForAccount($object->id, Tools::getValue('resources', []));
-    }
-
-    protected function afterUpdate($object)
-    {
-        Tools::generateHtaccess();
-        WebserviceKey::setPermissionForAccount($object->id, Tools::getValue('resources', []));
-    }
-
     public function checkForWarning()
     {
         if (strpos($_SERVER['SERVER_SOFTWARE'], 'Apache') === false) {
             $this->warnings[] = $this->l('To avoid operating problems, please use an Apache server.');
             if (function_exists('apache_get_modules')) {
-                $apache_modules = apache_get_modules();
-                if (!in_array('mod_auth_basic', $apache_modules)) {
+                $apacheModules = apache_get_modules();
+                if (!in_array('mod_auth_basic', $apacheModules)) {
                     $this->warnings[] = $this->l('Please activate the \'mod_auth_basic\' Apache module to allow authentication of thirty bees\' webservice.');
                 }
-                if (!in_array('mod_rewrite', $apache_modules)) {
+                if (!in_array('mod_rewrite', $apacheModules)) {
                     $this->warnings[] = $this->l('Please activate the \'mod_rewrite\' Apache module to allow the thirty bees webservice.');
                 }
             } else {
@@ -301,11 +273,115 @@ class AdminWebserviceControllerCore extends AdminController
         foreach ($this->_list as $k => $item) {
             if ($item['is_module'] && $item['class_name'] && $item['module_name'] &&
                 ($instance = Module::getInstanceByName($item['module_name'])) &&
-                !$instance->useNormalPermissionBehaviour()) {
+                !$instance->useNormalPermissionBehaviour()
+            ) {
                 unset($this->_list[$k]);
             }
         }
 
         $this->renderList();
+    }
+
+    /**
+     * Function used to render the options for this controller
+     *
+     * @return string
+     *
+     * @sinc 1.0.0
+     */
+    public function renderOptions()
+    {
+        if ($this->fields_options && is_array($this->fields_options)) {
+            $helper = new HelperOptions($this);
+            $this->setHelperDisplay($helper);
+            $helper->toolbar_scroll = true;
+            $helper->toolbar_btn = [
+                'save' => [
+                    'href' => '#',
+                    'desc' => $this->l('Save'),
+                ],
+            ];
+            $helper->id = $this->id;
+            $helper->tpl_vars = $this->tpl_option_vars;
+            $options = $helper->generateOptions($this->fields_options);
+
+            return $options;
+        }
+
+        return '';
+    }
+
+    /**
+     * Initialize processing
+     *
+     * @return void
+     *
+     * @since 1.0.0
+     */
+    public function initProcess()
+    {
+        parent::initProcess();
+        // This is a composite page, we don't want the "options" display mode
+        if ($this->display == 'options') {
+            $this->display = '';
+        }
+    }
+
+    /**
+     * Post processing
+     *
+     * @return bool
+     *
+     * @since 1.0.0
+     */
+    public function postProcess()
+    {
+        if (Tools::getValue('key') && strlen(Tools::getValue('key')) < 32) {
+            $this->errors[] = Tools::displayError('Key length must be 32 character long.');
+        }
+        if (WebserviceKey::keyExists(Tools::getValue('key')) && !Tools::getValue('id_webservice_account')) {
+            $this->errors[] = Tools::displayError('This key already exists.');
+        }
+
+        return parent::postProcess();
+    }
+
+    /**
+     * Process update options
+     *
+     * @return void
+     *
+     * @since 1.0.0
+     */
+    protected function processUpdateOptions()
+    {
+        parent::processUpdateOptions();
+        Tools::generateHtaccess();
+    }
+
+    /**
+     * @param ObjectModel $object
+     *
+     * @return void
+     *
+     * @since 1.0.0
+     */
+    protected function afterAdd($object)
+    {
+        Tools::generateHtaccess();
+        WebserviceKey::setPermissionForAccount($object->id, Tools::getValue('resources', []));
+    }
+
+    /**
+     * @param ObjectModel $object
+     *
+     * @return void
+     *
+     * @since 1.0.0
+     */
+    protected function afterUpdate($object)
+    {
+        Tools::generateHtaccess();
+        WebserviceKey::setPermissionForAccount($object->id, Tools::getValue('resources', []));
     }
 }
