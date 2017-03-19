7db9a5d6378e8a50eea9d3dcd8cb1202f251b5ad
Michael Dekker <git@michaeldekker.nl>
2017-03-19 02:12:18 +0100

    Cleanup AdminTabsController

--- controllers/admin/AdminTabsController.php
+++ controllers/admin/AdminTabsController.php
@@ -21,21 +21,31 @@
  * versions in the future. If you wish to customize PrestaShop for your
  * needs please refer to https://www.thirtybees.com for more information.
  *
- *  @author    Thirty Bees <contact@thirtybees.com>
- *  @author    PrestaShop SA <contact@prestashop.com>
- *  @copyright 2017 Thirty Bees
- *  @copyright 2007-2016 PrestaShop SA
- *  @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ * @author    Thirty Bees <contact@thirtybees.com>
+ * @author    PrestaShop SA <contact@prestashop.com>
+ * @copyright 2017 Thirty Bees
+ * @copyright 2007-2016 PrestaShop SA
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  *  PrestaShop is an internationally registered trademark & property of PrestaShop SA
  */
 
 /**
- * @property Tab $object
+ * Class AdminTabsControllerCore
+ *
+ * @since 1.0.0
  */
 class AdminTabsControllerCore extends AdminController
 {
+    // @codingStandardsIgnoreStart
+    /** @var string $position_identifier */
     protected $position_identifier = 'id_tab';
+    // @codingStandardsIgnoreEnd
 
+    /**
+     * AdminTabsControllerCore constructor.
+     *
+     * @since 1.0.0
+     */
     public function __construct()
     {
         $this->bootstrap = true;
@@ -47,50 +57,57 @@ class AdminTabsControllerCore extends AdminController
         $this->lang = true;
         $this->fieldImageSettings = [
             'name' => 'icon',
-            'dir' => 't'
+            'dir'  => 't',
         ];
         $this->imageType = 'gif';
         $this->bulk_actions = [
             'delete' => [
-                'text' => $this->l('Delete selected'),
+                'text'    => $this->l('Delete selected'),
                 'confirm' => $this->l('Delete selected items?'),
-                'icon' => 'icon-trash'
-            ]
+                'icon'    => 'icon-trash',
+            ],
         ];
         $this->fields_list = [
-            'id_tab' => [
+            'id_tab'     => [
                 'title' => $this->l('ID'),
                 'align' => 'center',
-                'class' => 'fixed-width-xs'
+                'class' => 'fixed-width-xs',
             ],
-            'name' => [
-                'title' => $this->l('Name')
+            'name'       => [
+                'title' => $this->l('Name'),
             ],
             'class_name' => [
-                'title' => $this->l('Class')
+                'title' => $this->l('Class'),
             ],
-            'module' => [
-                'title' => $this->l('Module')
+            'module'     => [
+                'title' => $this->l('Module'),
             ],
-            'active' => [
-                'title' => $this->l('Enabled'),
-                'align' => 'center',
-                'active' => 'status',
-                'type' => 'bool',
-                'orderby' => false
+            'active'     => [
+                'title'   => $this->l('Enabled'),
+                'align'   => 'center',
+                'active'  => 'status',
+                'type'    => 'bool',
+                'orderby' => false,
             ],
-            'position' => [
-                'title' => $this->l('Position'),
+            'position'   => [
+                'title'      => $this->l('Position'),
                 'filter_key' => 'a!position',
-                'position' => 'position',
-                'align' => 'center',
-                'class' => 'fixed-width-md'
-            ]
+                'position'   => 'position',
+                'align'      => 'center',
+                'class'      => 'fixed-width-md',
+            ],
         ];
 
         parent::__construct();
     }
 
+    /**
+     * Initialize page header toolbar
+     *
+     * @return void
+     *
+     * @since 1.0.0
+     */
     public function initPageHeaderToolbar()
     {
         $this->page_header_toolbar_title = $this->l('Menus');
@@ -99,13 +116,13 @@ class AdminTabsControllerCore extends AdminController
             $this->page_header_toolbar_btn['back_to_list'] = [
                 'href' => Context::getContext()->link->getAdminLink('AdminTabs'),
                 'desc' => $this->l('Back to list', null, null, false),
-                'icon' => 'process-icon-back'
+                'icon' => 'process-icon-back',
             ];
         } elseif (empty($this->display)) {
             $this->page_header_toolbar_btn['new_menu'] = [
                 'href' => self::$currentIndex.'&addtab&token='.$this->token,
                 'desc' => $this->l('Add new menu', null, null, false),
-                'icon' => 'process-icon-new'
+                'icon' => 'process-icon-new',
             ];
         }
 
@@ -114,7 +131,12 @@ class AdminTabsControllerCore extends AdminController
 
     /**
      * AdminController::renderForm() override
+     *
      * @see AdminController::renderForm()
+     *
+     * @return string
+     *
+     * @since 1.0.0
      */
     public function renderForm()
     {
@@ -129,77 +151,77 @@ class AdminTabsControllerCore extends AdminController
         }
 
         // added category "Home" in var $tabs
-        $tab_zero = [
+        $tabZero = [
             'id_tab' => 0,
-            'name' => $this->l('Home')
+            'name'   => $this->l('Home'),
         ];
-        array_unshift($tabs, $tab_zero);
+        array_unshift($tabs, $tabZero);
 
         $this->fields_form = [
             'legend' => [
                 'title' => $this->l('Menus'),
-                'icon' => 'icon-list-ul'
+                'icon'  => 'icon-list-ul',
             ],
-            'input' => [
+            'input'  => [
                 [
-                    'type' => 'hidden',
-                    'name' => 'position',
-                    'required' => false
-                ],
-                [
-                    'type' => 'text',
-                    'label' => $this->l('Name'),
-                    'name' => 'name',
-                    'lang' => true,
-                    'required' => true,
-                    'hint' => $this->l('Invalid characters:').' &lt;&gt;;=#{}'
-                ],
-                [
-                    'type' => 'text',
-                    'label' => $this->l('Class'),
-                    'name' => 'class_name',
-                    'required' => true
-                ],
-                [
-                    'type' => 'text',
-                    'label' => $this->l('Module'),
-                    'name' => 'module'
-                ],
-                [
-                    'type' => 'switch',
-                    'label' => $this->l('Status'),
-                    'name' => 'active',
+                    'type'     => 'hidden',
+                    'name'     => 'position',
                     'required' => false,
-                    'is_bool' => true,
-                    'values' => [
+                ],
+                [
+                    'type'     => 'text',
+                    'label'    => $this->l('Name'),
+                    'name'     => 'name',
+                    'lang'     => true,
+                    'required' => true,
+                    'hint'     => $this->l('Invalid characters:').' &lt;&gt;;=#{}',
+                ],
+                [
+                    'type'     => 'text',
+                    'label'    => $this->l('Class'),
+                    'name'     => 'class_name',
+                    'required' => true,
+                ],
+                [
+                    'type'  => 'text',
+                    'label' => $this->l('Module'),
+                    'name'  => 'module',
+                ],
+                [
+                    'type'     => 'switch',
+                    'label'    => $this->l('Status'),
+                    'name'     => 'active',
+                    'required' => false,
+                    'is_bool'  => true,
+                    'values'   => [
                         [
-                            'id' => 'active_on',
+                            'id'    => 'active_on',
                             'value' => 1,
-                            'label' => $this->l('Enabled')
+                            'label' => $this->l('Enabled'),
                         ],
                         [
-                            'id' => 'active_off',
+                            'id'    => 'active_off',
                             'value' => 0,
-                            'label' => $this->l('Disabled')
-                        ]
+                            'label' => $this->l('Disabled'),
+                        ],
                     ],
-                    'hint' => $this->l('Show or hide menu.')
+                    'hint'     => $this->l('Show or hide menu.'),
                 ],
             ],
             'submit' => [
                 'title' => $this->l('Save'),
-            ]
+            ],
         ];
 
         $this->fields_form['input'][] = [
-            'type' => 'select',
-            'label' => $this->l('Parent'),
-            'name' => 'id_parent',
+            'type'    => 'select',
+            'label'   => $this->l('Parent'),
+            'name'    => 'id_parent',
             'options' => [
                 'query' => $tabs,
-                'id' => 'id_tab',
-                'name' => 'name'
-            ]
+                'id'    => 'id_tab',
+                'name'  => 'name',
+            ],
         ];
 
         return parent::renderForm();
@@ -207,7 +229,12 @@ class AdminTabsControllerCore extends AdminController
 
     /**
      * AdminController::renderList() override
+     *
      * @see AdminController::renderList()
+     *
+     * @return string
+     *
+     * @since 1.0.0
      */
     public function renderList()
     {
@@ -221,6 +248,13 @@ class AdminTabsControllerCore extends AdminController
         return parent::renderList();
     }
 
+    /**
+     * Initialize processing
+     *
+     * @return void
+     *
+     * @since 1.0.0
+     */
     public function initProcess()
     {
         if (Tools::getIsset('details'.$this->table)) {
@@ -233,9 +267,16 @@ class AdminTabsControllerCore extends AdminController
             $this->list_id = 'tab';
         }
 
-        return parent::initProcess();
+        parent::initProcess();
     }
 
+    /**
+     * Render details
+     *
+     * @return string
+     *
+     * @since 1.0.0
+     */
     public function renderDetails()
     {
         if (($id = Tools::getValue('id_tab'))) {
@@ -251,70 +292,83 @@ class AdminTabsControllerCore extends AdminController
 
             $this->_select = 'b.*';
             $this->_join = 'LEFT JOIN `'._DB_PREFIX_.'tab_lang` b ON (b.`id_tab` = a.`id_tab` AND b.`id_lang` = '.$this->context->language->id.')';
-            $this->_where = 'AND a.`id_parent` = '.(int)$id;
+            $this->_where = 'AND a.`id_parent` = '.(int) $id;
             $this->_orderBy = 'position';
             $this->_use_found_rows = false;
 
             self::$currentIndex = self::$currentIndex.'&details'.$this->table;
             $this->processFilter();
+
             return parent::renderList();
         }
+
+        return '';
     }
 
+    /**
+     * Post processing
+     *
+     * @return void
+     *
+     * @since 1.0.0
+     */
     public function postProcess()
     {
         /* PrestaShop demo mode */
         if (_PS_MODE_DEMO_) {
             $this->errors[] = Tools::displayError('This functionality has been disabled.');
+
             return;
         }
         /* PrestaShop demo mode*/
 
-        if (($id_tab = (int)Tools::getValue('id_tab')) && ($direction = Tools::getValue('move')) && Validate::isLoadedObject($tab = new Tab($id_tab))) {
+        if (($idTab = (int) Tools::getValue('id_tab')) && ($direction = Tools::getValue('move')) && Validate::isLoadedObject($tab = new Tab($idTab))) {
             if ($tab->move($direction)) {
                 Tools::redirectAdmin(self::$currentIndex.'&token='.$this->token);
             }
         } elseif (Tools::getValue('position') && !Tools::isSubmit('submitAdd'.$this->table)) {
             if ($this->tabAccess['edit'] !== '1') {
                 $this->errors[] = Tools::displayError('You do not have permission to edit this.');
-            } elseif (!Validate::isLoadedObject($object = new Tab((int)Tools::getValue($this->identifier)))) {
+            } elseif (!Validate::isLoadedObject($object = new Tab((int) Tools::getValue($this->identifier)))) {
                 $this->errors[] = Tools::displayError('An error occurred while updating the status for an object.').
                     ' <b>'.$this->table.'</b> '.Tools::displayError('(cannot load object)');
             }
-            if (!$object->updatePosition((int)Tools::getValue('way'), (int)Tools::getValue('position'))) {
+            if (!$object->updatePosition((int) Tools::getValue('way'), (int) Tools::getValue('position'))) {
                 $this->errors[] = Tools::displayError('Failed to update the position.');
             } else {
                 Tools::redirectAdmin(self::$currentIndex.'&conf=5&token='.Tools::getAdminTokenLite('AdminTabs'));
             }
         } elseif (Tools::isSubmit('submitAdd'.$this->table) && Tools::getValue('id_tab') === Tools::getValue('id_parent')) {
             $this->errors[] = Tools::displayError('You can\'t put this menu inside itself. ');
-        } elseif (Tools::isSubmit('submitAdd'.$this->table) && $id_parent = (int)Tools::getValue('id_parent')) {
-            $this->redirect_after = self::$currentIndex.'&id_'.$this->table.'='.$id_parent.'&details'.$this->table.'&conf=4&token='.$this->token;
+        } elseif (Tools::isSubmit('submitAdd'.$this->table) && $idParent = (int) Tools::getValue('id_parent')) {
+            $this->redirect_after = self::$currentIndex.'&id_'.$this->table.'='.$idParent.'&details'.$this->table.'&conf=4&token='.$this->token;
         } elseif (isset($_GET['details'.$this->table]) && is_array($this->bulk_actions)) {
-            $submit_bulk_actions = array_merge(
+            $submitBulkActions = array_merge(
                 [
-                'enableSelection' => [
-                    'text' => $this->l('Enable selection'),
-                    'icon' => 'icon-power-off text-success'
+                    'enableSelection'  => [
+                        'text' => $this->l('Enable selection'),
+                        'icon' => 'icon-power-off text-success',
+                    ],
+                    'disableSelection' => [
+                        'text' => $this->l('Disable selection'),
+                        'icon' => 'icon-power-off text-danger',
+                    ],
                 ],
-                'disableSelection' => [
-                    'text' => $this->l('Disable selection'),
-                    'icon' => 'icon-power-off text-danger'
-                ]
-                ], $this->bulk_actions);
-            foreach ($submit_bulk_actions as $bulk_action => $params) {
-                if (Tools::isSubmit('submitBulk' . $bulk_action . $this->table) || Tools::isSubmit('submitBulk' . $bulk_action)) {
+                $this->bulk_actions
+            );
+            foreach ($submitBulkActions as $bulkAction => $params) {
+                if (Tools::isSubmit('submitBulk'.$bulkAction.$this->table) || Tools::isSubmit('submitBulk'.$bulkAction)) {
                     if ($this->tabAccess['edit'] === '1') {
-                        $this->action = 'bulk' . $bulk_action;
-                        $this->boxes = Tools::getValue($this->list_id . 'Box');
+                        $this->action = 'bulk'.$bulkAction;
+                        $this->boxes = Tools::getValue($this->list_id.'Box');
                     } else {
                         $this->errors[] = Tools::displayError('You do not have permission to edit this.');
                     }
                     break;
                 } elseif (Tools::isSubmit('submitBulk')) {
                     if ($this->tabAccess['edit'] === '1') {
-                        $this->action = 'bulk' . Tools::getValue('select_submitBulk');
-                        $this->boxes = Tools::getValue($this->list_id . 'Box');
+                        $this->action = 'bulk'.Tools::getValue('select_submitBulk');
+                        $this->boxes = Tools::getValue($this->list_id.'Box');
                     } else {
                         $this->errors[] = Tools::displayError('You do not have permission to edit this.');
                     }
@@ -333,19 +387,17 @@ class AdminTabsControllerCore extends AdminController
         }
     }
 
-    protected function afterImageUpload()
-    {
-        /** @var Tab $obj */
-        if (!($obj = $this->loadObject(true))) {
-            return;
-        }
-        @rename(_PS_IMG_DIR_.'t/'.$obj->id.'.gif', _PS_IMG_DIR_.'t/'.$obj->class_name.'.gif');
-    }
-
+    /**
+     * Ajax process update positions
+     *
+     * @return void
+     *
+     * @since 1.0.0
+     */
     public function ajaxProcessUpdatePositions()
     {
-        $way = (int)(Tools::getValue('way'));
-        $id_tab = (int)(Tools::getValue('id'));
+        $way = (int) (Tools::getValue('way'));
+        $idTab = (int) (Tools::getValue('id'));
         $positions = Tools::getValue('tab');
 
         // when changing positions in a tab sub-list, the first array value is empty and needs to be removed
@@ -358,19 +410,35 @@ class AdminTabsControllerCore extends AdminController
         foreach ($positions as $position => $value) {
             $pos = explode('_', $value);
 
-            if (isset($pos[2]) && (int)$pos[2] === $id_tab) {
-                if ($tab = new Tab((int)$pos[2])) {
+            if (isset($pos[2]) && (int) $pos[2] === $idTab) {
+                if ($tab = new Tab((int) $pos[2])) {
                     if (isset($position) && $tab->updatePosition($way, $position)) {
-                        echo 'ok position '.(int)$position.' for tab '.(int)$pos[1].'\r\n';
+                        echo 'ok position '.(int) $position.' for tab '.(int) $pos[1].'\r\n';
                     } else {
-                        echo '{"hasError" : true, "errors" : "Can not update tab '.(int)$id_tab.' to position '.(int)$position.' "}';
+                        echo '{"hasError" : true, "errors" : "Can not update tab '.(int) $idTab.' to position '.(int) $position.' "}';
                     }
                 } else {
-                    echo '{"hasError" : true, "errors" : "This tab ('.(int)$id_tab.') can t be loaded"}';
+                    echo '{"hasError" : true, "errors" : "This tab ('.(int) $idTab.') can t be loaded"}';
                 }
 
                 break;
             }
         }
+    }
+
+    /**
+     * After image upload
+     *
+     * @return void
+     *
+     * @since 1.0.0
+     */
+    protected function afterImageUpload()
+    {
+        /** @var Tab $obj */
+        if (!($obj = $this->loadObject(true))) {
+            return;
+        }
+        @rename(_PS_IMG_DIR_.'t/'.$obj->id.'.gif', _PS_IMG_DIR_.'t/'.$obj->class_name.'.gif');
     }
 }
