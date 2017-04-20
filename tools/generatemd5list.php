2470cc36b2c63649b08b0cd2d6e736982610efe7
Markus Hitter <mah@jump-ing.de>
2017-04-20 15:19:14 +0200

    Introduce tools/generatemd5list.php.
    That's a PHP version of tools/generatemd5list.js. The JavaScript
    version was suspect to create subtle differences.
    
    In a develoment installation, output of the JS version is the same,
    except for order/sorting and some files directly in admin/ missing.
    While building a package however, the JS version picks up some 900
    additional files, some of which can't be parsed by the back office
    verifier.

---
+++ tools/generatemd5list.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Copyright (C) 2017 Thirty Bees
+ *
+ * Thirty Bees is an extension to the PrestaShop e-commerce software developed by PrestaShop SA
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@thirtybees.com so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade thirty bees to newer
+ * versions in the future. If you want to customize thirty bees for your
+ * needs please refer to https://www.thirtybees.com for more information.
+ *
+ *  @author    Thirty Bees <contact@thirtybees.com>
+ *  @copyright 2017 Thirty Bees
+ *  @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+
+if (!defined('_PS_ADMIN_DIR_')) {
+  // Find admin dir even on non-developer installations.
+  $adminDir = null;
+  $rootDir = dir(dirname(__FILE__).'/..');
+  while (($entry = $rootDir->read())) {
+    $found = strpos($entry, 'admin');
+    if ($found !== false && $found === 0) {
+      $adminDir = $rootDir->path.'/'.$entry;
+      break;
+    }
+  }
+  $rootDir->close();
+  define('_PS_ADMIN_DIR_', $adminDir);
+}
+
+// These should work with or without installation.
+require_once(dirname(__FILE__).'/../config/defines.inc.php');
+require_once(_PS_ROOT_DIR_.'/config/autoload.php');
+
+AdminInformationControllerCore::generateMd5List();
