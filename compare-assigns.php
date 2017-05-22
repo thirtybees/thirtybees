b6762d71dcf198701f5996cb3b219f24bec0c26a
Markus Hitter <mah@jump-ing.de>
2017-05-22 21:03:26 +0200

    Theme17, DEBUG: teach comparison tool to dive into subarrays.
    Huge help for comparing this huge Smarty assignment 'product'.
    To dive into a subarray, simply add the names of these arrays
    as parameters. Like:
    
      php compare-assigns.php product

--- compare-assigns.php
+++ compare-assigns.php
@@ -33,23 +33,46 @@ class Product {
 eval('$set16 = '.file_get_contents('config/debug').';');
 eval('$set17 = '.file_get_contents('../www.reprap-diy.com/shop2/config/debug').';');
 
+// Command line parameters dive into sub-arrays.
+array_shift($argv);
+$toplevel = true;
+foreach ($argv as $argument) {
+  print("Diving to sub-array '$argument'.\n");
+
+  $set16 = $set16[$argument];
+  $set17 = $set17[$argument];
+
+  foreach (['set16', 'set17'] as $setName) {
+    if (is_array(${$setName}) && count(${$setName}) === 3
+        && array_keys(${$setName})[0] === 'value'
+        && array_keys(${$setName})[1] === 'nocache'
+        && array_keys(${$setName})[2] === 'scope') {
+      // It's the Smarty wrapper array.
+      ${$setName} = ${$setName}['value'];
+    }
+  }
+
+  $toplevel = false;
+}
+print("\n");
+
 print("# Keys in 16, but not in 17:\n");
 foreach (array_keys($set16) as $key) {
   // Ignore fields assigned by modules.
-  if (in_array($key, [
-                        'ctheme',  // ctconfiguration
-                     ])) {
+  if ($toplevel && in_array($key, [
+                                    'ctheme',  // ctconfiguration
+                                  ])) {
     continue;
   }
 
   // Ignore fields introduced for module retrocompatibility.
-  if (in_array($key, [
-                        'request',
-                        'shop_name',
-                        'meta_title',
-                        'meta_description',
-                        'PS_CATALOG_MODE',
-                     ])) {
+  if ($toplevel && in_array($key, [
+                                      'request',
+                                      'shop_name',
+                                      'meta_title',
+                                      'meta_description',
+                                      'PS_CATALOG_MODE',
+                                  ])) {
     continue;
   }
 
