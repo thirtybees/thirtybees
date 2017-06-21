2044a7add2406473c4aed8c2484551f81557529d
Michael Dekker <git@michaeldekker.nl>
2017-06-22 01:20:37 +0200

    Optimize AdminLocalizationController db queries

--- controllers/admin/AdminLocalizationController.php
+++ controllers/admin/AdminLocalizationController.php
@@ -164,7 +164,7 @@ class AdminLocalizationControllerCore extends AdminController
                 'validation' => 'isAnything',
                 'type'       => 'select',
                 'class'      => 'chosen',
-                'list'       => Db::getInstance(_PS_USE_SQL_SLAVE_)->executeS('SELECT `name` FROM `'._DB_PREFIX_.'timezone`'),
+                'list'       => Db::getInstance(_PS_USE_SQL_SLAVE_)->executeS((new DbQuery())->select('`name`')->from('timezone')),
                 'identifier' => 'name',
                 'visibility' => Shop::CONTEXT_ALL,
             ];
