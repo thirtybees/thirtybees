de1c601d324667697f5d90f974d8f153834bb5d3
Michael Dekker <git@michaeldekker.nl>
2017-06-24 02:42:56 +0200

    Select CacheFs by default

--- controllers/admin/AdminPerformanceController.php
+++ controllers/admin/AdminPerformanceController.php
@@ -774,7 +774,7 @@ class AdminPerformanceControllerCore extends AdminController
         ];
         $depth = Configuration::get('PS_CACHEFS_DIRECTORY_DEPTH');
         $this->fields_value['TB_CACHE_ENABLED'] = (bool) Configuration::get('TB_CACHE_ENABLED');
-        $this->fields_value['TB_CACHE_SYSTEM'] = Configuration::get('TB_CACHE_SYSTEM');
+        $this->fields_value['TB_CACHE_SYSTEM'] = Configuration::get('TB_CACHE_SYSTEM') ?: 'CacheFs';
         $this->fields_value['ps_cache_fs_directory_depth'] = $depth ? $depth : 1;
         $this->tpl_form_vars['memcached_servers'] = CacheMemcache::getMemcachedServers();
         $this->tpl_form_vars['redis_servers'] = CacheRedis::getRedisServers();
