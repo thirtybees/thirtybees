4eea0f607cf162dbf0cc43dba2085e314751287b
Michael Dekker <git@michaeldekker.nl>
2017-06-25 03:50:15 +0200

    Cleanup Helper

--- classes/helper/Helper.php
+++ classes/helper/Helper.php
@@ -21,11 +21,11 @@
  * versions in the future. If you wish to customize PrestaShop for your
  * needs please refer to https://www.thirtybees.com for more information.
  *
- *  @author    Thirty Bees <contact@thirtybees.com>
- *  @author    PrestaShop SA <contact@prestashop.com>
- *  @copyright 2017 Thirty Bees
- *  @copyright 2007-2016 PrestaShop SA
- *  @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ * @author    Thirty Bees <contact@thirtybees.com>
+ * @author    PrestaShop SA <contact@prestashop.com>
+ * @copyright 2017 Thirty Bees
+ * @copyright 2007-2016 PrestaShop SA
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  *  PrestaShop is an internationally registered trademark & property of PrestaShop SA
  */
 
@@ -37,46 +37,46 @@
 class HelperCore
 {
     // @codingStandardsIgnoreStart
+    /** @var string */
     public $currentIndex;
+    /** @var string $table */
     public $table = 'configuration';
+    /** @var string $identifier */
     public $identifier;
+    /** @var string $token */
     public $token;
+    /** @var array $toolbar_btn */
     public $toolbar_btn;
+    /** @var mixed $ps_help_context */
     public $ps_help_context;
+    /** @var string $title */
     public $title;
+    /** @var bool $show_toolbar */
     public $show_toolbar = true;
+    /** @var Context $context */
     public $context;
+    /** @var bool $toolbar_scroll */
     public $toolbar_scroll = false;
+    /** @var bool $bootstrap */
     public $bootstrap = false;
-
-    /**
-     * @var Module
-     */
+    /** @var Module $module */
     public $module;
-
     /** @var string Helper tpl folder */
     public $base_folder;
-
     /** @var string Controller tpl folder */
     public $override_folder;
-
-    /**
-     * @var Smarty_Internal_Template base template object
-     */
-    protected $tpl;
-
-    /**
-     * @var string base template name
-     */
+    /** @var string base template name */
     public $base_tpl = 'content.tpl';
-
+    /** @var array $tpl_vars */
     public $tpl_vars = [];
+    /** @var Smarty_Internal_Template base template object */
+    protected $tpl;
     // @codingStandardsIgnoreEnd
 
     /**
      * HelperCore constructor.
      *
-     * @since 1.0.0
+     * @since   1.0.0
      * @version 1.0.0 Initial version
      */
     public function __construct()
@@ -85,68 +85,18 @@ class HelperCore
     }
 
     /**
-     * @param $tpl
+     * @deprecated 2.0.0
      *
-     * @since 1.0.0
-     * @version 1.0.0 Initial version
-     */
-    public function setTpl($tpl)
-    {
-        $this->tpl = $this->createTemplate($tpl);
-    }
-
-    /**
-     * Create a template from the override file, else from the base file.
-     *
-     * @param string $tplName filename
-     *
-     * @return Smarty_Internal_Template
-     *
-     * @since 1.0.0
-     * @version 1.0.0 Initial version
-     */
-    public function createTemplate($tplName)
-    {
-        if ($this->override_folder) {
-            if ($this->context->controller instanceof ModuleAdminController) {
-                $overrideTplPath = $this->context->controller->getTemplatePath().$this->override_folder.$this->base_folder.$tplName;
-            } elseif ($this->module) {
-                $overrideTplPath = _PS_MODULE_DIR_.$this->module->name.'/views/templates/admin/_configure/'.$this->override_folder.$this->base_folder.$tplName;
-            } else {
-                if (file_exists($this->context->smarty->getTemplateDir(1).$this->override_folder.$this->base_folder.$tplName)) {
-                    $overrideTplPath = $this->context->smarty->getTemplateDir(1).$this->override_folder.$this->base_folder.$tplName;
-                } elseif (file_exists($this->context->smarty->getTemplateDir(0).'controllers'.DIRECTORY_SEPARATOR.$this->override_folder.$this->base_folder.$tplName)) {
-                    $overrideTplPath = $this->context->smarty->getTemplateDir(0).'controllers'.DIRECTORY_SEPARATOR.$this->override_folder.$this->base_folder.$tplName;
-                }
-            }
-        } elseif ($this->module) {
-            $overrideTplPath = _PS_MODULE_DIR_.$this->module->name.'/views/templates/admin/_configure/'.$this->base_folder.$tplName;
-        }
-
-        if (isset($overrideTplPath) && file_exists($overrideTplPath)) {
-            return $this->context->smarty->createTemplate($overrideTplPath, $this->context->smarty);
-        } else {
-            return $this->context->smarty->createTemplate($this->base_folder.$tplName, $this->context->smarty);
-        }
-    }
-
-    /**
-     * default behaviour for helper is to return a tpl fetched
+     * @param array  $translations
+     * @param array  $selectedCat
+     * @param string $inputName
+     * @param bool   $useRadio
+     * @param bool   $useSearch
+     * @param array  $disabledCategories
+     * @param bool   $useInPopup
      *
      * @return string
-     *
-     * @since 1.0.0
-     * @version 1.0.0 Initial version
-     */
-    public function generate()
-    {
-        $this->tpl->assign($this->tpl_vars);
-
-        return $this->tpl->fetch();
-    }
-
-    /**
-     * @deprecated 2.0.0
+     * @throws PrestaShopException
      */
     public static function renderAdminCategorieTree(
         $translations,
@@ -168,7 +118,7 @@ class HelperCore
             throw new PrestaShopException('Missing root category parameter.');
         }
 
-        return $helper->renderCategoryTree($root, $selectedCat, $inputName, $useRadio, $useSearch, $disabledCategories, $useInPopup);
+        return $helper->renderCategoryTree($root, $selectedCat, $inputName, $useRadio, $useSearch, $disabledCategories);
     }
 
     /**
@@ -182,7 +132,7 @@ class HelperCore
      *
      * @return string
      *
-     * @since 1.0.0
+     * @since   1.0.0
      * @version 1.0.0 Initial version
      */
     public function renderCategoryTree(
@@ -257,7 +207,7 @@ class HelperCore
 						<input type="text" name="search_cat" id="search_cat"/>
 					</div>
 				</div>' : '')
-        .'</div>';
+            .'</div>';
 
         $homeIsSelected = false;
         if (is_array($selectedCat)) {
@@ -265,14 +215,14 @@ class HelperCore
                 if (is_array($cat)) {
                     $disabled = in_array($cat['id_category'], $disabledCategories);
                     if ($cat['id_category'] != $root['id_category']) {
-                        $html .= '<input '.($disabled?'disabled="disabled"':'').' type="hidden" name="'.$inputName.'" value="'.$cat['id_category'].'" >';
+                        $html .= '<input '.($disabled ? 'disabled="disabled"' : '').' type="hidden" name="'.$inputName.'" value="'.$cat['id_category'].'" >';
                     } else {
                         $homeIsSelected = true;
                     }
                 } else {
                     $disabled = in_array($cat, $disabledCategories);
                     if ($cat != $root['id_category']) {
-                        $html .= '<input '.($disabled?'disabled="disabled"':'').' type="hidden" name="'.$inputName.'" value="'.$cat.'" >';
+                        $html .= '<input '.($disabled ? 'disabled="disabled"' : '').' type="hidden" name="'.$inputName.'" value="'.$cat.'" >';
                     } else {
                         $homeIsSelected = true;
                     }
@@ -281,13 +231,13 @@ class HelperCore
         }
 
         $rootInput = '';
-        if ($root['id_category'] != (int)Configuration::get('PS_ROOT_CATEGORY') || (Tools::isSubmit('ajax') && Tools::getValue('action') == 'getCategoriesFromRootCategory')) {
+        if ($root['id_category'] != (int) Configuration::get('PS_ROOT_CATEGORY') || (Tools::isSubmit('ajax') && Tools::getValue('action') == 'getCategoriesFromRootCategory')) {
             $rootInput = '
 				<p class="checkbox"><i class="icon-folder-open"></i><label>
 					<input type="'.(!$useRadio ? 'checkbox' : 'radio').'" name="'
-                        .$inputName.'" value="'.$root['id_category'].'" '
-                        .($homeIsSelected ? 'checked' : '').' onclick="clickOnCategoryBox($(this));" />'
-                    .$root['name'].
+                .$inputName.'" value="'.$root['id_category'].'" '
+                .($homeIsSelected ? 'checked' : '').' onclick="clickOnCategoryBox($(this));" />'
+                .$root['name'].
                 '</label></p>';
         }
         $html .= '
@@ -307,100 +257,6 @@ class HelperCore
         if ($useSearch) {
             $html .= '<script type="text/javascript">searchCategory();</script>';
         }
-
-        return $html;
-    }
-
-    /**
-     * use translations files to replace english expression.
-     *
-     * @param mixed $string term or expression in english
-     * @param string $class
-     * @param bool $addslashes if set to true, the return value will pass through addslashes(). Otherwise, stripslashes().
-     * @param bool $htmlentities if set to true(default), the return value will pass through htmlentities($string, ENT_QUOTES, 'utf-8')
-     *
-     * @return string the translation if available, or the english default text.
-     *
-     * @since   1.0.0
-     * @version 1.0.0 Initial version
-     */
-    protected function l($string, $class = 'AdminTab', $addslashes = false, $htmlentities = true)
-    {
-        // if the class is extended by a module, use modules/[module_name]/xx.php lang file
-        $currentClass = get_class($this);
-        if (Module::getModuleNameFromClass($currentClass)) {
-            return Translate::getModuleTranslation(Module::$classInModule[$currentClass], $string, $currentClass);
-        }
-
-        return Translate::getAdminTranslation($string, get_class($this), $addslashes, $htmlentities);
-    }
-
-    /**
-     * Render a form with potentials required fields
-     *
-     * @param string $className
-     * @param string $identifier
-     * @param array  $tableFields
-     *
-     * @return string
-     *
-     * @since 1.0.0
-     * @version 1.0.0 Initial version
-     */
-    public function renderRequiredFields($className, $identifier, $tableFields)
-    {
-        $rules = call_user_func_array([$className, 'getValidationRules'], [$className]);
-        $requiredClassFields = [$identifier];
-        foreach ($rules['required'] as $required) {
-            $requiredClassFields[] = $required;
-        }
-
-        /** @var ObjectModel $object */
-        $object = new $className();
-        $res = $object->getFieldsRequiredDatabase();
-
-        $requiredFields = [];
-        foreach ($res as $row) {
-            $requiredFields[(int) $row['id_required_field']] = $row['field_name'];
-        }
-
-        $this->tpl_vars = [
-            'table_fields' => $tableFields,
-            'irow' => 0,
-            'required_class_fields' => $requiredClassFields,
-            'required_fields' => $requiredFields,
-            'current' => $this->currentIndex,
-            'token' => $this->token,
-        ];
-
-        $tpl = $this->createTemplate('helpers/required_fields.tpl');
-        $tpl->assign($this->tpl_vars);
-
-        return $tpl->fetch();
-    }
-
-    /**
-     * @param $modulesList
-     *
-     * @return mixed
-     *
-     * @since 1.0.0
-     * @version 1.0.0 Initial version
-     */
-    public function renderModulesList($modulesList)
-    {
-        $this->tpl_vars = [
-            'modules_list' => $modulesList,
-            'modules_uri' => __PS_BASE_URI__.basename(_PS_MODULE_DIR_)
-        ];
-        // The translations for this are defined by AdminModules, so override the context for the translations
-        $overrideControllerNameForTranslations = Context::getContext()->override_controller_name_for_translations;
-        Context::getContext()->override_controller_name_for_translations = 'AdminModules';
-        $tpl = $this->createTemplate('helpers/modules_list/list.tpl');
-        $tpl->assign($this->tpl_vars);
-        $html = $tpl->fetch();
-        // Restore the previous context
-        Context::getContext()->override_controller_name_for_translations = $overrideControllerNameForTranslations;
 
         return $html;
     }
@@ -435,8 +291,6 @@ class HelperCore
 
         // Generate HTML
         $url = $_SERVER['REQUEST_URI'].(($_SERVER['QUERY_STRING']) ? '&' : '?').'setShopContext=';
-        $shop = new Shop(Shop::getContextShopID());
-
         // $html = '<a href="#"><i class="icon-home"></i> '.$shop->name.'</a>';
         $html = '<select class="shopList" onchange="location.href = \''.htmlspecialchars($url).'\'+$(this).val();">';
         $html .= '<option value="" class="first">'.Translate::getAdminTranslation('All shops').'</option>';
@@ -461,5 +315,160 @@ class HelperCore
         $html .= '</select>';
 
         return $html;
+    }
+
+    /**
+     * @param string $tpl
+     *
+     * @since   1.0.0
+     * @version 1.0.0 Initial version
+     */
+    public function setTpl($tpl)
+    {
+        $this->tpl = $this->createTemplate($tpl);
+    }
+
+    /**
+     * Create a template from the override file, else from the base file.
+     *
+     * @param string $tplName filename
+     *
+     * @return Smarty_Internal_Template|object
+     *
+     * @since   1.0.0
+     * @version 1.0.0 Initial version
+     */
+    public function createTemplate($tplName)
+    {
+        if ($this->override_folder) {
+            if ($this->context->controller instanceof ModuleAdminController) {
+                $overrideTplPath = $this->context->controller->getTemplatePath($tplName).$this->override_folder.$this->base_folder.$tplName;
+            } elseif ($this->module) {
+                $overrideTplPath = _PS_MODULE_DIR_.$this->module->name.'/views/templates/admin/_configure/'.$this->override_folder.$this->base_folder.$tplName;
+            } else {
+                if (file_exists($this->context->smarty->getTemplateDir(1).$this->override_folder.$this->base_folder.$tplName)) {
+                    $overrideTplPath = $this->context->smarty->getTemplateDir(1).$this->override_folder.$this->base_folder.$tplName;
+                } elseif (file_exists($this->context->smarty->getTemplateDir(0).'controllers'.DIRECTORY_SEPARATOR.$this->override_folder.$this->base_folder.$tplName)) {
+                    $overrideTplPath = $this->context->smarty->getTemplateDir(0).'controllers'.DIRECTORY_SEPARATOR.$this->override_folder.$this->base_folder.$tplName;
+                }
+            }
+        } elseif ($this->module) {
+            $overrideTplPath = _PS_MODULE_DIR_.$this->module->name.'/views/templates/admin/_configure/'.$this->base_folder.$tplName;
+        }
+
+        if (isset($overrideTplPath) && file_exists($overrideTplPath)) {
+            return $this->context->smarty->createTemplate($overrideTplPath, $this->context->smarty);
+        } else {
+            return $this->context->smarty->createTemplate($this->base_folder.$tplName, $this->context->smarty);
+        }
+    }
+
+    /**
+     * default behaviour for helper is to return a tpl fetched
+     *
+     * @return string
+     *
+     * @since   1.0.0
+     * @version 1.0.0 Initial version
+     */
+    public function generate()
+    {
+        $this->tpl->assign($this->tpl_vars);
+
+        return $this->tpl->fetch();
+    }
+
+    /**
+     * Render a form with potentials required fields
+     *
+     * @param string $className
+     * @param string $identifier
+     * @param array  $tableFields
+     *
+     * @return string
+     *
+     * @since   1.0.0
+     * @version 1.0.0 Initial version
+     */
+    public function renderRequiredFields($className, $identifier, $tableFields)
+    {
+        $rules = call_user_func_array([$className, 'getValidationRules'], [$className]);
+        $requiredClassFields = [$identifier];
+        foreach ($rules['required'] as $required) {
+            $requiredClassFields[] = $required;
+        }
+
+        /** @var ObjectModel $object */
+        $object = new $className();
+        $res = $object->getFieldsRequiredDatabase();
+
+        $requiredFields = [];
+        foreach ($res as $row) {
+            $requiredFields[(int) $row['id_required_field']] = $row['field_name'];
+        }
+
+        $this->tpl_vars = [
+            'table_fields'          => $tableFields,
+            'irow'                  => 0,
+            'required_class_fields' => $requiredClassFields,
+            'required_fields'       => $requiredFields,
+            'current'               => $this->currentIndex,
+            'token'                 => $this->token,
+        ];
+
+        $tpl = $this->createTemplate('helpers/required_fields.tpl');
+        $tpl->assign($this->tpl_vars);
+
+        return $tpl->fetch();
+    }
+
+    /**
+     * @param array $modulesList
+     *
+     * @return mixed
+     *
+     * @since   1.0.0
+     * @version 1.0.0 Initial version
+     */
+    public function renderModulesList($modulesList)
+    {
+        $this->tpl_vars = [
+            'modules_list' => $modulesList,
+            'modules_uri'  => __PS_BASE_URI__.basename(_PS_MODULE_DIR_),
+        ];
+        // The translations for this are defined by AdminModules, so override the context for the translations
+        $overrideControllerNameForTranslations = Context::getContext()->override_controller_name_for_translations;
+        Context::getContext()->override_controller_name_for_translations = 'AdminModules';
+        $tpl = $this->createTemplate('helpers/modules_list/list.tpl');
+        $tpl->assign($this->tpl_vars);
+        $html = $tpl->fetch();
+        // Restore the previous context
+        Context::getContext()->override_controller_name_for_translations = $overrideControllerNameForTranslations;
+
+        return $html;
+    }
+
+    /**
+     * use translations files to replace english expression.
+     *
+     * @param mixed  $string       term or expression in english
+     * @param string $class
+     * @param bool   $addslashes   if set to true, the return value will pass through addslashes(). Otherwise, stripslashes().
+     * @param bool   $htmlentities if set to true(default), the return value will pass through htmlentities($string, ENT_QUOTES, 'utf-8')
+     *
+     * @return string the translation if available, or the english default text.
+     *
+     * @since   1.0.0
+     * @version 1.0.0 Initial version
+     */
+    protected function l($string, $class = 'AdminTab', $addslashes = false, $htmlentities = true)
+    {
+        // if the class is extended by a module, use modules/[module_name]/xx.php lang file
+        $currentClass = get_class($this);
+        if (Module::getModuleNameFromClass($currentClass)) {
+            return Translate::getModuleTranslation(Module::$classInModule[$currentClass], $string, $currentClass);
+        }
+
+        return Translate::getAdminTranslation($string, get_class($this), $addslashes, $htmlentities);
     }
 }
