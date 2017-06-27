2c777af6bbd4f98672c4ce9fa4457406e4db84c6
Michael Dekker <git@michaeldekker.nl>
2017-06-27 10:30:45 +0200

    Cleanup StockMvtWS

--- classes/stock/StockMvtWS.php
+++ classes/stock/StockMvtWS.php
@@ -36,124 +36,7 @@
  */
 class StockMvtWSCore extends ObjectModelCore
 {
-    // @codingStandarsIgnoreStart
-    public $id;
-
-    /**
-     * @var string The creation date of the movement
-     */
-    public $date_add;
-
-    /**
-     * @var int The employee id, responsible of the movement
-     */
-    public $id_employee;
-
-    /**
-     * @var string The first name of the employee responsible of the movement
-     */
-    public $employee_firstname;
-
-    /**
-     * @var string The last name of the employee responsible of the movement
-     */
-    public $employee_lastname;
-
-    /**
-     * @var int The stock id on wtich the movement is applied
-     */
-    public $id_stock;
-
-    /**
-     * @var int the quantity of product with is moved
-     */
-    public $physical_quantity;
-
-    /**
-     * @var int id of the movement reason assoiated to the movement
-     */
-    public $id_stock_mvt_reason;
-
-    /**
-     * @var int Used when the movement is due to a customer order
-     */
-    public $id_order = null;
-
-    /**
-     * @var int detrmine if the movement is a positive or negative operation
-     */
-    public $sign;
-
-    /**
-     * @var int Used when the movement is due to a supplier order
-     */
-    public $id_supply_order = null;
-
-    /**
-     * @var float Last value of the weighted-average method
-     */
-    public $last_wa = null;
-
-    /**
-     * @var float Current value of the weighted-average method
-     */
-    public $current_wa = null;
-
-    /**
-     * @var float The unit price without tax of the product associated to the movement
-     */
-    public $price_te;
-
-    /**
-     * @var int Refers to an other id_stock_mvt : used for LIFO/FIFO implementation in StockManager
-     */
-    public $referer;
-
-    /**
-     * @var int id_product (@see Stock::id_product)
-     */
-    public $id_product;
-
-    /**
-     * @var int id_product_attribute (@see Stock::id_product_attribute)
-     */
-    public $id_product_attribute;
-
-    /**
-     * @var int id_warehouse (@see Stock::id_warehouse)
-     */
-    public $id_warehouse;
-
-    /**
-     * @var int id_currency (@see Warehouse::id_currency)
-     */
-    public $id_currency;
-
-    /**
-     * @var string management_type (@see Warehouse::management_type)
-     */
-    public $management_type;
-
-    /*
-     * @var string : Name of the product (@see Product::getProductName)
-     */
-    public $product_name;
-
-    /**
-     * @var string EAN13 of the product (@see Stock::product_ean13)
-     */
-    public $ean13;
-
-    /**
-     * @var string UPC of the product (@see Stock::product_upc)
-     */
-    public $upc;
-
-    /**
-     * @var string Reference of the product (@see Stock::product_reference)
-     */
-    public $reference;
-
+    // @codingStandardsIgnoreStart
     /**
      * @see ObjectModel::$definition
      */
@@ -161,23 +44,116 @@ class StockMvtWSCore extends ObjectModelCore
         'table'   => 'stock_mvt',
         'primary' => 'id_stock_mvt',
         'fields'  => [
-            'id_employee'         => ['type' => self::TYPE_INT,    'validate' => 'isUnsignedId',  'required' => true],
-            'employee_firstname'  => ['type' => self::TYPE_STRING, 'validate' => 'isName'                           ],
-            'employee_lastname'   => ['type' => self::TYPE_STRING, 'validate' => 'isName'                           ],
-            'id_stock'            => ['type' => self::TYPE_INT,    'validate' => 'isUnsignedId',  'required' => true],
-            'physical_quantity'   => ['type' => self::TYPE_INT,    'validate' => 'isUnsignedInt', 'required' => true],
-            'id_stock_mvt_reason' => ['type' => self::TYPE_INT,    'validate' => 'isUnsignedId',  'required' => true],
-            'id_order'            => ['type' => self::TYPE_INT,    'validate' => 'isUnsignedId'                     ],
-            'id_supply_order'     => ['type' => self::TYPE_INT,    'validate' => 'isUnsignedId'                     ],
-            'sign'                => ['type' => self::TYPE_INT,    'validate' => 'isInt',         'required' => true],
-            'last_wa'             => ['type' => self::TYPE_FLOAT,  'validate' => 'isPrice'                          ],
-            'current_wa'          => ['type' => self::TYPE_FLOAT,  'validate' => 'isPrice'                          ],
-            'price_te'            => ['type' => self::TYPE_FLOAT,  'validate' => 'isPrice',       'required' => true],
-            'referer'             => ['type' => self::TYPE_INT,    'validate' => 'isUnsignedId'                     ],
-            'date_add'            => ['type' => self::TYPE_DATE,   'validate' => 'isDate',        'required' => true],
+            'id_employee'         => ['type' => self::TYPE_INT, 'validate' => 'isUnsignedId', 'required' => true],
+            'employee_firstname'  => ['type' => self::TYPE_STRING, 'validate' => 'isName'],
+            'employee_lastname'   => ['type' => self::TYPE_STRING, 'validate' => 'isName'],
+            'id_stock'            => ['type' => self::TYPE_INT, 'validate' => 'isUnsignedId', 'required' => true],
+            'physical_quantity'   => ['type' => self::TYPE_INT, 'validate' => 'isUnsignedInt', 'required' => true],
+            'id_stock_mvt_reason' => ['type' => self::TYPE_INT, 'validate' => 'isUnsignedId', 'required' => true],
+            'id_order'            => ['type' => self::TYPE_INT, 'validate' => 'isUnsignedId'],
+            'id_supply_order'     => ['type' => self::TYPE_INT, 'validate' => 'isUnsignedId'],
+            'sign'                => ['type' => self::TYPE_INT, 'validate' => 'isInt', 'required' => true],
+            'last_wa'             => ['type' => self::TYPE_FLOAT, 'validate' => 'isPrice'],
+            'current_wa'          => ['type' => self::TYPE_FLOAT, 'validate' => 'isPrice'],
+            'price_te'            => ['type' => self::TYPE_FLOAT, 'validate' => 'isPrice', 'required' => true],
+            'referer'             => ['type' => self::TYPE_INT, 'validate' => 'isUnsignedId'],
+            'date_add'            => ['type' => self::TYPE_DATE, 'validate' => 'isDate', 'required' => true],
         ],
     ];
-
+    /** @var int $id */
+    public $id;
+    /**
+     * @var string The creation date of the movement
+     */
+    public $date_add;
+    /**
+     * @var int The employee id, responsible of the movement
+     */
+    public $id_employee;
+    /**
+     * @var string The first name of the employee responsible of the movement
+     */
+    public $employee_firstname;
+    /**
+     * @var string The last name of the employee responsible of the movement
+     */
+    public $employee_lastname;
+    /**
+     * @var int The stock id on wtich the movement is applied
+     */
+    public $id_stock;
+    /**
+     * @var int the quantity of product with is moved
+     */
+    public $physical_quantity;
+    /**
+     * @var int id of the movement reason assoiated to the movement
+     */
+    public $id_stock_mvt_reason;
+    /**
+     * @var int Used when the movement is due to a customer order
+     */
+    public $id_order = null;
+    /**
+     * @var int detrmine if the movement is a positive or negative operation
+     */
+    public $sign;
+    /**
+     * @var int Used when the movement is due to a supplier order
+     */
+    public $id_supply_order = null;
+    /**
+     * @var float Last value of the weighted-average method
+     */
+    public $last_wa = null;
+    /**
+     * @var float Current value of the weighted-average method
+     */
+    public $current_wa = null;
+    /**
+     * @var float The unit price without tax of the product associated to the movement
+     */
+    public $price_te;
+    /**
+     * @var int Refers to an other id_stock_mvt : used for LIFO/FIFO implementation in StockManager
+     */
+    public $referer;
+    /**
+     * @var int id_product (@see Stock::id_product)
+     */
+    public $id_product;
+    /**
+     * @var int id_product_attribute (@see Stock::id_product_attribute)
+     */
+    public $id_product_attribute;
+    /**
+     * @var int id_warehouse (@see Stock::id_warehouse)
+     */
+    public $id_warehouse;
+    /**
+     * @var int id_currency (@see Warehouse::id_currency)
+     */
+    public $id_currency;
+    /**
+     * @var string management_type (@see Warehouse::management_type)
+     */
+    public $management_type;
+    /**
+     * @var string : Name of the product (@see Product::getProductName)
+     */
+    public $product_name;
+    /**
+     * @var string EAN13 of the product (@see Stock::product_ean13)
+     */
+    public $ean13;
+    /**
+     * @var string UPC of the product (@see Stock::product_upc)
+     */
+    public $upc;
+    /**
+     * @var string Reference of the product (@see Stock::product_reference)
+     */
+    public $reference;
     /**
      * @see ObjectModel::$webserviceParameters
      */
@@ -262,11 +238,10 @@ class StockMvtWSCore extends ObjectModelCore
         $query = 'SELECT DISTINCT main.'.$this->def['primary'].' ';
 
         if ($full) {
-            $query .= ', s.id_product, s.id_product_attribute, s.id_warehouse, w.id_currency, w.management_type,
-					   s.ean13, s.upc, s.reference ';
+            $query .= ', s.id_product, s.id_product_attribute, s.id_warehouse, w.id_currency, w.management_type,s.ean13, s.upc, s.reference ';
         }
 
-        $old_filter = $filter;
+        $oldFilter = $filter;
         if ($filter) {
             foreach ($this->tables_assoc as $key => $value) {
                 $filter = str_replace('main.`'.$key.'`', $value['table'].'.`'.$key.'`', $filter);
@@ -275,7 +250,7 @@ class StockMvtWSCore extends ObjectModelCore
 
         $query .= 'FROM '._DB_PREFIX_.$this->def['table'].' as main ';
 
-        if ($filter !== $old_filter || $full) {
+        if ($filter !== $oldFilter || $full) {
             $query .= 'LEFT JOIN '._DB_PREFIX_.'stock s ON (s.id_stock = main.id_stock) ';
             $query .= 'LEFT JOIN '._DB_PREFIX_.'warehouse w ON (w.id_warehouse = s.id_warehouse) ';
             $query .= 'LEFT JOIN '._DB_PREFIX_.'currency c ON (c.id_currency = w.id_currency) ';
@@ -304,6 +279,8 @@ class StockMvtWSCore extends ObjectModelCore
 
     /**
      * Webservice : getter for the product name
+     *
+     * @return array
      *
      * @since   1.0.0
      * @version 1.0.0 Initial version
