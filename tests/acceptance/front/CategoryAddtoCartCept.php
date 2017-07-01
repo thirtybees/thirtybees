f22df2c1404636704af2c300d33a0a393c070fa2
dh42 <lesley@dh42.com>
2017-06-30 19:15:44 -0500

    Fixing acceptance test

--- tests/acceptance/front/CategoryAddtoCartCept.php
+++ tests/acceptance/front/CategoryAddtoCartCept.php
@@ -6,5 +6,11 @@ $I->resizeWindow(1920, 1080);
 $I->amOnPage('/index.php');
 $I->see('Women');
 $I->click('Women');
-$I->waitForElementVisible(['css' => '.category-banner']);
+
+$I->see('Women Subcategories');
+$I->click('Add to cart');
+$I->waitForElementVisible(['css' => '.layer_cart_product']);
+$I->see('Product successfully added to your shopping cart');
+
+$I->click('Tops');
 
