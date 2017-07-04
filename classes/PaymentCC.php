bbbb06aaadde6547750c56523e6d98b311d8908b
Michael Dekker <git@michaeldekker.nl>
2017-07-04 02:28:18 +0200

    Cleanup PaymentCC

--- classes/PaymentCC.php
+++ classes/PaymentCC.php
@@ -37,45 +37,59 @@
  */
 class PaymentCCCore extends OrderPayment
 {
+    // @codingStandardsIgnoreStart
+    /** @var int $id_order */
     public $id_order;
+    /** @var int $id_currency */
     public $id_currency;
+    /** @var float $amount */
     public $amount;
+    /** @var string $transaction_id */
     public $transaction_id;
+    /** @var string $card_number */
     public $card_number;
+    /** @var string $card_brand */
     public $card_brand;
+    /** @var string $card_expiration */
     public $card_expiration;
+    /** @var string $card_holder*/
     public $card_holder;
+    /** @var string $date_add */
     public $date_add;
-
     protected $fieldsRequired = ['id_currency', 'amount'];
     protected $fieldsSize = ['transaction_id' => 254, 'card_number' => 254, 'card_brand' => 254, 'card_expiration' => 254, 'card_holder' => 254];
     protected $fieldsValidate = [
         'id_order' => 'isUnsignedId', 'id_currency' => 'isUnsignedId', 'amount' => 'isPrice',
         'transaction_id' => 'isAnything', 'card_number' => 'isAnything', 'card_brand' => 'isAnything', 'card_expiration' => 'isAnything', 'card_holder' => 'isAnything'
     ];
-
     public static $definition = [
         'table' => 'payment_cc',
         'primary' => 'id_payment_cc',
     ];
-
+    // @codingStandardsIgnoreEnd
 
     /**
      * @deprecated 1.5.0.2
-     * @see OrderPaymentCore
+     * @see        OrderPaymentCore
+     *
+     * @param bool $autoDate
+     * @param bool $nullValues
+     *
+     * @return bool
      */
     public function add($autoDate = true, $nullValues = false)
     {
         Tools::displayAsDeprecated();
+
         return parent::add($autoDate, $nullValues);
     }
 
     /**
     * Get the detailed payment of an order
     *
-*@param int $idOrder
-     *
-*@return array
+    * @param int $idOrder
+    *
+    * @return array
     * @deprecated 1.5.0.1
     * @see OrderPaymentCore
     */
@@ -83,6 +97,7 @@ class PaymentCCCore extends OrderPayment
     {
         Tools::displayAsDeprecated();
         $order = new Order($idOrder);
+
         return OrderPayment::getByOrderReference($order->reference);
     }
 }
