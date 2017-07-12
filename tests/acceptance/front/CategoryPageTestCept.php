2716e64e6302e9500d669ec01b1e0e4403a5031e
Michael Dekker <git@michaeldekker.nl>
2017-07-12 08:45:36 +0200

    Change tests for new fixtures

--- tests/acceptance/front/CategoryPageTestCept.php
+++ tests/acceptance/front/CategoryPageTestCept.php
@@ -4,4 +4,4 @@ $I->am('Visitor');
 $I->wantTo('Make sure the category page works');
 $I->resizeWindow(1920, 1080);
 $I->amOnPage('/index.php?id_category=3&controller=category&id_lang=1');
-$I->see('Polyester');
+$I->see('Coffee');
