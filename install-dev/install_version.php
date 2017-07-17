47aaf03fdb773c3f0a25a6b994ebd1e1f834c956
Michael Dekker <git@michaeldekker.nl>
2017-07-17 12:48:14 +0200

    Set to version 1.0.2

--- install-dev/install_version.php
+++ install-dev/install_version.php
@@ -29,4 +29,4 @@
  *  PrestaShop is an internationally registered trademark & property of PrestaShop SA
  */
 
-define('_TB_INSTALL_VERSION_', '1.0.2-beta.3');
+define('_TB_INSTALL_VERSION_', '1.0.2');
