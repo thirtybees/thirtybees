fef16e4fc5f32d19387285e7113ed65ac884c76e
Kubilay Erdogan <kublaios@gmail.com>
2017-08-15 17:04:41 +0300

    fixing unit tests for `Tools` class where `$runTestInSeparateProcess` was causing errors

--- tests/unit/ToolsTest.php
+++ tests/unit/ToolsTest.php
@@ -9,7 +9,6 @@ class ToolsTest extends \Codeception\TestCase\Test
     protected $tester;
 
 	protected $preserveGlobalState = FALSE;
-	protected $runTestInSeparateProcess = TRUE;
 
     protected function _before()
     {
