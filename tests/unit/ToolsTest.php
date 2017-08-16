0458092d54a0161e1c46948c3630b43d067c6446
Kubilay Erdogan <kublaios@gmail.com>
2017-08-16 16:46:53 +0300

    trying to mock `Tools::secureReferrer`

--- tests/unit/ToolsTest.php
+++ tests/unit/ToolsTest.php
@@ -1,24 +1,25 @@
 <?php
+use AspectMock\Test as test;
 
-
-class ToolsTest extends \Codeception\TestCase\Test
+class ToolsTest extends \Codeception\Test\Unit
 {
-    /**
-     * @var \UnitTester
-     */
-    protected $tester;
+	/**
+	 * @var \UnitTester
+	 */
+	protected $tester;
 
-    protected function _before()
-    {
-        $_GET = [];
-        $_POST = [];
-    }
+	protected function _before()
+	{
+		$_GET = [];
+		$_POST = [];
+	}
 
-    protected function _after()
-    {
-    }
+	protected function _after()
+	{
+		\AspectMock\Test::clean();
+	}
 
-    // FIXME
+	// FIXME
 	public function testPasswdGen()
 	{
 		$this->assertTrue(true);
@@ -280,21 +281,9 @@ class ToolsTest extends \Codeception\TestCase\Test
 	 */
 	public function testSecureReferrer($expected, $referrer)
 	{
-		$this->assertTrue(true);
-		/*
-		$_SERVER = [
-			'SERVER_NAME' => 'server.domain',
-		];
-		if (!defined('_PS_SSL_PORT_') && !isset($_SESSION['_PS_SSL_PORT_'])) {
-			define('_PS_SSL_PORT_', 443);
-			$_SESSION['_PS_SSL_PORT_'] = true;
-		}
-		if (!defined('__PS_BASE_URI__') && !isset($_SESSION['__PS_BASE_URI__'])) {
-			define('__PS_BASE_URI__', 'server.domain');
-			$_SESSION['__PS_BASE_URI__'] = true;
-		}
+		$invoke = test::double('Tools', ['secureReferrer' => 'server.domain']);
 		$this->assertEquals($expected, Tools::secureReferrer($referrer));
-		*/
+		$invoke->verifyInvoked('secureReferrer');
 	}
 
 	public function getServerNameDataProvider()
@@ -390,177 +379,177 @@ class ToolsTest extends \Codeception\TestCase\Test
 		$this->assertTrue(true);
 	}
 
-    public function testGetValueBaseCase()
-    {
-        $_GET = [
-            'hello' => 'world',
-        ];
+	public function testGetValueBaseCase()
+	{
+		$_GET = [
+			'hello' => 'world',
+		];
 
-        $this->assertEquals('world', Tools::getValue('hello'));
-    }
+		$this->assertEquals('world', Tools::getValue('hello'));
+	}
 
-    public function testGetValueDefaultValueIsFalse()
-    {
-        $this->assertEquals(false, Tools::getValue('hello'));
-    }
+	public function testGetValueDefaultValueIsFalse()
+	{
+		$this->assertEquals(false, Tools::getValue('hello'));
+	}
 
-    public function testGetValueUsesDefaultValue()
-    {
-        $this->assertEquals('I AM DEFAULT', Tools::getValue('hello', 'I AM DEFAULT'));
-    }
+	public function testGetValueUsesDefaultValue()
+	{
+		$this->assertEquals('I AM DEFAULT', Tools::getValue('hello', 'I AM DEFAULT'));
+	}
 
-    public function testGetValuePrefersPost()
-    {
-        $_GET = [
-            'hello' => 'world',
-        ];
-        $_POST = [
-            'hello' => 'cruel world',
-        ];
+	public function testGetValuePrefersPost()
+	{
+		$_GET = [
+			'hello' => 'world',
+		];
+		$_POST = [
+			'hello' => 'cruel world',
+		];
 
-        $this->assertEquals('cruel world', Tools::getValue('hello'));
-    }
+		$this->assertEquals('cruel world', Tools::getValue('hello'));
+	}
 
-    public function testGetValueAcceptsOnlyTruthyStringsAsKeys()
-    {
-        $_GET = [
-            '' => true,
-            ' ' => true,
-            null => true,
-        ];
+	public function testGetValueAcceptsOnlyTruthyStringsAsKeys()
+	{
+		$_GET = [
+			'' => true,
+			' ' => true,
+			null => true,
+		];
 
-        $this->assertEquals(false, Tools::getValue('', true));
-        $this->assertEquals(true, Tools::getValue(' '));
-        $this->assertEquals(false, Tools::getValue(null, true));
-    }
+		$this->assertEquals(false, Tools::getValue('', true));
+		$this->assertEquals(true, Tools::getValue(' '));
+		$this->assertEquals(false, Tools::getValue(null, true));
+	}
 
-    public function testGetValueStripsNullCharsFromReturnedStringsExamples()
-    {
-        return [
-            ["\0", ''],
-            ["haxx\0r", 'haxxr'],
-            ["haxx\0\0\0r", 'haxxr'],
-        ];
-    }
+	public function testGetValueStripsNullCharsFromReturnedStringsExamples()
+	{
+		return [
+			["\0", ''],
+			["haxx\0r", 'haxxr'],
+			["haxx\0\0\0r", 'haxxr'],
+		];
+	}
 
-    /**
-     * @dataProvider testGetValueStripsNullCharsFromReturnedStringsExamples
-     */
-    public function testGetValueStripsNullCharsFromReturnedStrings($rawString, $cleanedString)
-    {
-        /**
-         * Check it cleans values stored in POST
-         */
-        $_GET = [
-            'rawString' => $rawString
-        ];
-        $this->assertEquals($cleanedString, Tools::getValue('rawString'));
+	/**
+	 * @dataProvider testGetValueStripsNullCharsFromReturnedStringsExamples
+	 */
+	public function testGetValueStripsNullCharsFromReturnedStrings($rawString, $cleanedString)
+	{
+		/**
+		 * Check it cleans values stored in POST
+		 */
+		$_GET = [
+			'rawString' => $rawString
+		];
+		$this->assertEquals($cleanedString, Tools::getValue('rawString'));
 
-        /**
-         * Check it cleans values stored in GET
-         */
-        $_GET = [];
-        $_POST = [
-            'rawString' => $rawString,
-        ];
-        $this->assertEquals($cleanedString, Tools::getValue('rawString'));
+		/**
+		 * Check it cleans values stored in GET
+		 */
+		$_GET = [];
+		$_POST = [
+			'rawString' => $rawString,
+		];
+		$this->assertEquals($cleanedString, Tools::getValue('rawString'));
 
-        /**
-         * Check it cleans default values too
-         */
-        $_GET = [];
-        $_POST = [];
-        $this->assertEquals($cleanedString, Tools::getValue('NON EXISTING KEY', $rawString));
-    }
+		/**
+		 * Check it cleans default values too
+		 */
+		$_GET = [];
+		$_POST = [];
+		$this->assertEquals($cleanedString, Tools::getValue('NON EXISTING KEY', $rawString));
+	}
 
-    public function testSpreadAmountExamples()
-    {
-        return [
-            [
-                // base case
-                [['a' => 2], ['a' => 1]], // expected result
-                1, 0,                                     // amount and precision
-                [['a' => 1], ['a' => 1]], // source rows
-                'a'                                         // sort column
-            ],
-            [
-                // check with 1 decimal
-                [['a' => 1.5], ['a' => 1.5]],
-                1, 1,
-                [['a' => 1], ['a' => 1]],
-                'a',
-            ],
-            [
-                // 2 decimals, but only one really needed
-                [['a' => 1.5], ['a' => 1.5]],
-                1, 2,
-                [['a' => 1], ['a' => 1]],
-                'a',
-            ],
-            [
-                // check that the biggest "a" gets the adjustment
-                [['a' => 3], ['a' => 1]],
-                1, 0,
-                [['a' => 1], ['a' => 2]],
-                'a',
-            ],
-            [
-                // check it works with amount > count($rows)
-                [['a' => 4], ['a' => 2]],
-                3, 0,
-                [['a' => 1], ['a' => 2]],
-                'a',
-            ],
-            [
-                // 2 decimals
-                [['a' => 2.01], ['a' => 1]],
-                0.01, 2,
-                [['a' => 1], ['a' => 2]],
-                'a',
-            ],
-            [
-                // 2 decimals, equal level of adjustment
-                [['a' => 2.01], ['a' => 1.01]],
-                0.02, 2,
-                [['a' => 1], ['a' => 2]],
-                'a',
-            ],
-            [
-                // 2 decimals, different levels of adjustmnt
-                [['a' => 2.02], ['a' => 1.01]],
-                0.03, 2,
-                [['a' => 1], ['a' => 2]],
-                'a',
-            ],
-            [
-                // check associative arrays are OK too
-                [['a' => 2.01], ['a' => 1.01]],
-                0.02, 2,
-                ['z' => ['a' => 1], 'x' => ['a' => 2]],
-                'a',
-            ],
-            [
-                // check amount is rounded if it needs more precision than asked for
-                [['a' => 2.02], ['a' => 1.01]],
-                0.025, 2,
-                [['a' => 1], ['a' => 2]],
-                'a',
-            ],
-            [
-                [['a' => 7.69], ['a' => 4.09], ['a' => 1.8]],
-                -0.32, 2,
-                [['a' => 7.8], ['a' => 4.2], ['a' => 1.9]],
-                'a',
-            ],
-        ];
-    }
+	public function testSpreadAmountExamples()
+	{
+		return [
+			[
+				// base case
+				[['a' => 2], ['a' => 1]], // expected result
+				1, 0,                                     // amount and precision
+				[['a' => 1], ['a' => 1]], // source rows
+				'a'                                         // sort column
+			],
+			[
+				// check with 1 decimal
+				[['a' => 1.5], ['a' => 1.5]],
+				1, 1,
+				[['a' => 1], ['a' => 1]],
+				'a',
+			],
+			[
+				// 2 decimals, but only one really needed
+				[['a' => 1.5], ['a' => 1.5]],
+				1, 2,
+				[['a' => 1], ['a' => 1]],
+				'a',
+			],
+			[
+				// check that the biggest "a" gets the adjustment
+				[['a' => 3], ['a' => 1]],
+				1, 0,
+				[['a' => 1], ['a' => 2]],
+				'a',
+			],
+			[
+				// check it works with amount > count($rows)
+				[['a' => 4], ['a' => 2]],
+				3, 0,
+				[['a' => 1], ['a' => 2]],
+				'a',
+			],
+			[
+				// 2 decimals
+				[['a' => 2.01], ['a' => 1]],
+				0.01, 2,
+				[['a' => 1], ['a' => 2]],
+				'a',
+			],
+			[
+				// 2 decimals, equal level of adjustment
+				[['a' => 2.01], ['a' => 1.01]],
+				0.02, 2,
+				[['a' => 1], ['a' => 2]],
+				'a',
+			],
+			[
+				// 2 decimals, different levels of adjustmnt
+				[['a' => 2.02], ['a' => 1.01]],
+				0.03, 2,
+				[['a' => 1], ['a' => 2]],
+				'a',
+			],
+			[
+				// check associative arrays are OK too
+				[['a' => 2.01], ['a' => 1.01]],
+				0.02, 2,
+				['z' => ['a' => 1], 'x' => ['a' => 2]],
+				'a',
+			],
+			[
+				// check amount is rounded if it needs more precision than asked for
+				[['a' => 2.02], ['a' => 1.01]],
+				0.025, 2,
+				[['a' => 1], ['a' => 2]],
+				'a',
+			],
+			[
+				[['a' => 7.69], ['a' => 4.09], ['a' => 1.8]],
+				-0.32, 2,
+				[['a' => 7.8], ['a' => 4.2], ['a' => 1.9]],
+				'a',
+			],
+		];
+	}
 
-    /**
-     * @dataProvider testSpreadAmountExamples
-     */
-    public function testSpreadAmount($expectedRows, $amount, $precision, $rows, $column)
-    {
-        Tools::spreadAmount($amount, $precision, $rows, $column);
-        $this->assertEquals(array_values($expectedRows), array_values($rows));
-    }
+	/**
+	 * @dataProvider testSpreadAmountExamples
+	 */
+	public function testSpreadAmount($expectedRows, $amount, $precision, $rows, $column)
+	{
+		Tools::spreadAmount($amount, $precision, $rows, $column);
+		$this->assertEquals(array_values($expectedRows), array_values($rows));
+	}
 }
