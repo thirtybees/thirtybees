8b684a06242d7a5fc423073a8181de8488809208
Kubilay Erdogan <kublaios@gmail.com>
2017-08-18 09:21:24 +0300

    overriding the default `defines.inc.php` with a customizable one

--- tests/unit/_bootstrap.php
+++ tests/unit/_bootstrap.php
@@ -14,11 +14,7 @@ $kernel->init([
     ],
 ]);
 
-require_once __DIR__.'/../../config/defines.inc.php';
+require_once __DIR__.'/config/defines.inc.php';
 require_once __DIR__.'/../../config/settings.inc.php';
 require_once __DIR__.'/../_support/unitloadclasses.php';
 require_once __DIR__.'/../../config/alias.php';
-
-if (!defined('_PS_PRICE_DISPLAY_PRECISION_')) {
-	define('_PS_PRICE_DISPLAY_PRECISION_', 2);
-}
