7c86090aaf14450a8c2ffa28a0b260a99c2200e8
Michael Dekker <git@michaeldekker.nl>
2017-11-21 03:22:47 +0100

    Remove todo: admin redirect is fine

--- controllers/front/CartController.php
+++ controllers/front/CartController.php
@@ -193,7 +193,6 @@ class CartControllerCore extends FrontController
         } elseif ($product->hasAttributes()) {
             $minimumQuantity = ($product->out_of_stock == 2) ? !Configuration::get('PS_ORDER_OUT_OF_STOCK') : !$product->out_of_stock;
             $this->id_product_attribute = Product::getDefaultAttribute($product->id, $minimumQuantity);
-            // @todo do something better than a redirect admin !!
             if (!$this->id_product_attribute) {
                 Tools::redirectAdmin($this->context->link->getProductLink($product));
             } elseif (!Product::isAvailableWhenOutOfStock($product->out_of_stock) && !Attribute::checkAttributeQty($this->id_product_attribute, $qtyToCheck)) {
