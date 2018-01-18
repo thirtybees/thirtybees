dc4a7b9701d4211df4f506ad251bec1e482dfb5c
David Gasperoni <mcdado@gmail.com>
2018-01-18 01:01:20 +0100

    Fix reordering product images and setting cover
    This happens when multishop is enabled and some images are not enabled
    in the currently selected shop.

--- controllers/admin/AdminProductsController.php
+++ controllers/admin/AdminProductsController.php
@@ -1516,8 +1516,21 @@ class AdminProductsControllerCore extends AdminController
             $json = stripslashes($json);
             $images = json_decode($json, true);
             foreach ($images as $id => $position) {
-                $img = new Image((int) $id);
-                $img->position = (int) $position;
+                /*
+                 * If the the image is not associated with the currently selected shop, the fields that are also in the
+                 * image_shop table (like id_product and cover) cannot be loaded properly, so we have to load them
+                 * separately.
+                 */
+                $img = new Image((int)$id);
+                $def = $img::$definition;
+                $sql = 'SELECT * FROM `' . _DB_PREFIX_ . $def['table'] . '` WHERE `' . $def['primary'] . '` = ' . (int)$id;
+                $fields_from_table = Db::getInstance()->getRow($sql);
+                foreach ($def['fields'] as $key => $value) {
+                    if (!$value['lang']) {
+                        $img->{$key} = $fields_from_table[$key];
+                    }
+                }
+                $img->position = (int)$position;
                 $res &= $img->update();
             }
         }
@@ -1539,7 +1552,21 @@ class AdminProductsControllerCore extends AdminController
             $this->ajaxDie(json_encode(['error' => $this->l('You do not have the right permission')]));
         }
         Image::deleteCover((int) Tools::getValue('id_product'));
-        $img = new Image((int) Tools::getValue('id_image'));
+        $id_image = (int)Tools::getValue('id_image');
+
+        /*
+         * If the the image is not associated with the currently selected shop, the fields that are also in the
+         * image_shop table (like id_product and cover) cannot be loaded properly, so we have to load them separately.
+         */
+        $img = new Image($id_image);
+        $def = $img::$definition;
+        $sql = 'SELECT * FROM `' . _DB_PREFIX_ . $def['table'] . '` WHERE `' . $def['primary'] . '` = ' . $id_image;
+        $fields_from_table = Db::getInstance()->getRow($sql);
+        foreach ($def['fields'] as $key => $value) {
+            if (!$value['lang']) {
+                $img->{$key} = $fields_from_table[$key];
+            }
+        }
         $img->cover = 1;
 
         @unlink(_PS_TMP_IMG_DIR_.'product_'.(int) $img->id_product.'.jpg');
