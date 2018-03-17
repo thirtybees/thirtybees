a2e668ed316f24304b76cb2ac9eb0b0c3c11925c
Yaniv Mirel <yanivmirel@gmail.com>
2018-03-17 22:45:43 +0200

    define $unactiveList Variable

--- controllers/admin/AdminPaymentController.php
+++ controllers/admin/AdminPaymentController.php
@@ -386,6 +386,7 @@ class AdminPaymentControllerCore extends AdminController
     {
         if ($this->getModulesList($this->filter_modules_list)) {
             $activeList = [];
+            $unactiveList = [];
             foreach ($this->modules_list as $key => $module) {
                 if (in_array($module->name, $this->list_partners_modules)) {
                     $this->modules_list[$key]->type = 'addonsPartner';
