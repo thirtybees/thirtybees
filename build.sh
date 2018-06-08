519d6aab33bc04ebd3456b4d8d166794dc453b95
Markus Hitter <mah@jump-ing.de>
2018-06-08 16:32:46 +0200

    buildmodule.sh: first skeleton.

--- build.sh
+++ build.sh
@@ -171,6 +171,7 @@ KEEP=("lib/Twig/Node/Expression/Test")
 
 # Exclude paths, for individual files and directories to be excluded.
 # EXCLUDE_PATH=("generatemd5list.php")  <- Can't get removed.
+EXCLUDE_PATH=("tools/buildmodule.sh")
 EXCLUDE_PATH=("tools/validatemodule.sh")
 EXCLUDE_PATH+=("tools/templates/")
 
