PrestaShop/PrestaShop

replace serialize with json_encode

classes/Cart.php
@@ -3034,7 +3034,7 @@ class CartCore extends ObjectModel
             $this->id_carrier = $this->getIdCarrierFromDeliveryOption($delivery_option);
         }
 
-        $this->delivery_option = serialize($delivery_option);
+        $this->delivery_option = json_encode($delivery_option);
     }
 
     /**
@@ -3080,18 +3080,20 @@ class CartCore extends ObjectModel
 
         // The delivery option was selected
         if (isset($this->delivery_option) && $this->delivery_option != '') {
-            $delivery_option = Tools::unSerialize($this->delivery_option);
+            $delivery_option = json_decode($this->delivery_option, true);
             $validated = true;
-            foreach ($delivery_option as $id_address => $key) {
-                if (!isset($delivery_option_list[$id_address][$key])) {
-                    $validated = false;
-                    break;
+            if (is_array($delivery_option)) {
+                foreach ($delivery_option as $id_address => $key) {
+                    if (!isset($delivery_option_list[$id_address][$key])) {
+                        $validated = false;
+                        break;
+                    }
                 }
-            }
+                if ($validated) {
+                    $cache[$cache_id] = $delivery_option;
 
-            if ($validated) {
-                $cache[$cache_id] = $delivery_option;
-                return $delivery_option;
+                    return $delivery_option;
+                }
             }
         }
 

thirtybees/thirtybees

classes/Cart.php
@@ -1034,19 +1034,21 @@ class CartCore extends ObjectModel
 
         // The delivery option was selected
         if (isset($this->delivery_option) && $this->delivery_option != '') {
-            $deliveryOption = unserialize($this->delivery_option);
+            $deliveryOption = json_decode($this->delivery_option);
             $validated = true;
-            foreach ($deliveryOption as $idAddress => $key) {
-                if (!isset($deliveryOptionList[$idAddress][$key])) {
-                    $validated = false;
-                    break;
+            if (is_array($delivery_option)) {
+                foreach ($deliveryOption as $idAddress => $key) {
+                    if (!isset($deliveryOptionList[$idAddress][$key])) {
+                        $validated = false;
+                        break;
+                    }
                 }
-            }
 
-            if ($validated) {
-                $cache[$cacheId] = $deliveryOption;
+                if ($validated) {
+                    $cache[$cacheId] = $deliveryOption;
 
-                return $deliveryOption;
+                    return $deliveryOption;
+                }
             }
         }
 
@@ -1118,7 +1120,7 @@ class CartCore extends ObjectModel
             $this->id_carrier = $this->getIdCarrierFromDeliveryOption($deliveryOption);
         }
 
-        $this->delivery_option = serialize($deliveryOption);
+        $this->delivery_option = json_encode($deliveryOption);
     }
 
     /**