thirtybees/thirtybees

commit dcece6c25012af341c34c7126fbcb9c8904faa71

Password validation at login time is pointless.
This doesn't touch password verification, of course.

Validation wether a given password meets certain criteria makes
sense when setting the password, only. Too short or too long
passwords are now treated like non-matching passwords.

classes/Employee.php
@@ -379,7 +379,7 @@ class EmployeeCore extends ObjectModel
      */
     public function getByEmail($email, $plainTextPassword = null, $activeOnly = true)
     {
-        if (!Validate::isEmail($email) || ($plainTextPassword != null && !Validate::isPasswd($plainTextPassword))) {
+        if (!Validate::isEmail($email) || empty($plainTextPassword)) {
             die(Tools::displayError());
         }
 

controllers/admin/AdminLoginController.php
@@ -235,8 +235,6 @@ class AdminLoginControllerCore extends AdminController
 
         if (empty($passwd)) {
             $this->errors[] = Tools::displayError('The password field is blank.');
-        } elseif (!Validate::isPasswd($passwd)) {
-            $this->errors[] = Tools::displayError('Invalid password.');
         }
 
         if (!count($this->errors)) {

commit 41bac4cb6b165bfbd75be565b7d500c2a402a616

Fix Employee->getByEmail().
Commit dcece6c25012af341c34c7126fbcb9c8904faa71 broke it for
'forgot password' requests. My fault.

This should solve issue #568.

classes/Employee.php
@@ -435,7 +435,7 @@ class EmployeeCore extends ObjectModel
      */
     public function getByEmail($email, $plainTextPassword = null, $activeOnly = true)
     {
-        if (!Validate::isEmail($email) || empty($plainTextPassword)) {
+        if (!Validate::isEmail($email)) {
             return false;
         }
 