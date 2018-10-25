c7579babd102ee487657e742ce1f35c882206a73
Garanin George <5345528+truexakep@users.noreply.github.com>
2018-10-25 11:14:13 +0300

    Possibility to save html tags in CustomCode into the DB

--- controllers/admin/AdminCustomCodeController.php
+++ controllers/admin/AdminCustomCodeController.php
@@ -173,11 +173,12 @@ class AdminCustomCodeControllerCore extends AdminController
     /**
      * @param string $key
      * @param string $value
+     * @param bool   $htmlOK
      *
      * @throws PrestaShopDatabaseException
      * @throws PrestaShopException
      */
-    protected function updateOptionUnescaped($key, $value)
+    protected function updateOptionUnescaped($key, $value, $htmlOK = false)
     {
         if (Db::getInstance(_PS_USE_SQL_SLAVE_)->getValue(
             (new DbQuery())
@@ -188,7 +189,7 @@ class AdminCustomCodeControllerCore extends AdminController
             Db::getInstance()->update(
                 bqSQL(Configuration::$definition['table']),
                 [
-                    'value'    => pSQL($value),
+                    'value'    => pSQL($value, $htmlOK),
                     'date_upd' => ['type' => 'sql', 'value' => 'NOW()'],
                 ],
                 '`id_shop` IS NULL AND `id_shop_group` IS NULL AND `name` = \''.pSQL($key).'\'',
@@ -200,7 +201,7 @@ class AdminCustomCodeControllerCore extends AdminController
                 bqSQL(Configuration::$definition['table']),
                 [
                     'name'          => pSQL($key),
-                    'value'         => pSQL($value),
+                    'value'         => pSQL($value, $htmlOK),
                     'id_shop'       => null,
                     'id_shop_group' => null,
                     'date_add'      => ['type' => 'sql', 'value' => 'NOW()'],
@@ -221,7 +222,7 @@ class AdminCustomCodeControllerCore extends AdminController
                 Db::getInstance()->update(
                     bqSQL(Configuration::$definition['table']),
                     [
-                        'value'    => pSQL($value),
+                        'value'    => pSQL($value, $htmlOK),
                         'date_upd' => ['type' => 'sql', 'value' => 'NOW()'],
                     ],
                     '`id_shop` = '.(int) $idShop.' AND `id_shop_group` = '.$idShopGroup.' AND `name` = \''.pSQL($key).'\'',
@@ -233,7 +234,7 @@ class AdminCustomCodeControllerCore extends AdminController
                     bqSQL(Configuration::$definition['table']),
                     [
                         'name'          => pSQL($key),
-                        'value'         => pSQL($value),
+                        'value'         => pSQL($value, $htmlOK),
                         'id_shop'       => $idShop,
                         'id_shop_group' => $idShopGroup,
                         'date_add'      => ['type' => 'sql', 'value' => 'NOW()'],
