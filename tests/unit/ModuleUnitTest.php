c4ac06cc204c80373dc0e2f9d8290534b22a3fe5
Markus Hitter <mah@jump-ing.de>
2018-12-11 01:33:17 +0100

    ModuleUnitTest: use a concrete subclass for testing.
    Class Module is an abstract class. Running this test worked so far
    only because an override test is done before this test.
    
    Also equip both files with a license header.

--- tests/unit/ModuleUnitTest.php
+++ tests/unit/ModuleUnitTest.php
@@ -1,4 +1,23 @@
 <?php
+/**
+ * Copyright (C) 2017-2018 thirty bees
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.md.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@thirtybees.com so we can send you a copy immediately.
+ *
+ * @author    thirty bees <contact@thirtybees.com>
+ * @copyright 2017-2018 thirty bees
+ * @license   Open Software License (OSL 3.0)
+ */
+
+require_once __DIR__.'/support/TestModule.php';
 
 use AspectMock\Test as test;
 
@@ -11,49 +30,49 @@ class ModuleUnitTest extends \Codeception\Test\Unit
 
     public function testCheckCompliancyPS16()
     {
-        $mod = test::double('Module')->construct();
+        $mod = test::double('TestModule')->construct();
         $mod->ps_versions_compliancy = [ 'min' => '1.6', 'max' => '1.6.1.999'];
         $this->assertEquals(true, $mod->checkCompliancy());
     }
 
     public function testCheckCompliancyPS16OpenMax()
     {
-        $mod = test::double('Module')->construct();
+        $mod = test::double('TestModule')->construct();
         $mod->ps_versions_compliancy = [ 'min' => '1.6', 'max' => _PS_VERSION_];
         $this->assertEquals(true, $mod->checkCompliancy());
     }
 
     public function testCheckCompliancyPS1_6_1_19()
     {
-        $mod = test::double('Module')->construct();
+        $mod = test::double('TestModule')->construct();
         $mod->ps_versions_compliancy = [ 'min' => '1.6', 'max' => '1.6.1.19'];
         $this->assertEquals(false, $mod->checkCompliancy());
     }
 
     public function testCheckCompliancyPS1_6_1_20()
     {
-        $mod = test::double('Module')->construct();
+        $mod = test::double('TestModule')->construct();
         $mod->ps_versions_compliancy = [ 'min' => '1.6', 'max' => '1.6.1.20'];
         $this->assertEquals(true, $mod->checkCompliancy());
     }
 
     public function testCheckCompliancyPS1_6_1_99()
     {
-        $mod = test::double('Module')->construct();
+        $mod = test::double('TestModule')->construct();
         $mod->ps_versions_compliancy = [ 'min' => '1.6', 'max' => '1.6.1.99'];
         $this->assertEquals(true, $mod->checkCompliancy());
     }
 
     public function testCheckCompliancyPS17()
     {
-        $mod = test::double('Module')->construct();
+        $mod = test::double('TestModule')->construct();
         $mod->ps_versions_compliancy = [ 'min' => '1.7', 'max' => '1.7.1.999'];
         $this->assertEquals(false, $mod->checkCompliancy());
     }
 
     public function testCheckCompliancyPS17OpenMax()
     {
-        $mod = test::double('Module')->construct();
+        $mod = test::double('TestModule')->construct();
         $mod->ps_versions_compliancy = [ 'min' => '1.7', 'max' => _PS_VERSION_];
         $this->assertEquals(false, $mod->checkCompliancy());
     }
