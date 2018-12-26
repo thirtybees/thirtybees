42fc2f4dc738206c078c0620f4e7ad518f96e378
Petr Hučík <petr@getdatakick.com>
2018-12-27 00:36:19 +0100

    Introduce test for Configuration::updateValue
    Configuration::updateValue sanitize and escapes input value, and then
    stores it to database and to memory cache.
    
    This tests ensures that data in cache and database are consistent

---
+++ tests/integration/ConfigurationCoreTest.php
@@ -0,0 +1,69 @@
+<?php
+
+
+class ConfigurationCoreTest extends \Codeception\Test\Unit
+{
+
+    public function updateEscapingData()
+    {
+        return [
+            [ false, 'simple string' , 'simple string' ],
+            [ false, "string with ' apostrophe", "string with ' apostrophe"],
+            [ false, 'string with <a>html</a> tags', 'string with html tags'],
+            [ false, 'html &gt; entities > test', 'html &gt; entities > test'],
+            [ false, '{ "json": true }', '{ "json": true }' ],
+            //
+            [ true, 'simple string' , 'simple string' ],
+            [ true, "string with ' apostrophe", "string with ' apostrophe"],
+            [ true, 'string with <a>html</a> tags', 'string with <a>html</a> tags'],
+            [ true, 'html &gt; entities > test', 'html &gt; entities &gt; test'],
+            [ true, '{ "json": true }', '{ "json": true }' ],
+        ];
+    }
+
+    /**
+     * This tests verifies that values in configuration cache are properly updated
+     * during Configuration::updateValue method
+     *
+     * @dataProvider updateEscapingData
+     */
+    public function testUpdateValueCachedData($allowHtml, $input, $expected)
+    {
+        $key = 'TEST__ConfigurationTest_testUpdateValue';
+
+        // delete data
+        Configuration::deleteByName($key);
+        static::assertEquals(Configuration::get($key), false, "Key does not exists yet");
+
+        // update value
+        Configuration::updateValue($key, $input, $allowHtml);
+
+        // test that values in cache matches expectation
+        static::assertEquals($expected, Configuration::get($key), "Value matches immediately after update");
+    }
+
+
+    /**
+     * This tests verifies that data stored into database during Configuration::updateValue contains
+     * expected value
+     *
+     * @dataProvider updateEscapingData
+     */
+    public function testUpdateValueStoredData($allowHtml, $input, $expected)
+    {
+        $key = 'TEST__ConfigurationTest_testUpdateValue';
+
+        // delete data
+        Configuration::deleteByName($key);
+        static::assertEquals(Configuration::get($key), false, "Key does not exists yet");
+
+        // update value
+        Configuration::updateValue($key, $input, $allowHtml);
+
+        // flush configuration cache --> force reload from database
+        Configuration::clearConfigurationCacheForTesting();
+
+        // test that value read from db matches expectation
+        static::assertEquals($expected, Configuration::get($key), "Value matches when re-loaded from db");
+    }
+}
