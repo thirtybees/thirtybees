8a541290425bf532e9dd8cc0bf7e3423bebb350c
Petr Hučík <petr@getdatakick.com>
2019-01-13 17:46:29 +0100

    Add error handler to ErrorHandler class

--- classes/ErrorHandler.php
+++ classes/ErrorHandler.php
@@ -46,6 +46,16 @@ class ErrorHandlerCore
     protected $initialized = false;
 
     /**
+     * @var array list of errors, warnings and notices encountered during request processing
+     */
+    protected $errorMessages = [];
+
+    /**
+     * @var bool indicates, whether we should prevent default error handler or not
+     */
+    protected $preventDefaultErrorHandler = false;
+
+    /**
      * Get instance of error handler
      *
      * @return ErrorHandlerCore
@@ -74,10 +84,34 @@ class ErrorHandlerCore
             throw new PrestaShopException("Error handler already initialized");
         }
 
-        /* Set uncaught exception handler */
+        @ini_set('display_errors', 'off');
+        @error_reporting(E_ALL | E_STRICT);
+
+        // if we can't turn off display errors, we have to prevent default error handler instead
+        $this->preventDefaultErrorHandler = @ini_get('display_errors') !== 'off';
+
+        // Set uncaught exception handler
         set_exception_handler([$this, 'uncaughtExceptionHandler']);
 
+        // Set error handler
+        set_error_handler([$this, 'errorHandler']);
+
+
+
         $this->initialized = true;
+    }
+
+    /**
+     * @return array of collected error messages
+     */
+    public function getErrorMessages($includeSuppressed = false)
+    {
+        if ($this->errorMessages) {
+            return $includeSuppressed ? $this->errorMessages : array_filter($this->errorMessages, function ($error) {
+                return !$error['suppressed'];
+            });
+        }
+        return [];
     }
 
     /**
@@ -91,6 +125,69 @@ class ErrorHandlerCore
     {
         $exception = new PrestaShopException($e->getMessage(), $e->getCode(), null, $e->getTrace(), $e->getFile(), $e->getLine());
         $exception->displayMessage();
+    }
+
+
+    /**
+     * Error handler. It only records any error, warning or notice to $errors array
+     * and yields to default handler
+     *
+     * @param int $errno level of the error raised
+     * @param string $errstr error message
+     * @param string $errfile filename that the error was raised in
+     * @param int $errline line number the error was raised at
+     *
+     * @return bool
+     *
+     * @since   1.0.9
+     * @version 1.0.9 Initial version
+     */
+    public function errorHandler($errno, $errstr, $errfile, $errline)
+    {
+        // detect whether this message was
+        $suppressed = error_reporting() === 0;
+        $error = [
+
+            'errno' => $errno,
+            'errstr' => $errstr,
+            'errfile' => $errfile,
+            'errline' => $errline,
+            'suppressed' => $suppressed,
+            'type' => static::getErrorType($errno)
+        ];
+        $this->errorMessages[] = $error;
+
+        return $suppressed || $this->preventDefaultErrorHandler;
+    }
+
+    /**
+     * Returns error type for given error level
+     *
+     * @param int $errno level of the error raised
+     *
+     * @return string error type
+     *
+     * @since   1.0.9
+     * @version 1.0.9 Initial version
+     */
+    public static function getErrorType($errno)
+    {
+        switch ($errno) {
+            case E_USER_ERROR:
+            case E_ERROR:
+                return 'Fatal error';
+            case E_USER_WARNING:
+            case E_WARNING:
+                return 'Warning';
+            case E_USER_NOTICE:
+            case E_NOTICE:
+                return 'Notice';
+            case E_USER_DEPRECATED:
+            case E_DEPRECATED:
+                return 'Deprecation';
+            default:
+                return 'Unknown error';
+        }
     }
 
 }
