f6d1da4ea4fb58edb6431380971f237d752b26a3
Petr Hučík <petr@getdatakick.com>
2019-03-29 20:49:45 +0100

    ObjectModel: fix string length validation
    Validation for string length should be performed only on text fields,
    it does not make sense to check string length int/float values.

--- classes/ObjectModel.php
+++ classes/ObjectModel.php
@@ -1155,7 +1155,7 @@ abstract class ObjectModelCore implements Core_Foundation_Database_EntityInterfa
         }
 
         // Check field size
-        if (!in_array('size', $skip) && !empty($data['size'])) {
+        if (!in_array('size', $skip) && !empty($data['size']) && in_array($data['type'], [static::TYPE_STRING, static::TYPE_HTML])) {
             $size = $data['size'];
             if (!is_array($data['size'])) {
                 $size = ['min' => 0, 'max' => $data['size']];
@@ -1290,7 +1290,7 @@ abstract class ObjectModelCore implements Core_Foundation_Database_EntityInterfa
             }
 
             // Checking for maximum fields sizes
-            if (isset($data['size']) && !$isEmpty && mb_strlen($value) > $data['size']) {
+            if (isset($data['size']) && !$isEmpty && in_array($data['type'], [static::TYPE_STRING, static::TYPE_HTML]) && mb_strlen($value) > $data['size']) {
                 $errors[$field] = sprintf(
                     Tools::displayError('%1$s is too long. Maximum length: %2$d'),
                     static::displayFieldName($field, get_class($this), $htmlentities),
