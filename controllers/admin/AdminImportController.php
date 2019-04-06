thirtybees/thirtybees

commit 5de70ca997e91e0c248a579f4ec04f1b85e2ecbb

ImportController: remove unused code.
Found during development of tests/validate.sh.

controllers/admin/AdminImportController.php
@@ -2117,10 +2117,9 @@ class AdminImportControllerCore extends AdminController
 
             if ($regenerate) {
                 $previousPath = null;
-                $pathInfos = [];
-                $pathInfos[] = [$tgtWidth, $tgtHeight, $path.'.jpg'];
                 foreach ($imagesTypes as $imageType) {
-                    $tmpfile = static::get_best_path($imageType['width'], $imageType['height'], $pathInfos);
+                    $tmpfile = static::get_best_path($imageType['width'], $imageType['height'],
+                                                     [$tgtWidth, $tgtHeight, $path.'.jpg']);
 
                     $tgtWidthX2 = $tgtWidth * 2;
                     $tgtHeightX2 = $tgtHeight * 2;
@@ -2187,10 +2186,6 @@ class AdminImportControllerCore extends AdminController
                     }
 
                     if ($success) {
-                        // the last image should not be added in the candidate list if it's bigger than the original image
-                        if ($tgtWidth <= $srcWidth && $tgtHeight <= $srcHeight) {
-                            $pathInfos[] = [$tgtWidth, $tgtHeight, $path.'-'.stripslashes($imageType['name']).'.jpg'];
-                        }
                         if ($entity == 'products') {
                             if (is_file(_PS_TMP_IMG_DIR_.'product_mini_'.(int) $idEntity.'.jpg')) {
                                 unlink(_PS_TMP_IMG_DIR_.'product_mini_'.(int) $idEntity.'.jpg');

commit 1d584bb4695121ca30483e80fb574477eab72bef

CSV Import: Fix product image thumbnails regeneration
There used to be complicated mechanism to choose the best source image file when generating
product thumbnails. It used list of potential image sources. At the beginning, this list
contained only one file. Whenever new thumbnail was generated, its file was added to this
list of sources.

Commit 5de70ca997e91e0c248a579f4ec04f1b85e2ecbb broke this list genration mechanism, and result
was that no image thumbnails were generated during import.

We could revert this commit, but I don't see any reason why we should have this mechanism at all.
There is best source for resize operation -- source image file. There's no reason to resize
already resized image -- this can only lead to loss of image quality.

This original mechanism *may* be a little bit faster because it uses smaller image sources
for resize operation, but I don't think this benefit is significant enought to justify
image quality loss (and unnecesseary complicated php code)

Closes #884

controllers/admin/AdminImportController.php
@@ -2118,9 +2118,6 @@ class AdminImportControllerCore extends AdminController
             if ($regenerate) {
                 $previousPath = null;
                 foreach ($imagesTypes as $imageType) {
-                    $tmpfile = static::get_best_path($imageType['width'], $imageType['height'],
-                                                     [$tgtWidth, $tgtHeight, $path.'.jpg']);
-
                     $tgtWidthX2 = $tgtWidth * 2;
                     $tgtHeightX2 = $tgtHeight * 2;
                     $success = ImageManager::resize(
@@ -2208,29 +2205,6 @@ class AdminImportControllerCore extends AdminController
         unlink($origTmpfile);
 
         return true;
-    }
-
-    /**
-     * @param $tgtWidth
-     * @param $tgtHeight
-     * @param $pathInfos
-     *
-     * @return string
-     *
-     * @since 1.0.0
-     */
-    protected static function get_best_path($tgtWidth, $tgtHeight, $pathInfos)
-    {
-        $pathInfos = array_reverse($pathInfos);
-        $path = '';
-        foreach ($pathInfos as $pathInfo) {
-            list($width, $height, $path) = $pathInfo;
-            if ($width >= $tgtWidth && $height >= $tgtHeight) {
-                return $path;
-            }
-        }
-
-        return $path;
     }
 
     /**