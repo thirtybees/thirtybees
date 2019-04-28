be2813efa62a9bdce3f42450cc78632e29fa7e1b
Markus Hitter <mah@jump-ing.de>
2019-04-28 21:07:02 +0200

    Module: use version_compare() for comparing module versions.
    SemVer is limited to versions with 3 parts, trying to compare a
    version like '1.2.3.4' throws an exception. This behavior isn't
    compatible with some third party modules.
    
    This should fix issue #915.

--- classes/module/Module.php
+++ classes/module/Module.php
@@ -1057,7 +1057,11 @@ abstract class ModuleCore
             foreach ($modules as $name => $module) {
                 if (isset($modulesNameToCursor[mb_strtolower(strval($name))])) {
                     $moduleFromList = $modulesNameToCursor[mb_strtolower(strval($name))];
-                    if ($moduleFromList->version && Version::gt($module['version'], $moduleFromList->version)) {
+                    if ($moduleFromList->version && version_compare(
+                        $module['version'],
+                        $moduleFromList->version,
+                        '>'
+                    )) {
                         $moduleFromList->version_addons = $module['version'];
                         $modulesNameToCursor[mb_strtolower(strval($name))] = $moduleFromList;
                     }
