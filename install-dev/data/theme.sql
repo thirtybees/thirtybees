ca1f97db18f534d6a4d88e98c4ea41ce7a55e73c
Markus Hitter <mah@jump-ing.de>
2019-06-20 17:56:26 +0200

    Installer: 'the Netherlands' is lower case on the first word.
    This slipped in yet again :-/

--- install-dev/data/theme.sql
+++ install-dev/data/theme.sql
@@ -13,4 +13,4 @@ UPDATE `PREFIX_configuration` SET value = 'sales@yourcompany.com' WHERE name = '
 UPDATE `PREFIX_configuration` SET value = '0123-456-789' WHERE name = 'BLOCKCONTACTINFOS_PHONE';
 UPDATE `PREFIX_configuration` SET value = 'sales@yourcompany.com' WHERE name = 'BLOCKCONTACTINFOS_EMAIL';
 UPDATE `PREFIX_configuration` SET value = 'Your Company' WHERE name = 'BLOCKCONTACTINFOS_COMPANY';
-UPDATE `PREFIX_configuration` SET value = '42 Bee Lane\n12345 The Hive\nThe Netherlands' WHERE name = 'BLOCKCONTACTINFOS_ADDRESS';
+UPDATE `PREFIX_configuration` SET value = '42 Bee Lane\n12345 The Hive\nthe Netherlands' WHERE name = 'BLOCKCONTACTINFOS_ADDRESS';
