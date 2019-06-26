f988a7ef38345de18cbc791bb21738ce21ec573a
Petr Hučík <petr@getdatakick.com>
2019-06-26 12:13:17 +0200

    AdminConfigurationPagesTest: test AdminAddonsCatalog controller

--- tests/acceptance/back/AdminConfigurationPagesCest.php
+++ tests/acceptance/back/AdminConfigurationPagesCest.php
@@ -39,6 +39,7 @@ class AdminConfigurationPagesCest
         ],
         'AdminParentModules' => [
             'AdminModules',
+            'AdminAddonsCatalog',
             'AdminModulesPositions',
             'AdminPayment',
         ],
