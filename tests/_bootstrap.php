a5b78fa098220f1a00da0604936d8178e684e6ac
Petr Hučík <petr@getdatakick.com>
2019-06-26 10:34:32 +0200

    Tests: run tests with debug mode enabled
    Thirtybees has nasty habit to hide errors and warnings in production
    mode.
    
    If we run tests in debug mode, all errors, warnings, and
    even notices will be reported.

--- tests/_bootstrap.php
+++ tests/_bootstrap.php
@@ -1,5 +1,11 @@
 <?php
 // This is global bootstrap for autoloading
+
 if (!defined('TESTS_RUNNING')) {
     define('TESTS_RUNNING', true);
+}
+
+// Enable debug mode to detect all errors and warnings during test run
+if (!defined('_PS_MODE_DEV_')) {
+    define('_PS_MODE_DEV_', true);
 }
