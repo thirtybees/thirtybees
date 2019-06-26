14d0891183983e2e8860ab2af58dc3b1c7419d4c
Petr Hučík <petr@getdatakick.com>
2019-06-26 15:24:30 +0200

    AcceptanceTests: method to retrieve php errors
    When debug mode is enabled, then page can contain global
    variable phpMessages.
    
    This commit introduces new method that checks this global
    variable doesn't exists, or is empty. This way, we can assert
    that during page generation no error/warning/notice/deprecation
    was raised

--- tests/_support/AcceptanceTester.php
+++ tests/_support/AcceptanceTester.php
@@ -23,4 +23,19 @@ class AcceptanceTester extends \Codeception\Actor
    /**
     * Define custom actions here
     */
+
+    public function withoutErrors()
+    {
+        $result = json_decode($this->executeJS('return JSON.stringify(window.phpMessages || [])', 30), true);
+        if ($result) {
+            $messages = implode("\n", array_map(function($msg) {
+                $ret = "  - " .$msg['type'].': '.$msg['message'] . ' in file ' . $msg['file'];
+                if ($msg['line']) {
+                    $ret .= ' at line '.$msg['line'];
+                }
+                return $ret;
+            }, $result));
+            $this->fail("Page contains PHP messages:\n$messages\n");
+        }
+    }
 }
