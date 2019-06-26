6b41b0b4a23592ad6257cad1a9d64b470ab2e61a
Petr Hučík <petr@getdatakick.com>
2019-06-26 15:28:14 +0200

    CategoryPageTestCept: check for php messages

--- tests/acceptance/front/CategoryPageTestCept.php
+++ tests/acceptance/front/CategoryPageTestCept.php
@@ -5,3 +5,4 @@ $I->wantTo('Make sure the category page works');
 $I->resizeWindow(1920, 1080);
 $I->amOnPage('/index.php?id_category=3&controller=category&id_lang=1');
 $I->see('Coffee');
+$I->withoutErrors();
