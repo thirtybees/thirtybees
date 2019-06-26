1d0c945ef82b68ae0d771a4bb858ff8e2ff6dbdb
Petr Hučík <petr@getdatakick.com>
2019-06-26 15:27:53 +0200

    BasicGuestFiveStepCheckoutCept: check for php messages

--- tests/acceptance/front/BasicGuestFiveStepCheckoutCept.php
+++ tests/acceptance/front/BasicGuestFiveStepCheckoutCept.php
@@ -5,13 +5,20 @@ $I->wantTo('Buy a product');
 $I->resizeWindow(1920, 1080);
 $I->amOnPage('/index.php?id_product=1&controller=product');
 $I->see('Add to cart');
+$I->withoutErrors();
+
 $I->click('Add to cart');
 $I->waitForElementVisible(['css' => '.layer_cart_product']);
 $I->see('Product successfully added to your shopping cart');
+$I->withoutErrors();
+
 $I->click('Proceed to checkout');
+$I->withoutErrors();
 
 $I->click('Proceed to checkout');
 $I->see('Authentication');
+$I->withoutErrors();
+
 $I->fillField('#email_create', 'testbuyer@test.test');
 $I->click('#SubmitCreate');
 
@@ -20,22 +27,29 @@ $I->fillField(['css' => '#customer_firstname'], 'test');
 $I->fillField(['css' => '#customer_lastname'], 'test');
 $I->fillField(['css' => '#passwd'], 'testtest');
 $I->click('Register');
+$I->withoutErrors();
 
 $I->click('Proceed to checkout');
+$I->withoutErrors();
+
 $I->fillField(['css' => '#address1'], 'Address 1');
 $I->fillField(['css' => '#city'], 'City');
 $I->fillField(['css' => '#phone'], '1234567890');
 $I->selectOption(['css' => '#id_state'], 'Alabama');
 $I->fillField(['css' => '#postcode'], '12345');
 $I->click('Save');
+$I->withoutErrors();
 
 $I->click('Proceed to checkout');
+$I->withoutErrors();
 
 $I->click('label[for="cgv"]');
 $I->click('Proceed to checkout');
+$I->withoutErrors();
 
 $I->click('Pay by bank wire');
+$I->withoutErrors();
 
 $I->click('I confirm my order');
-
 $I->see('Please send us a bank wire');
+$I->withoutErrors();
