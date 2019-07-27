4cb45468d0431b61dbb8c0da79205d0e1882c1ae
Petr Hučík <petr@getdatakick.com>
2019-07-27 23:19:29 +0200

    DashboardController: don't prepare new_version_url
    Smarty variable $new_version_url is not used in any template, so there's
    no reason to pass it to view engine.
    
    Another reason to get rid of this definition is that it used deprecated
    constant _PS_API_DOMAIN_. And this one should be phased out of the
    codebase.

--- controllers/admin/AdminDashboardController.php
+++ controllers/admin/AdminDashboardController.php
@@ -304,7 +304,6 @@ class AdminDashboardControllerCore extends AdminController
             //'translations' => $translations,
             'action'                  => '#',
             'warning'                 => $this->getWarningDomainName(),
-            'new_version_url'         => Tools::getCurrentUrlProtocolPrefix()._PS_API_DOMAIN_.'/version/check_version.php?v='._PS_VERSION_.'&lang='.$this->context->language->iso_code.'&autoupgrade='.(int) (Module::isInstalled('autoupgrade') && Module::isEnabled('autoupgrade')).'&hosted_mode='.(int) defined('_PS_HOST_MODE_'),
             'dashboard_use_push'      => Configuration::get('PS_DASHBOARD_USE_PUSH'),
             'calendar'                => $calendarHelper->generate(),
             'PS_DASHBOARD_SIMULATION' => Configuration::get('PS_DASHBOARD_SIMULATION'),
