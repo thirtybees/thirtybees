0d8ddccd967eab8b03efca7ce1f6e409c3fb1664
Zengraph <nicozengraph@gmail.com>
2019-08-05 17:33:59 +0200

    Add versioning for JS admin theme files

--- admin-dev/themes/default/js/modernizr-loads.js
+++ admin-dev/themes/default/js/modernizr-loads.js
@@ -1,8 +1,8 @@
 Modernizr.load([
 	{
 		test: window.matchMedia,
-		nope: ["themes/default/js/vendor/matchMedia.js", "themes/default/js/vendor/matchMedia.addListener.js"]
+		nope: ["themes/default/js/vendor/matchMedia.js?version="+window['_PS_VERSION_'], "themes/default/js/vendor/matchMedia.addListener.js?version="+window['_PS_VERSION_']]
 	},
-	"themes/default/js/vendor/enquire.min.js",
-	"themes/default/js/admin-theme.js",
+	"themes/default/js/vendor/enquire.min.js?version="+window['_PS_VERSION_'],
+	"themes/default/js/admin-theme.js?version="+window['_PS_VERSION_'],
 ]);
