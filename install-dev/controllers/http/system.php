3ca6967dc8c44badd7e1d8b455320f6660a87b91
Markus Hitter <mah@jump-ing.de>
2019-10-02 22:14:26 +0200

    Installer: add missing test messages.
    There have been cases with failures detected, but due to a missing
    error message not reported to the user. Which led to a refusal to
    install, without any hint on what's wrong.
    
    See https://forum.thirtybees.com/topic/3512-x
    and https://forum.thirtybees.com/topic/3005-x

--- install-dev/controllers/http/system.php
+++ install-dev/controllers/http/system.php
@@ -94,7 +94,24 @@ class InstallControllerHttpSystem extends InstallControllerHttp
                     'title'   => $this->l('Required PHP parameters'),
                     'success' => 1,
                     'checks'  => [
+                        // This list should have a message for every test
+                        // in ConfigurationTest::getDefaultTests().
+                        // Exceptions: 'MysqlVersion', 'Files'
                         'Bcmath'           => $this->l('The PHP bcmath extension is enabled'),
+                        'CacheDir'         => $this->l('Can write to cache/');
+                        'LogDir'           => $this->l('Can write to log/');
+                        'ImgDir'           => $this->l('Can write to img/');
+                        'ModuleDir'        => $this->l('Can write to modules/');
+                        'ThemeLangDir'     => $this->l('Can write to themes/niara/lang/');
+                        'ThemePdfLangDir'  => $this->l('Can write to themes/niara/pdf/lang/');
+                        'ThemeCacheDir'    => $this->l('Can write to themes/niara/cache/');
+                        'TranslationsDir'  => $this->l('Can write to translations/');
+                        'CustomizableProductsDir' => $this->l('Can write to upload/');
+                        'VirtualProductsDir'      => $this->l('Can write to download/');
+                        'ConfigDir'        => $this->l('Can write to config/');
+                        'MailsDir'         => $this->l('Can write to mails/');
+                        'System'           => $this->l('Critical PHP functions exist');
+                        'Fopen'            => $this->l('PHP\'s \'allow_url_fopen\' enabled'),
                         'Gd'               => $this->l('GD library is installed'),
                         'Json'             => $this->l('The PHP json extension is enabled'),
                         'MaxExecutionTime' => $this->l('Max execution time is higher than 30'),
