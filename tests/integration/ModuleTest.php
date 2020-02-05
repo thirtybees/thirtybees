thirtybees/thirtybees

commit 00833e37e225c3af89cc4466dcafca86cd393024

Module: implement getModuleNameById method
Add helper static method Module::getModuleNameById($moduleId) that
resolves module name by its ID.

Also, this commit introduces new cache for module name -> id mapping.
This increases performance, as all calls to Module::getModuleIdByName
dosn't have to access database anymore

classes/module/Module.php
@@ -1708,19 +1708,54 @@ abstract class ModuleCore
      */
     public static function getModuleIdByName($name)
     {
-        $cacheId = 'Module::getModuleIdByName_'.pSQL($name);
-        if (!Cache::isStored($cacheId)) {
-            $result = (int) Db::getInstance(_PS_USE_SQL_SLAVE_)->getValue(
-                (new DbQuery())
-                    ->select('`id_module`')
-                    ->from('module')
-                    ->where('`name` = \''.pSQL($name).'\'')
-            );
-            Cache::store($cacheId, $result);
+        $map = static::getModulesNameToIdMap();
+        return isset($map[$name]) ? $map[$name] : 0;
+    }
 
-            return $result;
+    /**
+     * Get module name by ID
+     *
+     * @param int $moduleId
+     *
+     * @since   1.1.1
+     * @return string | null
+     * @throws PrestaShopException
+     */
+    public static function getModuleNameById($moduleId)
+    {
+        $map = static::getModulesNameToIdMap();
+        foreach ($map as $moduleName => $id) {
+            if ($moduleId === $moduleId) {
+                return $moduleName;
+            }
         }
+        return null;
+    }
 
+    /**
+     * Returns mapping from modules name -> module IDs
+     *
+     * @since 1.1.1
+     * @return array
+     * @throws PrestaShopException
+     */
+    protected static function getModulesNameToIdMap()
+    {
+        $cacheId = 'Module::getModulesNameToIdMap';
+        if (!Cache::isStored($cacheId)) {
+            $sql = (new DbQuery())
+                ->select('`id_module`, `name`')
+                ->from('module');
+            $map = [];
+            foreach (Db::getInstance(_PS_USE_SQL_SLAVE_)->query($sql) as $row) {
+                $moduleId = (int)$row['id_module'];
+                $name = $row['name'];
+                $map[$name] = $moduleId;
+            }
+            Cache::store($cacheId, $map);
+
+            return $map;
+        }
         return Cache::retrieve($cacheId);
     }
 
@@ -2397,7 +2432,7 @@ abstract class ModuleCore
 
         // Uninstall the module
         if (Db::getInstance()->delete('module' , '`id_module` = '.(int) $this->id)) {
-            Cache::clean('Module::getModuleIdByName_'.pSQL($this->name));
+            Cache::clean('Module::getModulesNameToIdMap');
 
             return true;
         }

commit 29ed5d7d229d8a9e6d1ac84f9babe97668bebeaa

Module names: fix backwards compatibility
In previous versions of thirtybees, following statements were true:

  Module::getModuleIdByName('mymod') == Module::getModuleIdByName('MyMod')
  Module::isEnabled('mymod') == Module::isEnabled('MyMod')

This was was broken in commit 00833e37e225c3af89cc4466dcafca86cd393024,
and this commit reintroduce this behaviour for backwards compatibility.

Closes #1137

classes/module/Module.php
@@ -453,21 +453,24 @@ abstract class ModuleCore
             return false;
         }
 
-        if (!isset(static::$_INSTANCE[$moduleName])) {
-            if (! static::moduleExistsOnFilesystem($moduleName)) {
-                return false;
-            }
+        $className = strtolower($moduleName);
 
-            return Module::coreLoadModule($moduleName);
+        if (!isset(static::$_INSTANCE[$className])) {
+
+            $module = static::moduleExistsOnFilesystem($moduleName)
+                ? Module::coreLoadModule($moduleName)
+                : false;
+
+            static::$_INSTANCE[$className] = $module;
         }
 
-        return static::$_INSTANCE[$moduleName];
+        return static::$_INSTANCE[$className];
     }
 
     /**
      * @param $moduleName
      *
-     * @return bool|mixed|object
+     * @return Module
      *
      * @throws Adapter_Exception
      * @throws PrestaShopDatabaseException
@@ -478,7 +481,6 @@ abstract class ModuleCore
     protected static function coreLoadModule($moduleName)
     {
         // Define if we will log modules performances for this session
-        // @codingStandardsIgnoreStart
         if (Module::$_log_modules_perfs === null) {
             $modulo = _PS_DEBUG_PROFILING_ ? 1 : Configuration::get('PS_log_modules_perfs_MODULO');
             Module::$_log_modules_perfs = ($modulo && mt_rand(0, $modulo - 1) == 0);
@@ -492,27 +494,10 @@ abstract class ModuleCore
             $timeStart = microtime(true);
             $memoryStart = memory_get_usage(true);
         }
-        // @codingStandardsIgnoreEnd
 
-        include_once(_PS_MODULE_DIR_.$moduleName.'/'.$moduleName.'.php');
+        $module = static::instantiateModule($moduleName);
 
-        $r = false;
-        if (Tools::file_exists_no_cache(_PS_OVERRIDE_DIR_.'modules/'.$moduleName.'/'.$moduleName.'.php')) {
-            include_once(_PS_OVERRIDE_DIR_.'modules/'.$moduleName.'/'.$moduleName.'.php');
-            $override = $moduleName.'Override';
-
-            if (class_exists($override, false)) {
-                $r = static::$_INSTANCE[$moduleName] = Adapter_ServiceLocator::get($override);
-            }
-        }
-
-        if (!$r && class_exists($moduleName, false)) {
-            $r = static::$_INSTANCE[$moduleName] = Adapter_ServiceLocator::get($moduleName);
-        }
-
-        // @codingStandardsIgnoreStart
         if (Module::$_log_modules_perfs) {
-            // @codingStandardsIgnoreEnd
             $timeEnd = microtime(true);
             $memoryEnd = memory_get_usage(true);
 
@@ -530,7 +515,35 @@ abstract class ModuleCore
             );
         }
 
-        return $r;
+        return $module;
+    }
+
+    /**
+     * @param String $moduleName
+     *
+     * @return Module
+     * @throws Adapter_Exception
+     * @throws PrestaShopException
+     */
+    protected static function instantiateModule($moduleName)
+    {
+        include_once(_PS_MODULE_DIR_.$moduleName.'/'.$moduleName.'.php');
+
+        if (Tools::file_exists_no_cache(_PS_OVERRIDE_DIR_.'modules/'.$moduleName.'/'.$moduleName.'.php')) {
+
+            include_once(_PS_OVERRIDE_DIR_.'modules/'.$moduleName.'/'.$moduleName.'.php');
+            $override = $moduleName.'Override';
+
+            if (class_exists($override, false)) {
+                return Adapter_ServiceLocator::get($override);
+            }
+        }
+
+        if (class_exists($moduleName, false)) {
+            return Adapter_ServiceLocator::get($moduleName);
+        }
+
+        throw new PrestaShopException("Failed to instantiate module '$moduleName'");
     }
 
     /**
@@ -1711,7 +1724,8 @@ abstract class ModuleCore
     public static function getModuleIdByName($name)
     {
         $map = static::getModulesNameToIdMap();
-        return isset($map[$name]) ? $map[$name] : 0;
+        $key = strtolower($name);
+        return isset($map[$key]) ? $map[$key] : 0;
     }
 
     /**
@@ -1751,7 +1765,7 @@ abstract class ModuleCore
             $map = [];
             foreach (Db::getInstance(_PS_USE_SQL_SLAVE_)->query($sql) as $row) {
                 $moduleId = (int)$row['id_module'];
-                $name = $row['name'];
+                $name = strtolower($row['name']);
                 $map[$name] = $moduleId;
             }
             Cache::store($cacheId, $map);
@@ -3118,9 +3132,11 @@ abstract class ModuleCore
             return false;
         }
 
+        $name = strtolower($moduleName);
+
         return (
-            is_dir(_PS_MODULE_DIR_. $moduleName. DIRECTORY_SEPARATOR) &&
-            Tools::file_exists_no_cache(_PS_MODULE_DIR_ . $moduleName . '/' . $moduleName . '.php')
+            is_dir(_PS_MODULE_DIR_. $name. DIRECTORY_SEPARATOR) &&
+            Tools::file_exists_no_cache(_PS_MODULE_DIR_ . $name . '/' . $name . '.php')
         );
     }
 

tests/integration/ModuleTest.php
@@ -45,4 +45,28 @@ class ModuleTest extends \Codeception\Test\Unit
             $this->assertTrue((bool)$module->uninstall(), 'Module uninstall failed : '.$moduleName);
         }
     }
+
+    public function testValidModuleNameIsEnabled()
+    {
+        $this->assertTrue(Module::isEnabled("coreupdater"));
+    }
+
+    public function testValidModuleNameGetModuleId() {
+        $this->assertTrue(!!Module::getModuleIdByName("coreupdater"));
+    }
+
+    public function testBackwardCompatibilityModuleNameIsEnabled()
+    {
+        $this->assertTrue(Module::isEnabled("CoreUpdater"));
+    }
+
+    public function testBackwardCompatibilityModuleNameGetModuleId() {
+        $this->assertTrue(!!Module::getModuleIdByName("CoreUpdater"));
+    }
+
+    public function testMultipleInstantiation() {
+        $this->assertTrue(!!Module::getInstanceByName("coreupdater"));
+        $this->assertTrue(!!Module::getInstanceByName("CoreUpdater"));
+    }
+
 }