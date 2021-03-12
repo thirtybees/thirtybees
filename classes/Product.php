d8fc1d5d015c18a062e55066b6b98d35be50e63d
Petr Hučík <petr@getdatakick.com>
2021-03-12 11:19:34 +0100

    Webservice: fix product creation php errors
    Fix php errors thrown during product creation via API.

--- classes/Product.php
+++ classes/Product.php
@@ -7392,7 +7392,7 @@ class ProductCore extends ObjectModel
     {
         $result = $this->getCover($this->id);
 
-        return $result['id_image'];
+        return isset($result['id_image']) ? $result['id_image'] : null;
     }
 
     /**
@@ -8039,6 +8039,9 @@ class ProductCore extends ObjectModel
      */
     public function modifierWsLinkRewrite()
     {
+        if (! $this->link_rewrite) {
+            $this->link_rewrite = [];
+        }
         foreach ($this->name as $idLang => $name) {
             if (empty($this->link_rewrite[$idLang])) {
                 $this->link_rewrite[$idLang] = Tools::link_rewrite($name);
