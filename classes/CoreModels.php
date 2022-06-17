d8b040da6c75b118cc25f21dd46b47a3b10ce5af
Emanuel Schiendorfer <mandi.schiendorfer@gmail.com>
2022-06-17 13:24:15 +0200

    Adding feature_product_lang table by adding displayable field

--- classes/CoreModels.php
+++ classes/CoreModels.php
@@ -395,6 +395,7 @@ class CoreModelsCore
                 'id_feature'       => ['type' => ObjectModel::TYPE_INT, 'required' => true],
                 'id_product'       => ['type' => ObjectModel::TYPE_INT, 'required' => true],
                 'id_feature_value' => ['type' => ObjectModel::TYPE_INT, 'required' => true],
+                'displayable'      => ['type' => ObjectModel::TYPE_STRING, 'lang' => true, 'validate' => 'isGenericName', 'size' => 255],
             ],
             'keys' => [
                 'feature_product' => [
