thirtybees/thirtybees

commit 187f9dfa71901b4218a131acf3228612f1ea4aee

AdminController: fix php notices
Function preg_match should not be called with null input.

classes/controller/AdminController.php
@@ -1063,7 +1063,7 @@ class AdminControllerCore extends Controller
         }
 
         if ($this->multishop_context && Shop::isTableAssociated($this->table) && !empty($this->className)) {
-            if (Shop::getContext() != Shop::CONTEXT_ALL || !$this->context->employee->isSuperAdmin()) {
+            if ($this->_join && Shop::getContext() != Shop::CONTEXT_ALL || !$this->context->employee->isSuperAdmin()) {
                 $testJoin = !preg_match('#`?'.preg_quote(_DB_PREFIX_.$this->table.'_shop').'`? *sa#', $this->_join);
                 if (Shop::isFeatureActive() && $testJoin && Shop::isTableAssociated($this->table)) {
                     $this->_where .= ' AND EXISTS (
@@ -1116,7 +1116,7 @@ class AdminControllerCore extends Controller
                         $this->_listsql .= str_replace('!', '.`', $arrayValue['filter_key']).'` AS `'.$key.'`, ';
                     } elseif ($key == 'id_'.$this->table) {
                         $this->_listsql .= 'a.`'.bqSQL($key).'`, ';
-                    } elseif ($key != 'image' && !preg_match('/'.preg_quote($key, '/').'/i', $this->_select)) {
+                    } elseif ($key != 'image' && $this->_select && !preg_match('/'.preg_quote($key, '/').'/i', $this->_select)) {
                         $this->_listsql .= '`'.bqSQL($key).'`, ';
                     }
                 }

commit d58ec9e678ae822eb2ce22bf35e5e20e9b58a5ed

Fix List display issue
This problem was introduced by commit
187f9dfa71901b4218a131acf3228612f1ea4aee.

Property AdminController::_select can be null, and this causes warning
messages in PHP8 when used as parameter to preg_match. The fix was
for this problem was invalid, though.

This commit reverts the original fix, and replace it with a better one.

Closes #1465

classes/controller/AdminController.php
@@ -1063,8 +1063,8 @@ class AdminControllerCore extends Controller
         }
 
         if ($this->multishop_context && Shop::isTableAssociated($this->table) && !empty($this->className)) {
-            if ($this->_join && Shop::getContext() != Shop::CONTEXT_ALL || !$this->context->employee->isSuperAdmin()) {
-                $testJoin = !preg_match('#`?'.preg_quote(_DB_PREFIX_.$this->table.'_shop').'`? *sa#', $this->_join);
+            if (Shop::getContext() != Shop::CONTEXT_ALL || !$this->context->employee->isSuperAdmin()) {
+                $testJoin = !preg_match('#`?'.preg_quote(_DB_PREFIX_.$this->table.'_shop').'`? *sa#', $this->_join ?? '');
                 if (Shop::isFeatureActive() && $testJoin && Shop::isTableAssociated($this->table)) {
                     $this->_where .= ' AND EXISTS (
 						SELECT 1
@@ -1116,7 +1116,7 @@ class AdminControllerCore extends Controller
                         $this->_listsql .= str_replace('!', '.`', $arrayValue['filter_key']).'` AS `'.$key.'`, ';
                     } elseif ($key == 'id_'.$this->table) {
                         $this->_listsql .= 'a.`'.bqSQL($key).'`, ';
-                    } elseif ($key != 'image' && $this->_select && !preg_match('/'.preg_quote($key, '/').'/i', $this->_select)) {
+                    } elseif ($key != 'image' && !preg_match('/'.preg_quote($key, '/').'/i', $this->_select ?? '')) {
                         $this->_listsql .= '`'.bqSQL($key).'`, ';
                     }
                 }