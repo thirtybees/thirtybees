48f2ccac2ae0af016c6940ccc3a830109b7fe0f1
Petr Hučík <petr@getdatakick.com>
2022-12-07 13:19:10 +0100

    InstallXmlLoader::loadEntity - consistent return
    Method should always return SimpleXMLElement, or throw. Returning
    void/null will only cause strange errors down the line.

--- install-dev/classes/xmlLoader.php
+++ install-dev/classes/xmlLoader.php
@@ -238,10 +238,6 @@ class InstallXmlLoader
     protected function loadEntity($entity, $iso = null)
     {
         if (!isset($this->cache_xml_entity[$this->path_type][$entity][$iso])) {
-            if (substr($entity, 0, 1) == '.' || substr($entity, 0, 1) == '_') {
-                return;
-            }
-
             $path = $this->data_path.$entity.'.xml';
             if ($iso) {
                 $path = $this->lang_path.$this->getFallBackToDefaultLanguage($iso).'/data/'.$entity.'.xml';
