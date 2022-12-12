625501270306dac1d009639e1a3c1dbf1b23d88c
Petr Hučík <petr@getdatakick.com>
2022-12-12 16:26:51 +0100

    Forward themes and modules
      - blockcms
      - blockcontactinfos
      - blockviewed
      - blockslider
      - sendtoafriend
      - socialsharing
      - tbupdater
      - themeconfigurator
      - vatnumber
      - niara
    
    Also removed homeslider module configuration during system installation.
    This was replaced by more advanced fixture installation process inside
    homeslider module.

--- install-dev/data/theme.sql
+++ install-dev/data/theme.sql
@@ -17,15 +17,3 @@ UPDATE `PREFIX_configuration` SET value = '42 Bee Lane\n12345 The Hive\nthe Neth
 
 UPDATE `PREFIX_configuration` SET value = '4' WHERE name = 'PS_BLOCK_BESTSELLERS_TO_DISPLAY';
 UPDATE `PREFIX_configuration` SET value = '4' WHERE name = 'HOME_FEATURED_NBR';
-
-/* Home Slider Changes for Niara theme */
-UPDATE `PREFIX_configuration` SET value = '1140' WHERE name = 'HOMESLIDER_WIDTH';
-UPDATE `PREFIX_homeslider_slides_lang` SET `image` = 'sample-4.jpg' WHERE `id_homeslider_slides` = 1;
-UPDATE `PREFIX_homeslider_slides_lang` SET `image` = 'sample-5.jpg' WHERE `id_homeslider_slides` = 2;
-UPDATE `PREFIX_homeslider_slides_lang` SET `image` = 'sample-6.jpg' WHERE `id_homeslider_slides` = 3;
-UPDATE `PREFIX_homeslider_slides_lang` SET `description` = '<h3 style="float:left;clear:both;font-size:30px;">Shop Tea</h3>
-<p style="text-align:center;margin-top:20px;"><button class="btn btn-default" type="button">Shop now !</button></p>' WHERE `id_homeslider_slides` = 1;
-UPDATE `PREFIX_homeslider_slides_lang` SET `description` = '<h3 style="float:left;clear:both;font-size:30px;">View All Soaps</h3>
-<p style="text-align:center;margin-top:20px;"><button class="btn btn-default" type="button">Shop now !</button></p>' WHERE `id_homeslider_slides` = 2;
-UPDATE `PREFIX_homeslider_slides_lang` SET `description` = '<h3 style="float:left;clear:both;font-size:30px;">Shop Gifts</h3>
-<p style="text-align:center;margin-top:20px;"><button class="btn btn-default" type="button">Shop now !</button></p>' WHERE `id_homeslider_slides` = 3;
