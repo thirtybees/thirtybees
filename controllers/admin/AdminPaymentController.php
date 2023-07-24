136017aedcbf46f708b31df96ea2c11fd8acecf3
Petr Hučík <petr@getdatakick.com>
2023-07-24 13:47:51 +0200

    Fix payment module restriction display
    When creating list of payment modules, information about modules could
    be read from xml files. This xml file does not contain all the
    information about module, specifically properties $limited_countries,
    $currencies_mode, and $currencies.
    
    This caused transient/cache errors.
    
    Everything was displayed properly, when information about module was
    taken from module instance (for example when $need_instance was set to
    1, or when xml file was not in sync with module php file).
    
    This was not the case when module info was taken from xml files.
    
    This commit fixes this issue by not depending on module config.xml
    files. It always instantiate module to access these properties.
    
    Related to #1704

--- controllers/admin/AdminPaymentController.php
+++ controllers/admin/AdminPaymentController.php
@@ -53,69 +53,61 @@ class AdminPaymentControllerCore extends AdminController
         $modules = Module::getModulesOnDisk(true);
 
         foreach ($modules as $module) {
-            if ($module->tab == 'payments_gateways') {
-                if ($module->id) {
-                    if (!get_class($module) == 'SimpleXMLElement') {
-                        $module->country = [];
-                    }
-                    $sql = new DbQuery();
-                    $sql->select('`id_country`');
-                    $sql->from('module_country');
-                    $sql->where('`id_module` = '.(int) $module->id);
-                    $sql->where('`id_shop` = '.(int) $idShop);
+            if ($module->tab == 'payments_gateways' && Module::isEnabled($module->name)) {
+                $instance = Module::getInstanceByName($module->name);
 
-                    $conn = Db::readOnly();
-                    $countries = $conn->getArray($sql);
-                    foreach ($countries as $country) {
-                        $module->country[] = $country['id_country'];
-                    }
+                // sync settings
+                $module->limited_countries = $instance->limited_countries;
+                $module->currencies_mode = $instance->currencies_mode ?? 'checkbox';
+                $module->currencies = $instance->currencies ?? true;
 
-                    if (!get_class($module) == 'SimpleXMLElement') {
-                        $module->currency = [];
-                    }
-                    $sql = new DbQuery();
-                    $sql->select('`id_currency`');
-                    $sql->from('module_currency');
-                    $sql->where('`id_module` = '.(int) $module->id);
-                    $sql->where('`id_shop` = '.(int) $idShop);
+                $module->country = [];
+                $sql = new DbQuery();
+                $sql->select('`id_country`');
+                $sql->from('module_country');
+                $sql->where('`id_module` = '.(int) $module->id);
+                $sql->where('`id_shop` = '.(int) $idShop);
 
-                    $currencies = $conn->getArray($sql);
-                    foreach ($currencies as $currency) {
-                        $module->currency[] = $currency['id_currency'];
-                    }
+                $conn = Db::readOnly();
+                $countries = $conn->getArray($sql);
+                foreach ($countries as $country) {
+                    $module->country[] = $country['id_country'];
+                }
 
-                    if (!get_class($module) == 'SimpleXMLElement') {
-                        $module->group = [];
-                    }
-                    $sql = new DbQuery();
-                    $sql->select('`id_group`');
-                    $sql->from('module_group');
-                    $sql->where('`id_module` = '.(int) $module->id);
-                    $sql->where('`id_shop` = '.(int) $idShop);
+                $module->currency = [];
+                $sql = new DbQuery();
+                $sql->select('`id_currency`');
+                $sql->from('module_currency');
+                $sql->where('`id_module` = '.(int) $module->id);
+                $sql->where('`id_shop` = '.(int) $idShop);
 
-                    $groups = $conn->getArray($sql);
-                    foreach ($groups as $group) {
-                        $module->group[] = $group['id_group'];
-                    }
+                $currencies = $conn->getArray($sql);
+                foreach ($currencies as $currency) {
+                    $module->currency[] = $currency['id_currency'];
+                }
 
-                    if (!get_class($module) == 'SimpleXMLElement') {
-                        $module->reference = [];
-                    }
-                    $sql = new DbQuery();
-                    $sql->select('`id_reference`');
-                    $sql->from('module_carrier');
-                    $sql->where('`id_module` = '.(int) $module->id);
-                    $sql->where('`id_shop` = '.(int) $idShop);
+                $module->group = [];
+                $sql = new DbQuery();
+                $sql->select('`id_group`');
+                $sql->from('module_group');
+                $sql->where('`id_module` = '.(int) $module->id);
+                $sql->where('`id_shop` = '.(int) $idShop);
 
-                    $carriers = $conn->getArray($sql);
-                    foreach ($carriers as $carrier) {
-                        $module->reference[] = $carrier['id_reference'];
-                    }
-                } else {
-                    $module->country = null;
-                    $module->currency = null;
-                    $module->group = null;
-                    $module->reference = null;
+                $groups = $conn->getArray($sql);
+                foreach ($groups as $group) {
+                    $module->group[] = $group['id_group'];
+                }
+
+                $module->reference = [];
+                $sql = new DbQuery();
+                $sql->select('`id_reference`');
+                $sql->from('module_carrier');
+                $sql->where('`id_module` = '.(int) $module->id);
+                $sql->where('`id_shop` = '.(int) $idShop);
+
+                $carriers = $conn->getArray($sql);
+                foreach ($carriers as $carrier) {
+                    $module->reference[] = $carrier['id_reference'];
                 }
 
                 $this->payment_modules[] = $module;
@@ -330,22 +322,12 @@ class AdminPaymentControllerCore extends AdminController
                         $list['items'][$keyItem]['check_list'][$keyModule] = 'unchecked';
                     }
 
-                    if (!isset($module->$nameId)) {
-                        $module->$nameId = [];
-                    }
-                    if (!isset($module->currencies_mode)) {
-                        $module->currencies_mode = '';
-                    }
-                    if (!isset($module->currencies)) {
-                        $module->currencies = '';
-                    }
-
                     // If is a country list and the country is limited, remove it from list
-                    if ($nameId == 'country'
-                        && isset($module->limited_countries)
+                    if ($nameId === 'country'
                         && !empty($module->limited_countries)
                         && is_array($module->limited_countries)
-                        && !(in_array(strtoupper($item['iso_code']), array_map('strtoupper', $module->limited_countries)))) {
+                        && !(in_array(strtoupper($item['iso_code']), array_map('strtoupper', $module->limited_countries)))
+                    ) {
                         $list['items'][$keyItem]['check_list'][$keyModule] = null;
                     }
                 }
@@ -364,12 +346,5 @@ class AdminPaymentControllerCore extends AdminController
         ];
 
         return parent::renderView();
-    }
-
-    /**
-     * @return void
-     */
-    public function renderModulesList()
-    {
     }
 }
