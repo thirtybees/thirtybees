thirtybees/thirtybees

commit c93756a842b44da80d2a76ae0c4813cd75e62979

Use hook interface instead of direct evaluation
Method TaxManagerFactory::execHookTaxManagerFactory called modules hook
directly. We should use Hook methods to invoke hook instead.

classes/tax/TaxManagerFactory.php
@@ -35,7 +35,7 @@
 class TaxManagerFactoryCore
 {
     /**
-     * @var TaxManagerInterface
+     * @var TaxManagerInterface[]
      */
     protected static $cache_tax_manager;
 
@@ -43,26 +43,26 @@ class TaxManagerFactoryCore
      * Returns a tax manager able to handle this address
      *
      * @param Address $address
-     * @param int $type TaxRulesGroup id
+     * @param int $taxRuleGroupId TaxRulesGroup id
      *
      * @return TaxManagerInterface
      *
      * @throws PrestaShopDatabaseException
      * @throws PrestaShopException
      */
-    public static function getManager(Address $address, $type)
+    public static function getManager(Address $address, $taxRuleGroupId)
     {
-        $cacheId = TaxManagerFactory::getCacheKey($address).'-'.$type;
-        if (!isset(TaxManagerFactory::$cache_tax_manager[$cacheId])) {
-            $taxManager = TaxManagerFactory::execHookTaxManagerFactory($address, $type);
-            if (!($taxManager instanceof TaxManagerInterface)) {
-                $taxManager = new TaxRulesTaxManager($address, $type);
+        $cacheId = static::getCacheKey($address).'-'.$taxRuleGroupId;
+        if (!isset(static::$cache_tax_manager[$cacheId])) {
+            $taxManager = static::execHookTaxManagerFactory($address, $taxRuleGroupId);
+            if ($taxManager) {
+                static::$cache_tax_manager[$cacheId] = $taxManager;
+            } else {
+                static::$cache_tax_manager[$cacheId] = new TaxRulesTaxManager($address, $taxRuleGroupId);
             }
-
-            TaxManagerFactory::$cache_tax_manager[$cacheId] = $taxManager;
         }
 
-        return TaxManagerFactory::$cache_tax_manager[$cacheId];
+        return static::$cache_tax_manager[$cacheId];
     }
 
     /**
@@ -78,26 +78,22 @@ class TaxManagerFactoryCore
      */
     public static function execHookTaxManagerFactory(Address $address, $type)
     {
-        $modulesInfos = Hook::getModulesFromHook(Hook::getIdByName('taxManager'));
-        $taxManager = false;
+        $hookName = 'taxManager';
+        $modules = Hook::getModulesFromHook($hookName);
 
-        foreach ($modulesInfos as $moduleInfos) {
-            $moduleInstance = Module::getInstanceByName($moduleInfos['name']);
-            if (is_callable([$moduleInstance, 'hookTaxManager'])) {
-                $taxManager = $moduleInstance->hookTaxManager(
-                    [
-                        'address' => $address,
-                        'params'  => $type,
-                    ]
-                );
-            }
-
-            if ($taxManager) {
-                break;
+        foreach ($modules as $module) {
+            $moduleId = (int)$module['id_module'];
+            /** @var TaxManagerInterface|false|null $taxManager */
+            $taxManager = Hook::getResponse($hookName, $moduleId, [
+                'address' => $address,
+                'params' => $type,
+            ]);
+            if ($taxManager instanceof TaxManagerInterface) {
+                return $taxManager;
             }
         }
 
-        return $taxManager;
+        return false;
     }
 
     /**

classes/Product.php
@@ -1382,7 +1382,7 @@ class ProductCore extends ObjectModel
      * @param int $idProduct
      * @param Context|null $context
      *
-     * @return int|null
+     * @return int
      *
      * @throws PrestaShopException
      */

classes/tax/TaxManagerModule.php
@@ -53,7 +53,7 @@ abstract class TaxManagerModuleCore extends Module
     /**
      * @param array $args
      *
-     * @return bool
+     * @return TaxManagerInterface|false
      *
      * @throws PrestaShopException
      */

commit 8df21a62541e06ebbbe27595a8032a211a95de1b

Fix TaxManagerFactory::execHookTaxManagerFactory
Commit c93756a842b44da80d2a76ae0c4813cd75e62979 introduced serious bug.

Method Hook::getModulesFromHook expects hook ID and not hook name. This
blocked creation of specific tax managers, for example VAT tax manager.

classes/tax/TaxManagerFactory.php
@@ -79,7 +79,7 @@ class TaxManagerFactoryCore
     public static function execHookTaxManagerFactory(Address $address, $type)
     {
         $hookName = 'taxManager';
-        $modules = Hook::getModulesFromHook($hookName);
+        $modules = Hook::getModulesFromHook(Hook::getIdByName($hookName));
 
         foreach ($modules as $module) {
             $moduleId = (int)$module['id_module'];