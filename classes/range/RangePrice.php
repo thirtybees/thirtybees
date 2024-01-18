thirtybees/thirtybees

commit 4a189631a32cc8a0495b0bfb371c3231e2f3f73b

Cleanup RangePrice

classes/range/RangePrice.php
@@ -37,8 +37,11 @@
 class RangePriceCore extends ObjectModel
 {
     // @codingStandardsIgnoreStart
+    /** @var int $id_carrier */
     public $id_carrier;
+    /** @var float $delimiter1 */
     public $delimiter1;
+    /** @var float $delimiter2 */
     public $delimiter2;
     // @codingStandardsIgnoreEnd
 
@@ -103,6 +106,8 @@ class RangePriceCore extends ObjectModel
     /**
      * Get all available price ranges
      *
+     * @param int $idCarrier
+     *
      * @return array Ranges
      *
      * @since   1.0.0
@@ -111,19 +116,19 @@ class RangePriceCore extends ObjectModel
     public static function getRanges($idCarrier)
     {
         return Db::getInstance(_PS_USE_SQL_SLAVE_)->executeS(
-            '
-            SELECT *
-            FROM `'._DB_PREFIX_.'range_price`
-            WHERE `id_carrier` = '.(int) $idCarrier.'
-            ORDER BY `delimiter1` ASC'
+            (new DbQuery())
+                ->select('*')
+                ->from('range_price')
+                ->where('`id_carrier` = '.(int) $idCarrier)
+                ->orderBy('`delimiter1` ASC')
         );
     }
 
     /**
-     * @param      $idCarrier
-     * @param      $delimiter1
-     * @param      $delimiter2
-     * @param null $idReference
+     * @param int      $idCarrier
+     * @param float    $delimiter1
+     * @param float    $delimiter2
+     * @param int|null $idReference
      *
      * @return false|null|string
      *
@@ -133,42 +138,37 @@ class RangePriceCore extends ObjectModel
     public static function rangeExist($idCarrier, $delimiter1, $delimiter2, $idReference = null)
     {
         return Db::getInstance(_PS_USE_SQL_SLAVE_)->getValue(
-            '
-            SELECT count(*)
-            FROM `'._DB_PREFIX_.'range_price` rp'.
-            (is_null($idCarrier) && $idReference ? '
-            INNER JOIN `'._DB_PREFIX_.'carrier` c on (rp.`id_carrier` = c.`id_carrier`)' : '').'
-            WHERE'.
-            ($idCarrier ? ' `id_carrier` = '.(int) $idCarrier : '').
-            (is_null($idCarrier) && $idReference ? ' c.`id_reference` = '.(int) $idReference : '').'
-            AND `delimiter1` = '.(float) $delimiter1.' AND `delimiter2` = '.(float) $delimiter2
+            (new DbQuery())
+                ->select('COUNT(*)')
+                ->from('range_price', 'rp')
+                ->join((is_null($idCarrier) && $idReference) ? 'INNER JOIN `'._DB_PREFIX_.'carrier` c on (rp.`id_carrier` = c.`id_carrier`)' : '')
+                ->where($idCarrier ? ' `id_carrier` = '.(int) $idCarrier : '')
+                ->where(is_null($idCarrier) && $idReference ? ' c.`id_reference` = '.(int) $idReference : '')
+                ->where('`delimiter1` = '.(float) $delimiter1)
+                ->where('`delimiter2` = '.(float) $delimiter2)
         );
     }
 
     /**
-     * @param      $id_carrier
-     * @param      $delimiter1
-     * @param      $delimiter2
-     * @param null $id_rang
+     * @param int      $idCarrier
+     * @param int      $delimiter1
+     * @param int      $delimiter2
+     * @param int|null $idRang
      *
      * @return false|null|string
      *
      * @since   1.0.0
      * @version 1.0.0 Initial version
      */
-    public static function isOverlapping($id_carrier, $delimiter1, $delimiter2, $id_rang = null)
+    public static function isOverlapping($idCarrier, $delimiter1, $delimiter2, $idRang = null)
     {
         return Db::getInstance(_PS_USE_SQL_SLAVE_)->getValue(
-            '
-            SELECT count(*)
-            FROM `'._DB_PREFIX_.'range_price`
-            WHERE `id_carrier` = '.(int) $id_carrier.'
-            AND ((`delimiter1` >= '.(float) $delimiter1.' AND `delimiter1` < '.(float) $delimiter2.')
-                OR (`delimiter2` > '.(float) $delimiter1.' AND `delimiter2` < '.(float) $delimiter2.')
-                OR ('.(float) $delimiter1.' > `delimiter1` AND '.(float) $delimiter1.' < `delimiter2`)
-                OR ('.(float) $delimiter2.' < `delimiter1` AND '.(float) $delimiter2.' > `delimiter2`)
-            )
-            '.(!is_null($id_rang) ? ' AND `id_range_price` != '.(int) $id_rang : '')
+            (new DbQuery())
+                ->select('COUNT(*)')
+                ->from('range_price')
+                ->where('`id_carrier` = '.(int) $idCarrier)
+                ->where('((`delimiter1` >= '.(float) $delimiter1.' AND `delimiter1` < '.(float) $delimiter2.') OR (`delimiter2` > '.(float) $delimiter1.' AND `delimiter2` < '.(float) $delimiter2.') OR ('.(float) $delimiter1.' > `delimiter1` AND '.(float) $delimiter1.' < `delimiter2`) OR ('.(float) $delimiter2.' < `delimiter1` AND '.(float) $delimiter2.' > `delimiter2`)')
+                ->where(!is_null($idRang) ? ' AND `id_range_price` != '.(int) $idRang : '')
         );
     }
 }

commit e76fe3b4031d21c4ed32a3ea4f3e3a427e096e78

Fix SQL exception in methods isOverlapping
Refactoring from commit 4a189631a32cc8a0495b0bfb371c3231e2f3f73b caused
illegal SQL exception.

Methods isOverlapping is not used by core anymore. However, some third
party modules are using it.

classes/range/RangePrice.php
@@ -131,7 +131,7 @@ class RangePriceCore extends ObjectModel
                 ->select('COUNT(*)')
                 ->from('range_price')
                 ->where('`id_carrier` = '.(int) $idCarrier)
-                ->where('((`delimiter1` >= '.(float) $delimiter1.' AND `delimiter1` < '.(float) $delimiter2.') OR (`delimiter2` > '.(float) $delimiter1.' AND `delimiter2` < '.(float) $delimiter2.') OR ('.(float) $delimiter1.' > `delimiter1` AND '.(float) $delimiter1.' < `delimiter2`) OR ('.(float) $delimiter2.' < `delimiter1` AND '.(float) $delimiter2.' > `delimiter2`)')
+                ->where('(`delimiter1` >= '.(float) $delimiter1.' AND `delimiter1` < '.(float) $delimiter2.') OR (`delimiter2` > '.(float) $delimiter1.' AND `delimiter2` < '.(float) $delimiter2.') OR ('.(float) $delimiter1.' > `delimiter1` AND '.(float) $delimiter1.' < `delimiter2`) OR ('.(float) $delimiter2.' < `delimiter1` AND '.(float) $delimiter2.' > `delimiter2`)')
                 ->where(!is_null($idRang) ? '`id_range_price` != '.(int) $idRang : '')
         );
     }

classes/range/RangeWeight.php
@@ -133,7 +133,7 @@ class RangeWeightCore extends ObjectModel
                 ->select('COUNT(*)')
                 ->from('range_weight')
                 ->where('`id_carrier` = '.(int) $idCarrier)
-                ->where('((`delimiter1` >= '.(float) $delimiter1.' AND `delimiter1` < '.(float) $delimiter2.') OR (`delimiter2` > '.(float) $delimiter1.' AND `delimiter2` < '.(float) $delimiter2.') OR ('.(float) $delimiter1.' > `delimiter1` AND '.(float) $delimiter1.' < `delimiter2`) OR ('.(float) $delimiter2.' < `delimiter1` AND '.(float) $delimiter2.' > `delimiter2`)')
+                ->where('(`delimiter1` >= '.(float) $delimiter1.' AND `delimiter1` < '.(float) $delimiter2.') OR (`delimiter2` > '.(float) $delimiter1.' AND `delimiter2` < '.(float) $delimiter2.') OR ('.(float) $delimiter1.' > `delimiter1` AND '.(float) $delimiter1.' < `delimiter2`) OR ('.(float) $delimiter2.' < `delimiter1` AND '.(float) $delimiter2.' > `delimiter2`)')
                 ->where(!is_null($idRang) ? '`id_range_weight` != '.(int) $idRang : '')
         );
     }