b0a98c07fb826cc649a8e57afcab7bc4120a41cc
Petr Hučík <petr@getdatakick.com>
2024-07-18 12:08:02 +0200

    Implement Webservice Walker Test
    This tests goes over most webservice urls and verifies that they don't
    return errors
    
    Closes #1849

---
+++ tests/Functional/Front/WebserviceWalkerCest.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace Functional\Front;
+
+use Codeception\Example;
+use PrestaShopCollection;
+use PrestaShopException;
+use Tests\Support\FunctionalTester;
+use Validate;
+
+class WebserviceWalkerCest
+{
+    /**
+     * This tests walks through front office category pages and ensures that no error is thrown
+     *
+     * @param FunctionalTester $I
+     * @param Example $example
+     *
+     * @return void
+     *
+     * @dataProvider getResourceToTest
+     */
+    public function testResource(FunctionalTester $I, Example $example)
+    {
+        $key = WEBSERVICE_TEST_KEY;
+        $url = str_replace('?', '&', ltrim($example['url'], '/'));
+        $request = "/webservice/dispatcher.php?ws_key=$key&url=$url";
+        $I->amOnPage($request);
+        $this->verifyNoError($I);
+    }
+
+    /**
+     * @return array
+     * @throws PrestaShopException
+     */
+    protected function getResourceToTest()
+    {
+        // index pages
+        $toTest = [
+            ['url' => '/']
+        ];
+
+        foreach (\WebserviceRequest::getResources() as $key => $resource) {
+            $url = '/' . $key;
+            if ($key === 'search') {
+                $url .= '?query=candle&language=1';
+            }
+
+            // index page
+            $toTest[] = [
+                'url' => $url
+            ];
+
+            // one detail page
+            if (isset($resource['class'])) {
+                $class = $resource['class'];
+                $obj = (new PrestaShopCollection($class))->getFirst();
+                if (Validate::isLoadedObject($obj)) {
+                    $toTest[] = [
+                        'url' => $url . '/' . $obj->id
+                    ];
+                }
+            }
+        }
+
+        // images index page
+        $toTest[] = ['url' => '/images/general'];
+        $toTest[] = ['url' => '/images/products'];
+        $toTest[] = ['url' => '/images/categories'];
+        $toTest[] = ['url' => '/images/manufacturers'];
+        $toTest[] = ['url' => '/images/suppliers'];
+        $toTest[] = ['url' => '/images/stores'];
+        $toTest[] = ['url' => '/images/customizations'];
+
+        usort($toTest, function($a, $b) {
+            return strcmp($a['url'], $b['url']);
+        });
+
+        return $toTest;
+    }
+
+    /**
+     * @param FunctionalTester $I
+     *
+     * @return void
+     */
+    protected function verifyNoError(FunctionalTester $I)
+    {
+        $source = $I->grabPageSource();
+        if (! $source) {
+            $I->fail("Empty response");
+        }
+        $result = simplexml_load_string($source);
+        if (! $result) {
+            $I->fail("Failed to parse response XML: $source");
+        }
+        if (isset($result->errors)) {
+            foreach ($result->errors as $error) {
+                foreach ($error as $c1) {
+                    foreach ($c1 as $c2) {
+                        if ($c2->getName() === "message") {
+                            $I->fail($c2 . "\n\n" . $source);
+                            return;
+                        }
+                    }
+                }
+            }
+            $I->fail($source);
+        }
+    }
+}
--- tests/Functional/_bootstrap.php
+++ tests/Functional/_bootstrap.php
@@ -14,3 +14,20 @@ Configuration::updateValue('PS_SMARTY_FORCE_COMPILE', 1);
 
 // SET tracking id
 Configuration::updateValue(Configuration::TRACKING_ID, 'test-runner');
+
+// Enable webservice
+Configuration::updateValue('PS_WEBSERVICE', 1);
+define('WEBSERVICE_TEST_KEY', 'TEST_KEY________________________');
+if (!$key = WebserviceKey::getInstanceByKey(WEBSERVICE_TEST_KEY)) {
+    $key = new WebserviceKey();
+    $key->key = WEBSERVICE_TEST_KEY;
+    $key->context_employee_id = 1;
+    $key->save();
+}
+
+// set all permission
+$allPersmission = [];
+foreach (WebserviceRequest::getResources() as $k => $_) {
+    $allPersmission[$k] = ['GET' => 1, 'PUT' => 1, 'POST' => 1, 'DELETE' => 1, 'HEAD' => 1];
+}
+WebserviceKey::setPermissionForAccount($key->id, $allPersmission);
