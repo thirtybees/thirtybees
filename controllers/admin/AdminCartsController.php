a85491e10c91777f3bc35dbcaac8c3220957a55c
Petr Hučík <petr@getdatakick.com>
2024-10-03 08:27:57 +0200

    Cart list: hide delete button for converted carts
    AdminCartsController already contained that functionality, but it didn't
    work correctly because id_order was not exposed in list sql.
    
    Closes #1898

--- controllers/admin/AdminCartsController.php
+++ controllers/admin/AdminCartsController.php
@@ -54,7 +54,7 @@ class AdminCartsControllerCore extends AdminController
         $this->allow_export = true;
         $this->_orderWay = 'DESC';
 
-        $this->_select = 'CONCAT(LEFT(c.`firstname`, 1), \'. \', c.`lastname`) `customer`, a.id_cart total, ca.name carrier,
+        $this->_select = 'CONCAT(LEFT(c.`firstname`, 1), \'. \', c.`lastname`) `customer`, a.id_cart total, ca.name carrier, o.id_order,
 		IF (IFNULL(o.id_order, \''.$this->l('Non ordered').'\') = \''.$this->l('Non ordered').'\', IF(TIME_TO_SEC(TIMEDIFF(\''.pSQL(date('Y-m-d H:i:00', time())).'\', a.`date_add`)) > 86400, \''.$this->l('Abandoned cart').'\', \''.$this->l('Non ordered').'\'), o.id_order) AS status, a.`date_upd`, IF(o.id_order, 1, 0) badge_success, IF(o.id_order, 0, 1) badge_danger, IF(co.id_guest, 1, 0) id_guest';
         $this->_join = 'LEFT JOIN '._DB_PREFIX_.'customer c ON (c.id_customer = a.id_customer)
 		LEFT JOIN '._DB_PREFIX_.'currency cu ON (cu.id_currency = a.id_currency)
@@ -1098,15 +1098,15 @@ class AdminCartsControllerCore extends AdminController
         $skipList = [];
 
         foreach ($this->_list as $row) {
-            if (isset($row['id_order']) && is_numeric($row['id_order'])) {
-                $skipList[] = $row['id_cart'];
+            if ((int)$row['id_order']) {
+                $skipList[] = (int)$row['id_cart'];
             }
         }
 
         if (array_key_exists('delete', $helper->list_skip_actions)) {
-            $helper->list_skip_actions['delete'] = array_merge($helper->list_skip_actions['delete'], (array) $skipList);
+            $helper->list_skip_actions['delete'] = array_merge($helper->list_skip_actions['delete'], $skipList);
         } else {
-            $helper->list_skip_actions['delete'] = (array) $skipList;
+            $helper->list_skip_actions['delete'] = $skipList;
         }
 
         $list = $helper->generateList($this->_list, $this->fields_list);
