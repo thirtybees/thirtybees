bee1c8a1f366096009b565a986904a0a121fa8f0
Yabbertb <info@thirtybees.eu.org>
2024-12-12 05:42:13 +0100

    TinyMCE codemirror plugin

---
+++ js/tiny_mce/plugins/codemirror/langs/pl.js
@@ -0,0 +1,8 @@
+tinymce.addI18n('pl',{
+    'HTML source code': 'Kod źródłowy HTML',
+    'Start search': 'Wyszukiwanie',
+    'Find next': 'Znajdź następny',
+    'Find previous': 'Znajdź poprzedni',
+    'Replace': 'Zastąp',
+    'Replace all': 'Zastąp wszystko'
+});
