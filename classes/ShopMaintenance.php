6f0e24c3a3d225a8adea1deb1c0bd5c6c55d285d
therampagerado <office@rampagesport.eu>
2025-04-10 17:58:34 +0300

    Fix a bug with auto db backup
    Previously a concurrent execution was possible when page refreshes
    during active backup or if multiple employees are logged at the same
    time.

--- classes/ShopMaintenance.php
+++ classes/ShopMaintenance.php
@@ -27,6 +27,11 @@
 class ShopMaintenanceCore
 {
     /**
+     * Database lock name.
+     */
+    const BACKUP_LOCK_NAME = 'TB_AUTO_BACKUP_LOCK';
+
+    /**
      * Run tasks as needed. Should take care of running tasks not more often
      * than needed and that one run takes not longer than a few seconds.
      *
@@ -183,11 +188,50 @@ class ShopMaintenanceCore
      */
     public static function autoDbBackup()
     {
-        if (Configuration::get('TB_DB_AUTO_BACKUP')) {
+        if (!Configuration::get('TB_DB_AUTO_BACKUP')) {
+            return;
+        }
+
+        if (!self::lock()) {
+            throw new PrestaShopException('Automatic backup skipped - lock not acquired');
+        }
+
+        try {
             $backup = new PrestaShopBackup();
-            if ($backup->add()) {
-                PrestaShopLogger::addLog('Automatic backup created: ' . basename($backup->id), 1, null, 'ShopMaintenance', null, true);
+            if (!$backup->add()) {
+                throw new PrestaShopException('Automatic backup failed: backup->add() returned false');
             }
+        } finally {
+            self::releaseLock();
+        }
+    }
+
+    /**
+     * Attempts to acquire the MySQL lock defined by static::LOCK_NAME.
+     *
+     * @return bool True if the lock was acquired, false otherwise.
+     */
+    protected static function lock()
+    {
+        try {
+            $connection = Db::getInstance();
+            return (bool)(int)$connection->getValue("SELECT GET_LOCK('" . static::BACKUP_LOCK_NAME . "', 3)");
+        } catch (Throwable $e) {
+            return false;
+        }
+    }
+
+    /**
+     * Releases the MySQL lock identified by static::LOCK_NAME.
+     *
+     * @return void
+     */
+    protected static function releaseLock()
+    {
+        try {
+            $connection = Db::getInstance();
+            $connection->execute("SELECT RELEASE_LOCK('" . static::BACKUP_LOCK_NAME . "')");
+        } catch (Throwable $ignored) {
         }
     }
     
