8d81d076375d8a8bac5962eb66d14528553fb967
Petr Hučík <petr@getdatakick.com>
2025-05-19 14:52:20 +0200

    Fix calculation of front office tranlsation KPI
    The previous code always displayed translation KPI as 100%, because it
    stored the same value for both TRANSLATE_DONE_* and TRANSLATE_TOTAL_*
    keys.
    
    This commit also fixes the issue with invalid theme name key.
    
    Closes #2012

--- controllers/admin/AdminStatsController.php
+++ controllers/admin/AdminStatsController.php
@@ -221,9 +221,9 @@ class AdminStatsControllerCore extends AdminStatsTabController
                 foreach ($themes as $theme) {
                     /** @var Theme $theme */
                     foreach ($languages as $language) {
-                        $kpiKey = substr(strtoupper($theme->name.'_'.$language['iso_code']), 0, 16);
-                        $total += (int) ConfigurationKPI::get('TRANSLATE_TOTAL_'.$kpiKey);
-                        $translated += (int) ConfigurationKPI::get('TRANSLATE_DONE_'.$kpiKey);
+                        $kpiKey = $theme->name.'_'.$language['iso_code'];
+                        $total += (int) ConfigurationKPI::get(Configuration::getValidConfigKey('TRANSLATE_TOTAL_'.$kpiKey));
+                        $translated += (int) ConfigurationKPI::get(Configuration::getValidConfigKey('TRANSLATE_DONE_'.$kpiKey));
                     }
                 }
                 $value = 0;
@@ -997,22 +997,22 @@ class AdminStatsControllerCore extends AdminStatsTabController
             // Add flat fees for this order
             $flatFees = Configuration::get('CONF_ORDER_FIXED') + (
                 $order['id_currency'] == Configuration::get('PS_CURRENCY_DEFAULT')
-                    ? Configuration::get('CONF_'.strtoupper((string)$order['module']).'_FIXED')
-                    : Configuration::get('CONF_'.strtoupper((string)$order['module']).'_FIXED_FOREIGN')
+                    ? Configuration::get(Configuration::getValidConfigKey('CONF_'.$order['module'].'_FIXED'))
+                    : Configuration::get(Configuration::getValidConfigKey('CONF_'.$order['module'].'_FIXED_FOREIGN'))
                 );
 
             // Add variable fees for this order
             $varFees = $order['total_paid_tax_incl'] * (
                 $order['id_currency'] == Configuration::get('PS_CURRENCY_DEFAULT')
-                    ? Configuration::get('CONF_'.strtoupper((string)$order['module']).'_VAR')
-                    : Configuration::get('CONF_'.strtoupper((string)$order['module']).'_VAR_FOREIGN')
+                    ? Configuration::get(Configuration::getValidConfigKey('CONF_'.$order['module'].'_VAR'))
+                    : Configuration::get(Configuration::getValidConfigKey('CONF_'.$order['module'].'_VAR_FOREIGN'))
                 ) / 100;
 
             // Add shipping fees for this order
             $shippingFees = $order['total_shipping_tax_excl'] * (
                 $order['id_country'] == Configuration::get('PS_COUNTRY_DEFAULT')
-                    ? Configuration::get('CONF_'.strtoupper((string)$order['carrier_reference']).'_SHIP')
-                    : Configuration::get('CONF_'.strtoupper((string)$order['carrier_reference']).'_SHIP_OVERSEAS')
+                    ? Configuration::get(Configuration::getValidConfigKey('CONF_'.$order['carrier_reference'].'_SHIP'))
+                    : Configuration::get(Configuration::getValidConfigKey('CONF_'.$order['carrier_reference'].'_SHIP_OVERSEAS'))
                 ) / 100;
 
             // Tally up these fees
@@ -1175,4 +1175,5 @@ class AdminStatsControllerCore extends AdminStatsTabController
             return $tableAlias.'.`invoice_date`';
         }
     }
+
 }
