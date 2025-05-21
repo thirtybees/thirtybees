thirtybees/thirtybees

commit e4388ebbe1b4ca355831a7b0114d93480d0eadaa

Fix PHP warning
Method Meta::getMetaByPage can return false if no meta is defined for
give page. When that happen, PHP raised warning "Trying to access array
offset on value of type bool".

classes/Meta.php
@@ -384,14 +384,25 @@ class MetaCore extends ObjectModel
     public static function getHomeMetas($idLang, $pageName)
     {
         $metas = Meta::getMetaByPage($pageName, $idLang);
-        $ret['meta_title'] = (isset($metas['title']) && $metas['title']) ? $metas['title'].' - '.Configuration::get('PS_SHOP_NAME') : Configuration::get('PS_SHOP_NAME');
-        $ret['meta_description'] = (isset($metas['description']) && $metas['description']) ? $metas['description'] : '';
-        $ret['meta_keywords'] = (isset($metas['keywords']) && $metas['keywords']) ? $metas['keywords'] : '';
-        if ($metas['nobots']) {
-            $ret['nobots'] = true;
-            $ret['nofollow'] = true;
+        $shopName = (string)Configuration::get('PS_SHOP_NAME');
+        if ($metas) {
+            $title = (string)$metas['meta_title'];
+            return [
+                'meta_title' => $title ? $title . ' - ' . $shopName : $shopName,
+                'meta_description' => (string)$metas['description'],
+                'meta_keywords' => (string)$metas['keywords'],
+                'nobots' => (bool)$metas['nobots'],
+                'nofollow' => (bool)$metas['nofollow'],
+            ];
+        } else {
+            return [
+                'meta_title' => $shopName,
+                'meta_description' => '',
+                'meta_keywords' => '',
+                'nobots' => false,
+                'nofollow' => false
+            ];
         }
-        return $ret;
     }
 
     /**

commit aa8e2c7f618d5564be2791c5c17ae02d2070f0ed

Fix typo
Recent refactoring in commit e4388ebbe1b4ca355831a7b0114d93480d0eadaa
introduced a bug - incorrect array key was used.

classes/Meta.php
@@ -386,13 +386,13 @@ class MetaCore extends ObjectModel
         $metas = Meta::getMetaByPage($pageName, $idLang);
         $shopName = (string)Configuration::get('PS_SHOP_NAME');
         if ($metas) {
-            $title = (string)$metas['meta_title'];
+            $title = (string)$metas['title'];
             return [
                 'meta_title' => $title ? $title . ' - ' . $shopName : $shopName,
                 'meta_description' => (string)$metas['description'],
                 'meta_keywords' => (string)$metas['keywords'],
                 'nobots' => (bool)$metas['nobots'],
-                'nofollow' => (bool)$metas['nofollow'],
+                'nofollow' => (bool)$metas['nobots'],
             ];
         } else {
             return [